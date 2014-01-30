d0bb71905067dce870a00b887f3a23d228d889c6
Sven Woop <sven.woop@intel.com>
2014-01-30 13:14:59 +0100

    simple hair shading

--- tutorials/tutorial07/tutorial07_device.cpp
+++ tutorials/tutorial07/tutorial07_device.cpp
@@ -79,11 +79,12 @@ unsigned int addCube (RTCScene scene_i)
 
 /*! random number generator for floating point numbers in range [0,1] */
 inline float frand(int& seed) {
-  seed = 7 * seed + 5;
+  /*seed = 7 * seed + 5;
   seed = 13 * seed + 17;
   seed = 3 * seed + 2;
   seed = 127 * seed + 13;
-  return (seed & 0xFFFF)/(float)0xFFFF;
+  return (seed & 0xFFFF)/(float)0xFFFF;*/
+  return drand48();
 }
 
 /*! Uniform hemisphere sampling. Up direction is the z direction. */
@@ -97,6 +98,7 @@ Vec3f sampleSphere(const float& u, const float& v)
 Vec3f noise(Vec3f p, float t) {
   //return div(p,length(p));
   return p + Vec3f(sin(4.0f*t),4.0f*t,cos(4.0f*t));
+  //return p + Vec3f(4.0f*t,4.0f*t,0.0f);
 }
 
 Vertex* vertices = NULL;
@@ -121,7 +123,7 @@ unsigned int addHair (RTCScene scene_i)
 
 #else
   int seed = 879;
-  const int numCurves = 1;
+  const int numCurves = 1000;
   const int numCurveSegments = 4;
   const int numCurvePoints = 3*numCurveSegments+1;
   const float R = 0.01f;
@@ -137,10 +139,10 @@ unsigned int addHair (RTCScene scene_i)
   {
     float ru = frand(seed);
     float rv = frand(seed);
-    ru = rv = 0.5f;
+    //ru = rv = 0.5f;
     //Vec3f d = sampleSphere(ru,rv);
     //Vec3f p = div(d,max(abs(d.x),max(abs(d.y),abs(d.z))));
-    Vec3f p = Vec3f(-10.0f+ru*20.0f,-2.0f,-10.0f+rv*20.0f);
+    Vec3f p = Vec3f(-2.0f+ru*4.0f,-2.0f,-2.0f+rv*4.0f);
     for (size_t j=0; j<=numCurveSegments; j++) 
     {
       bool last = j == numCurveSegments;
@@ -250,6 +252,8 @@ __forceinline Vec3fa evalBezier(const int primID, const float t)
   //tangent = p21-p20;
 }
 
+float T_hair = 0.3f;
+
 float occluded(RTCScene scene, RTCRay& shadow)
 {
   float T = 1.0f;
@@ -259,20 +263,82 @@ float occluded(RTCScene scene, RTCRay& shadow)
     if (shadow.geomID == RTC_INVALID_GEOMETRY_ID) break;
     if (shadow.geomID != 0) return 0.0f;
     
-    /* calculate size of shadow ray at hit point */
+    /* calculate how much the curve occludes the ray */
     float sizeRay = max(shadow.org.w + shadow.tfar*shadow.dir.w, 0.00001f);
-    
-    /* calculate size of hair at hit point */
     float sizeCurve = evalBezier(shadow.primID,shadow.u).w;
-    
-    /* calculate how much the curve occluded the ray */
-    float Tcurve = 0.3f;
-    T *= 1.0f-clamp((1.0f-Tcurve)*sizeCurve/sizeRay,0.0f,1.0f);
+    T *= 1.0f-clamp((1.0f-T_hair)*sizeCurve/sizeRay,0.0f,1.0f);
+
+    /* continue shadow ray */
     shadow.geomID = RTC_INVALID_GEOMETRY_ID;
     shadow.tnear = 1.001f*shadow.tfar;
+    shadow.tfar = inf;
   }
   return T;
 }
+
+/*! Anisotropic power cosine microfacet distribution. */
+class AnisotropicPowerCosineDistribution {
+public:
+
+  __forceinline AnisotropicPowerCosineDistribution() {}
+
+  /*! Anisotropic power cosine distribution constructor. */
+  __forceinline AnisotropicPowerCosineDistribution(const Vec3fa& R, const Vec3fa& dx, float nx, const Vec3fa& dy, float ny, const Vec3fa& dz) 
+    : R(R), dx(dx), nx(nx), dy(dy), ny(ny), dz(dz),
+      //norm1(sqrtf((nx+1)*(ny+1)) * float(one_over_two_pi)),
+      //norm2(sqrtf((nx+2)*(ny+2)) * float(one_over_two_pi)) {}
+      norm1(1.0f), norm2(1.0f) {}
+
+  /*! Evaluates the power cosine distribution. \param wh is the half
+   *  vector */
+  __forceinline float eval(const Vec3fa& wh) const 
+  {
+    const float cosPhiH   = dot(wh, dx);
+    const float sinPhiH   = dot(wh, dy);
+    const float cosThetaH = dot(wh, dz);
+    const float R = sqr(cosPhiH)+sqr(sinPhiH);
+    if (R == 0.0f) return norm2;
+    const float n = (nx*sqr(cosPhiH)+ny*sqr(sinPhiH))*rcp(R);
+    return norm2 * pow(abs(cosThetaH), n);
+  }
+
+  __forceinline Vec3fa reflect(const Vec3fa& I, const Vec3fa& N) const {
+    return I-2.0f*dot(I,N)*N;
+  }
+
+  __forceinline Vec3fa eval(int geomID, const Vec3fa& wo, const Vec3fa& wi_) const
+  {
+    Vec3fa wi = wi_;
+    //if (dot(wi,dz) <= 0) return zero;
+    const float cosThetaO = dot(wo,dz);
+    const float cosThetaI = dot(wi,dz);
+    //if (cosThetaI <= 0.0f || cosThetaO <= 0.0f) return zero;
+    if (cosThetaI < 0.0f) {
+      wi = reflect(wi,dz);
+    }
+    //PRINT(cosThetaO);
+    //PRINT(cosThetaI);
+    const Vec3fa wh = normalize(wi + wo);
+    const float cosThetaH = dot(wh, dz);
+    //if (geomID == 0) PRINT(cosThetaH);
+    const float cosTheta = dot(wi, wh); // = dot(wo, wh);
+    const float D = eval(wh);
+    //const float G = min(1.0f, 2.0f * cosThetaH * cosThetaO * rcp(cosTheta), 2.0f * cosThetaH * cosThetaI * rcp(cosTheta));
+    //return R * D * G * rcp(4.0f*cosThetaO);
+    const float G = dot(wi,dz);
+    return R*D*G;
+  }
+
+public:
+  Vec3fa R;
+  Vec3fa dx;       //!< x-direction of the distribution.
+  float nx;        //!< Glossiness in x direction with range [0,infinity[ where 0 is a diffuse surface.
+  Vec3fa dy;       //!< y-direction of the distribution.
+  float ny;        //!< Exponent that determines the glossiness in y direction.
+  Vec3fa dz;       //!< z-direction of the distribution.
+  float norm1;     //!< Normalization constant for calculating the pdf for sampling.
+  float norm2;     //!< Normalization constant for calculating the distribution.
+};
 
 /* task that renders a single screen tile */
 Vec3fa renderPixelStandard(int x, int y, const Vec3fa& vx, const Vec3fa& vy, const Vec3fa& vz, const Vec3fa& p)
@@ -290,18 +356,58 @@ Vec3fa renderPixelStandard(int x, int y, const Vec3fa& vx, const Vec3fa& vy, con
   ray.primID = RTC_INVALID_GEOMETRY_ID;
   ray.mask = -1;
   ray.time = 0;
-  
-  /* intersect ray with scene */
-  rtcIntersect(g_scene,ray);
-  
-  /* shade pixels */
-  Vec3f color = Vec3f(0.0f);
-  if (ray.geomID != RTC_INVALID_GEOMETRY_ID) 
+
+  Vec3fa color = Vec3f(0.0f);
+  float weight = 1.0f;
+
+  while (true)
   {
-    Vec3f diffuse = Vec3f(1.0f,0.9f,0.8f); //colors[ray.primID];
-    if (ray.geomID == 1) diffuse = Vec3f(1.0f,1.0f,1.0f);
+    /* intersect ray with scene */
+    rtcIntersect(g_scene,ray);
+  
+    /* exit if we hit environment */
+    if (ray.geomID == RTC_INVALID_GEOMETRY_ID) 
+      return color;
+
+    /* calculate transmissivity of hair */
+    AnisotropicPowerCosineDistribution brdf;
+
+    float Th = 0.0f;
+    if (ray.geomID == 0) 
+    {
+      /* calculate how much the curve occludes the ray */
+      float sizeRay = max(ray.org.w + ray.tfar*ray.dir.w, 0.00001f);
+      float sizeCurve = evalBezier(ray.primID,ray.u).w;
+      Th = 1.0f-clamp((1.0f-T_hair)*sizeCurve/sizeRay,0.0f,1.0f);
+
+      /* calculate tangent space */
+      const Vec3fa dx = normalize(ray.Ng);
+      const Vec3fa dy = normalize(cross(ray.dir,dx));
+      const Vec3fa dz = normalize(cross(dy,dx));
+
+      /* generate anisotropic BRDF */
+      //const Vec3fa color(0.5f,0.4f,0.4f);
+      const Vec3fa color(1.0f);
+      new (&brdf) AnisotropicPowerCosineDistribution(color,dx,10.0f,dy,1.0f,dz);
+    }
+    else 
+    {
+      /* calculate tangent space */
+      const Vec3fa dz = normalize(ray.Ng);
+      const Vec3fa dx = normalize(cross(dz,ray.dir));
+      const Vec3fa dy = normalize(cross(dz,dx));
+      
+      /* generate isotropic BRDF */
+      const Vec3fa color(1.0f,1.0f,1.0f);
+      new (&brdf) AnisotropicPowerCosineDistribution(color,dx,1.0f,dy,1.0f,dz);
+    }
+    
+    /* calculate shadows */
+    //Vec3f diffuse = Vec3f(0.5f,0.4f,0.4f); //colors[ray.primID];
+    //if (ray.geomID == 1) diffuse = Vec3f(1.0f,1.0f,1.0f);
     //color = add(color,mul(diffuse,0.5f));
-    Vec3f lightDir = normalize(Vec3f(-1,-1,-1));
+    Vec3fa lightDir = normalize(Vec3fa(-1,-1,-1));
+    Vec3fa lightIntensity = Vec3fa(1.0f);
     
     /* initialize shadow ray */
     RTCRay shadow;
@@ -309,7 +415,7 @@ Vec3fa renderPixelStandard(int x, int y, const Vec3fa& vx, const Vec3fa& vy, con
     shadow.org.w = ray.org.w+ray.tfar*ray.dir.w;
     shadow.dir = neg(lightDir);
     shadow.dir.w = 0.0f;
-    shadow.tnear = 0.001f;
+    shadow.tnear = 0.1f;
     shadow.tfar = inf;
     shadow.geomID = RTC_INVALID_GEOMETRY_ID;
     shadow.primID = RTC_INVALID_GEOMETRY_ID;
@@ -320,7 +426,16 @@ Vec3fa renderPixelStandard(int x, int y, const Vec3fa& vx, const Vec3fa& vy, con
     float T = occluded(g_scene,shadow);
     
     /* add light contribution */
-    color = add(color,mul(T,diffuse)); //clamp(-dot(lightDir,normalize(ray.Ng)),0.0f,1.0f)));
+    Vec3fa c = brdf.eval(ray.geomID,neg(ray.dir),neg(lightDir));
+    //if (ray.geomID == 0) PRINT(c);
+    //Vec3fa c = clamp(dot(neg(ray.dir),brdf.dz),0.0f,1.0f);
+    color = add(color,mul(weight*(1.0f-Th),mul(c,mul(T,lightIntensity)))); //clamp(-dot(lightDir,normalize(ray.Ng)),0.0f,1.0f)));
+    weight *= Th;
+
+    /* continue ray */
+    ray.geomID = RTC_INVALID_GEOMETRY_ID;
+    ray.tnear = 1.001f*ray.tfar;
+    ray.tfar = inf;
   }
   return color;
 }
