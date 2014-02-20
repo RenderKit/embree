44e0b515c1d13d853a7de9e5d86befe30cb96f93
Sven Woop <sven.woop@intel.com>
2014-02-20 11:55:40 +0100

    compile fix for Xeon Phi

--- common/math/linearspace3.h
+++ common/math/linearspace3.h
@@ -103,11 +103,13 @@ namespace embree
     Vector vx,vy,vz;
   };
 
+#if !defined(__MIC__)
   /*! compute transposed matrix */
   template<> __forceinline const LinearSpace3<Vec3fa> LinearSpace3<Vec3fa>::transposed() const { 
     ssef rx,ry,rz; transpose((ssef&)vx,(ssef&)vy,(ssef&)vz,ssef(zero),rx,ry,rz);
     return LinearSpace3<Vec3fa>(Vec3fa(rx),Vec3fa(ry),Vec3fa(rz)); 
   }
+#endif
 
   ////////////////////////////////////////////////////////////////////////////////
   // Unary Operators
