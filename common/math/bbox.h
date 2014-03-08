4a17bc25aca6eb09274135fa72cbb0e3e3de810c
Ingo Wald <ingowald@gmail.com>
2014-03-08 12:06:30 -0600

    added volume() for box3f (not just box3fa) classes

--- common/math/bbox.h
+++ common/math/bbox.h
@@ -68,6 +68,8 @@ namespace embree
 
   /*! computes the volume of a bounding box */
   __forceinline float volume( const BBox<Vec3fa>& b ) { return reduce_mul(b.size()); }
+  /*! computes the volume of a bounding box */
+  __forceinline float volume( const BBox<Vec3f>& b )  { return reduce_mul(b.size()); }
 
   /*! computes the surface area of a bounding box */
   template<typename T> __forceinline const T area( const BBox<Vec2<T> >& b ) { const Vec2<T> d = size(b); return d.x*d.y; }
