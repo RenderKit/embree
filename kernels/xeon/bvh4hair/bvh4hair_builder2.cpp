4c0bf49609542275685fbe5627309d0d53b53479
Sven Woop <sven.woop@intel.com>
2014-03-12 10:03:41 +0100

    fixed issue with replication factor

--- kernels/xeon/bvh4hair/bvh4hair_builder2.cpp
+++ kernels/xeon/bvh4hair/bvh4hair_builder2.cpp
@@ -79,15 +79,6 @@ namespace embree
       else if (g_hair_accel_mode.substr(i,4) == "auSP") { enableAlignedSpatialSplits = enableUnalignedSpatialSplits = true; i+=4; } 
       else throw std::runtime_error("invalid hair accel mode");
     }
-
-    if (g_verbose >= 2) {
-      PRINT(enableAlignedObjectSplits);
-      PRINT(enableAlignedSpatialSplits);
-      PRINT(enableUnalignedObjectSplits);
-      PRINT(enableUnalignedSpatialSplits);
-      PRINT(enableStrandSplits);
-      PRINT(enablePreSubdivision);
-    }
   }
 
   void BVH4HairBuilder2::build(size_t threadIndex, size_t threadCount) 
@@ -105,7 +96,15 @@ namespace embree
     numFallbackSplits = 0;
 
     double t0 = 0.0;
-    if (g_verbose >= 2) {
+    if (g_verbose >= 2) 
+    {
+      PRINT(enableAlignedObjectSplits);
+      PRINT(enableAlignedSpatialSplits);
+      PRINT(enableUnalignedObjectSplits);
+      PRINT(enableUnalignedSpatialSplits);
+      PRINT(enableStrandSplits);
+      PRINT(enablePreSubdivision);
+
       std::cout << "building BVH4Hair<" + bvh->primTy.name + "> using BVH4HairBuilder2 ..." << std::flush;
       t0 = getSeconds();
     }
@@ -117,8 +116,6 @@ namespace embree
     BBox3fa bounds = empty;
     size_t numVertices = 0;
     atomic_set<PrimRefBlock> prims;
-    //atomic_set<PrimRefBlock>::item* block = prims.insert(alloc.malloc(threadIndex));
-
     for (size_t i=0; i<scene->size(); i++) 
     {
       Geometry* geom = scene->get(i);
@@ -208,7 +205,7 @@ namespace embree
       taskMutex.unlock();
 
       /* recursively finish task */
-      if (task.size < 512 || remainingReplications <= 0) {
+      if (task.size < 512) {
         atomic_add(&numActiveTasks,-1);
         recurseTask(threadIndex,task);
       }
