ca9746bde569e0bf6030a1375ed851b2badd5a72
cbenthin <carsten.benthin@intel.com>
2014-03-20 22:29:44 +0100

    added recursive intersection for occlusion fkt

--- kernels/xeon/geometry/bezier1i_intersector1.h
+++ kernels/xeon/geometry/bezier1i_intersector1.h
@@ -20,6 +20,9 @@
 #include "common/ray.h"
 #include "geometry/filter.h"
 
+#define USE_RECURSIVE_INTERSECTION 0
+
+
 namespace embree
 {
   /*! Intersector for a single ray with a bezier curve. */
@@ -352,9 +355,12 @@ namespace embree
 #if defined(PRE_SUBDIVISION_HACK)
 	  if (unlikely(any(pre.mbox.ids == curves[i].primID))) continue; // FIXME: works only for single hair set
 #endif
-	  intersect(pre,ray,curves[i],geom);
 
-	  //intersect_recursive(pre,ray,curves[i],geom);
+#if USE_RECURSIVE_INTERSECTION == 0
+	  intersect(pre,ray,curves[i],geom);
+#else
+	  intersect_recursive(pre,ray,curves[i],geom);
+#endif
 
 #if defined(PRE_SUBDIVISION_HACK)
 	  *(unsigned int*)&pre.mbox.ids[pre.mbox.index] = curves[i].primID; // FIXME: works only for single hair set
@@ -424,10 +430,91 @@ namespace embree
       return true;
     }
 
+
+    static bool occluded_recursive(const float max_radius, Ray& ray, const BezierCurve3D &curve2D, const void* geom, int geomID, int primID)
+    {
+      const BBox3fa bounds = curve2D.bounds();
+#if 1
+      if (bounds.lower.x > -max_radius ||
+          bounds.upper.x <  max_radius ||
+          bounds.lower.y > -max_radius ||
+          bounds.upper.y <  max_radius) return false;
+#endif
+      if (curve2D.depth == 0)
+        {
+          const Vec3fa &p0 = curve2D.v0;
+          const Vec3fa &p1 = curve2D.v3;
+
+          /* approximative intersection with cone */
+          const Vec3fa v = p1-p0;
+          const Vec3fa w = -p0;
+          const float d0 = w.x*v.x + w.y*v.y;
+          const float d1 = v.x*v.x + v.y*v.y;
+          const float u = clamp(d0*rcp(d1),0.0f,1.0f);
+          const Vec3fa p = p0 + u*v;
+          const float t = p.z;
+          const float d2 = p.x*p.x + p.y*p.y; 
+          const float r = p.w; //max(p.w,ray.org.w+ray.dir.w*t);
+          const float r2 = r*r;
+
+          STAT3(normal.trav_prim_hits,1,1,1);
+
+          if (unlikely(!( d2 <= r2 & ray.tnear < t & t < ray.tfar ))) return false;
+
+          const float uu = curve2D.t0; // FIXME: correct u range for subdivided segments
+
+          Vec3fa P,T; curve2D.eval(0.5f,P,T); 
+          if (T == Vec3fa(zero)) return false; // { valid[i] = 0; goto retry; } // ignore denormalized curves */
+
+          /* intersection filter test */
+#if defined(__INTERSECTION_FILTER__) && 0
+          Geometry* geometry = ((Scene*)geom)->get(geomID);
+          if (!likely(geometry->hasOcclusionFilter1())) 
+            {
+              return runOcclusionFilter1(geometry,ray,uu,0.0f,t,T,geomID,primID);
+            }
+#endif
+          return true;
+        }
+      else
+        {
+          BezierCurve3D left,right;
+          curve2D.subdivide(left,right);
+          return \
+            occluded_recursive(max_radius,ray,left,geom,geomID,primID) | 
+            occluded_recursive(max_radius,ray,right,geom,geomID,primID);
+        }
+    }
+
+
+    static __forceinline bool occluded_recursive(const Precalculations& pre, Ray& ray, const Bezier1i& curve_in, const void* geom)
+    {
+      /* load bezier curve control points */
+      STAT3(normal.trav_prims,1,1,1);
+      const Vec3fa& v0 = curve_in.p[0];
+      const Vec3fa& v1 = curve_in.p[1];
+      const Vec3fa& v2 = curve_in.p[2];
+      const Vec3fa& v3 = curve_in.p[3];
+
+      /* transform control points into ray space */
+      Vec3fa w0 = xfmVector(pre.ray_space,v0-ray.org); w0.w = v0.w;
+      Vec3fa w1 = xfmVector(pre.ray_space,v1-ray.org); w1.w = v1.w;
+      Vec3fa w2 = xfmVector(pre.ray_space,v2-ray.org); w2.w = v2.w;
+      Vec3fa w3 = xfmVector(pre.ray_space,v3-ray.org); w3.w = v3.w;
+      BezierCurve3D curve2D(w0,w1,w2,w3,0.0f,1.0f,3);
+      
+      const float max_radius = max(w0.w,w1.w,w2.w,w3.w);
+      return occluded_recursive(max_radius,ray,curve2D,geom,curve_in.geomID,curve_in.primID);
+    }
+
     static __forceinline bool occluded(const Precalculations& pre, Ray& ray, const Bezier1i* curves, size_t num, void* geom) 
     {
       for (size_t i=0; i<num; i++) 
+#if USE_RECURSIVE_INTERSECTION == 0
         if (occluded(pre,ray,curves[i],geom))
+#else
+        if (occluded_recursive(pre,ray,curves[i],geom))
+#endif
           return true;
 
       return false;
