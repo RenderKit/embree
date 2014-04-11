4d5163bc912389a7632b6bdcac69c0f0f53e72c4
cbenthin <carsten.benthin@intel.com>
2014-04-11 05:15:09 -0700

    merging changes from v2.2, part I

--- kernels/xeonphi/bvh4i/bvh4i_builder.cpp
+++ kernels/xeonphi/bvh4i/bvh4i_builder.cpp
@@ -35,8 +35,8 @@
 //#define PROFILE
 #define PROFILE_ITERATIONS 100
 
-
 //#define USE_QUANTIZATION
+#define MEASURE_MEMORY_ALLOCATION_TIME 0
 
 // TODO: CHECK     const float voxelArea    = current.cs_AABB.sceneArea();
 //                 const float centroidArea = current.cs_AABB.centroidArea();
@@ -134,6 +134,11 @@ namespace embree
 					 const size_t sizeNodeInBytes,
 					 const size_t sizeAccelInBytes)
   {
+#if MEASURE_MEMORY_ALLOCATION_TIME == 1
+    double msec = 0.0;
+    msec = getSeconds();
+#endif
+
     const size_t additional_size = 16 * CACHELINE_SIZE;
 
     /* free previously allocated memory */
@@ -183,6 +188,11 @@ namespace embree
     bvh->size_accel = size_accel;
 
     size_prims = size_primrefs;    
+
+#if MEASURE_MEMORY_ALLOCATION_TIME == 1
+    msec = getSeconds()-msec;    
+    std::cout << "allocation time " << 1000. * msec << " ms" << std::endl << std::flush;
+#endif
   }
   
   void BVH4iBuilder::allocateData(const size_t threadCount, const size_t totalNumPrimitives)
