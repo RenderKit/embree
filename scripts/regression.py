1cc814173f54ce688dde4189b8bdcafbf0539580
Sven Woop <sven.woop@intel.com>
2014-05-28 14:16:13 +0200

    updates

--- scripts/regression.py
+++ scripts/regression.py
@@ -148,7 +148,7 @@ def compile(OS,compiler,platform,build):
       sys.exit(1)
 
     # first compile Embree
-    command = 'mkdir build; cd build; cmake'
+    command = 'mkdir -p build && cd build && cmake > /dev/null'
     command += compilerOption
     command += ' -D RTCORE_RAY_MASK=OFF'
     command += ' -D RTCORE_BACKFACE_CULLING=OFF'
@@ -170,9 +170,8 @@ def compile(OS,compiler,platform,build):
       command += ' -D TARGET_AVX2=OFF'
 
     command += ' -D CMAKE_BUILD_TYPE=' + build
-    command += ' ..; make clean; make -j 8'
-    command += ' > ../' + logFile
-    print(command)
+    command += ' .. && make clean && make -j 8'
+    command += ' &> ../' + logFile
     return os.system(command)
 
 def compileLoop(OS):
@@ -192,7 +191,9 @@ def render(OS, compiler, platform, build, tutorial, scene, flags):
   base = configName(OS, compiler, platform, build, tutorial, scene, flags)
   logFile = testDir + dash + base + '.log'
   imageFile = testDir + dash + base + '.tga'
-  if not os.path.exists(logFile):
+  if os.path.exists(logFile):
+    sys.stdout.write(" [skipped]\n")
+  else:
     if OS == 'windows': command = platform + '\\' + build + '\\' + tutorial + ' '
     else:               command = 'build' + '/' + tutorial + ' '
     if scene != '':
