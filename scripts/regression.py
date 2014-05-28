92641c6aac4f60fcedfbddabb0a471d74daeec4a
Sven Woop <sven.woop@intel.com>
2014-05-28 05:37:41 +0200

    added regression.py

---
+++ scripts/regression.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python
+
+## ======================================================================== ##
+## Copyright 2009-2012 Intel Corporation                                    ##
+##                                                                          ##
+## Licensed under the Apache License, Version 2.0 (the "License");          ##
+## you may not use this file except in compliance with the License.         ##
+## You may obtain a copy of the License at                                  ##
+##                                                                          ##
+##     http://www.apache.org/licenses/LICENSE-2.0                           ##
+##                                                                          ##
+## Unless required by applicable law or agreed to in writing, software      ##
+## distributed under the License is distributed on an "AS IS" BASIS,        ##
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. ##
+## See the License for the specific language governing permissions and      ##
+## limitations under the License.                                           ##
+## ======================================================================== ##
+
+# Embree Regression Test Script
+# ===============================
+
+# Windows
+# -------
+
+# Prerequisites:
+#   Install Python 3.2+
+#   Install Visual Studio 2010 or 2008
+#   Install Intel C++ Compiler
+#   Check out Embree into <embree_dir>
+
+# Instructions:
+#   Open the "Visual Studio x64 Cross Tools Command Prompt (2010)"
+#   cd <embree_dir>
+#   mkdir TEST
+#   <python_dir>\python.exe <embree_dir>\scripts\benchmark.py render windows TEST
+#   <python_dir>\python.exe <embree_dir>\scripts\benchmark.py extract windows TEST
+
+# Linux and OS X
+# --------------
+
+# Prerequisites:
+#   Install Python 2.6+
+#   Install Intel C++ Compiler
+#   Check out Embree into <embree_dir>
+
+# Instructions:
+#   Open a shell
+#   cd <embree_dir>
+#   mkdir TEST
+#   ./scripts/benchmark.py render linux <model_dir> TEST
+#   ./scripts/benchmark.py extract linux TEST
+
+import sys
+import os
+import re
+
+dash = '/'
+
+########################## configuration ##########################
+
+#compilers_win = ['V100']
+#compilers_win = ['ICC']
+compilers_win  = ['V100', 'ICC']
+#compilers_unix = ['ICC']
+compilers_unix = ['GCC', 'ICC']
+#compilers_unix = ['GCC', 'CLANG', 'ICC']
+compilers      = []
+
+#builds_win = ['Debug']
+#builds_win = ['Release']
+builds_win = ['Release', 'ReleaseAVX', 'Debug']
+#builds_win = ['Release', 'ReleaseAVX', 'ReleaseAVX2', 'Debug']
+#builds_unix = ['Debug']
+#builds_unix = ['Release']
+builds_unix = ['Release', 'Debug']
+builds = []
+
+#platforms_win  = ['win32']
+#platforms_win  = ['x64']
+platforms_win  = ['win32', 'x64']
+platforms_unix = ['x64']
+platforms      = []
+
+devices = [ 'singleray', 'ispc' ]
+
+modelDir  = ''
+testDir = ''
+
+########################## compiling ##########################
+
+def compile(OS,compiler,platform,build):
+  if OS == 'windows':
+  
+    cfg = '/p:Configuration=' + build + ';'
+    cfg += 'Platform=' + platform + ';'
+    cfg += 'PlatformToolset=';
+#   if (compiler == 'ICC'): cfg += '"Intel C++ Compiler 12.1" '
+#   if (compiler == 'ICC'): cfg += '"Intel C++ Compiler XE 12.1" '
+    if (compiler == 'ICC'): cfg += '"Intel C++ Compiler XE 14.0" '
+    elif (compiler == 'V100'): cfg += 'v100 '
+    else: 
+      sys.stderr.write('unknown compiler: ' + compiler + '\n')
+      sys.exit(1)
+
+    # compile Embree
+    command =  'msbuild embree_vs2010.sln' + ' ' + cfg + ' /t:rebuild /verbosity:m'
+    os.system(command)
+  
+  else:
+
+    if (platform != 'x64'):
+      sys.stderr.write('unknown platform: ' + platform + '\n')
+      sys.exit(1)
+
+    if   (compiler == 'ICC'  ): compilerOption = ' -D COMPILER=ICC'
+    elif (compiler == 'GCC'  ): compilerOption = ' -D COMPILER=GCC'
+    elif (compiler == 'CLANG'): compilerOption = ' -D COMPILER=CLANG'
+    else:
+      sys.stderr.write('unknown compiler: ' + compiler + '\n')
+      sys.exit(1)
+
+    # first compile Embree
+    command = 'mkdir build; cd build; cmake'
+    command += compilerOption
+    command += ' -D RTCORE_RAY_MASK=OFF'
+    command += ' -D RTCORE_BACKFACE_CULLING=OFF'
+    command += ' -D RTCORE_INTERSECTION_FILTER=ON'
+    command += ' -D RTCORE_BUFFER_STRIDE=ON'
+
+    command += ' -D TARGET_SSE2=ON';
+    command += ' -D TARGET_SSE41=ON';
+    command += ' -D TARGET_SSE42=ON';
+    command += ' -D TARGET_AVX=ON'
+    command += ' -D TARGET_AVX2=ON'
+
+    command += ' -D CMAKE_BUILD_TYPE=' + build
+    command += ' ..; make clean; make -j 8';
+    os.system(command)
+
+def compileLoop(OS):
+    for compiler in compilers:
+      for platform in platforms:
+        for build in builds:
+          print(OS + ' ' + compiler + ' ' + platform + ' ' + build)
+          compile(OS,compiler,platform,isas,build)
+
+########################## rendering ##########################
+
+def configName(OS, compiler, platform, build):
+  return OS + '_' + compiler + '_' + platform + '_' + build + '_' + scene 
+
+def render(OS, compiler, platform, build, tutorial, scene):
+  if OS == 'windows': command = platform + '\\' + build + '\\' + tutorial
+  else:               command = 'build' + '/' + tutorial
+  base = configName(OS, scene, compiler, platform, build)
+  logFile = testDir + dash + base + tutorial + '.log'
+  imageFile = testDir + dash + base + tutorial + '.tga'
+  if not os.path.exists(logFile):
+    if scene != '':
+	    command += '-c scene '
+	if tutorial == 'regression':
+	    command += '-regressions 2000 '
+    command += '-o ' + imageFile + ' > ' + logFile
+    os.system(command)
+
+def renderLoop(OS):
+    for compiler in compilers:
+      for platform in platforms:
+        for build in builds:
+          print(compiler + ' ' + platform + ' ' + build)
+          compile(OS,compiler,platform,build)
+          
+          render(OS, compiler, platform, build, 'verify', '')
+          render(OS, compiler, platform, build, 'benchmark', '')
+
+          render(OS, compiler, platform, build, 'tutorial00', '')
+          render(OS, compiler, platform, build, 'tutorial01', '')
+          render(OS, compiler, platform, build, 'tutorial02', modelDir + dash + 'crown' + dash + 'crown_paper.ecs')
+          render(OS, compiler, platform, build, 'tutorial03', '')
+          render(OS, compiler, platform, build, 'tutorial04', '')
+          render(OS, compiler, platform, build, 'tutorial05', '')
+          render(OS, compiler, platform, build, 'tutorial06', modelDir + dash + 'crown' + dash + 'crown_paper.ecs')
+          render(OS, compiler, platform, build, 'tutorial07', '')
+
+          render(OS, compiler, platform, build, 'tutorial00_ispc', '')
+          render(OS, compiler, platform, build, 'tutorial01_ispc', '')
+          render(OS, compiler, platform, build, 'tutorial02_ispc', modelDir + dash + 'crown' + dash + 'crown_paper.ecs')
+          render(OS, compiler, platform, build, 'tutorial03_ispc', '')
+          render(OS, compiler, platform, build, 'tutorial04_ispc', '')
+          render(OS, compiler, platform, build, 'tutorial05_ispc', '')
+          render(OS, compiler, platform, build, 'tutorial06_ispc', modelDir + dash + 'crown' + dash + 'crown_paper.ecs')
+          render(OS, compiler, platform, build, 'tutorial07_ispc', '')
+          
+
+########################## command line parsing ##########################
+
+def printUsage():
+  sys.stderr.write('Usage: ' + sys.argv[0] + ' render  <os> <testDir>\n')
+  sys.stderr.write('       ' + sys.argv[0] + ' extract <os> <testDir>\n')
+  sys.exit(1)
+
+if len(sys.argv) < 3: printUsage()
+mode = sys.argv[1]
+OS = sys.argv[2]
+
+if OS == 'windows':
+  dash = '\\'
+  compilers = compilers_win
+  platforms = platforms_win
+  builds = builds_win
+  modelDir = '%HOMEPATH%\\models'
+
+else:
+  dash = '/'
+  compilers = compilers_unix
+  platforms = platforms_unix
+  builds = builds_unix
+  modelDir = '~/models'
+
+if mode == 'render':
+  if len(sys.argv) < 4: printUsage()
+  testDir = sys.argv[3]
+  renderLoop(OS)
+  sys.exit(1)
+
