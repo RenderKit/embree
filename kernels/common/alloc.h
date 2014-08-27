b3c4f237c0bec81b199d589a40e6f45f57654a99
Sven Woop <sven.woop@intel.com>
2014-08-27 11:03:23 +0200

    moved allocator logic into Block class

--- kernels/common/alloc.h
+++ kernels/common/alloc.h
@@ -229,7 +229,7 @@ namespace embree
       {
         cur += bytes + ((align - cur) & (align-1));
         if (likely(cur <= end)) return &ptr[cur - bytes];
-        ptr = (char*) alloc->malloc(allocBlockSize);
+        ptr = (char*) alloc->block.malloc(allocBlockSize);
         cur = 0;
         end = allocBlockSize;
         if (bytes > allocBlockSize) 
@@ -252,27 +252,12 @@ namespace embree
     };
 
     /*! Allocator default construction. */
-    LinearAllocatorPerThread () 
-      : ptr(NULL), cur(0), end(0), bytesAllocated(0)
-    {
-      size_t numThreads = getNumberOfLogicalThreads();
-      thread = new ThreadAllocator[numThreads];
-      for (size_t i=0; i<numThreads; i++) thread[i].alloc = this;
-      ptr = NULL;
-    }
-
-    /*! Allocator destructor. */
-    ~LinearAllocatorPerThread() {
-      delete[] thread; thread = NULL;
-      if (ptr) os_free(ptr,end); ptr = NULL;
-      cur = end = 0;
-    }
+    LinearAllocatorPerThread () {}
 
     /*! Return pointer to start of memory region */
-    __forceinline       void* base()       { return ptr; }
-    __forceinline const void* base() const { return ptr; }
-
-    __forceinline       void* curPtr()       { return ptr+cur; }
+    __forceinline       void* base()       { return block.ptr; }
+    __forceinline const void* base() const { return block.ptr; }
+    __forceinline       void* curPtr()     { return block.ptr+block.cur; }
 
     /*! Aligned memory allocation */
     __forceinline void* malloc(size_t tinfo, size_t bytes, size_t align = 16) {
@@ -282,23 +267,9 @@ namespace embree
     /*! clears the allocator */
     void clear () 
     {
-      cur = 0;
-      const size_t numThreads = getNumberOfLogicalThreads();
-      for (size_t i=0; i<numThreads; i++) 
+      block.clear();
+      for (size_t i=0; i<thread.size(); i++) 
 	thread[i].clear();
-    }
-
-    /*! initializes the allocator */
-    void init (size_t bytes) 
-    {
-      clear();
-      const size_t numThreads = getNumberOfLogicalThreads();
-      bytes = max(bytes,size_t(allocBlockSize*numThreads));
-      if (bytes != size_t(end)) {
-        if (ptr) os_free(ptr,end);
-        ptr = (char*) os_reserve(bytes);
-        end = bytes;
-      }
     }
 
     /*! initializes the allocator */
@@ -306,61 +277,85 @@ namespace embree
     {
       clear();
       const size_t numThreads = getNumberOfLogicalThreads();
-	  bytesReserve = max(bytesAllocate,bytesReserve);
+      if (thread.size() != numThreads) {
+	thread.resize(numThreads);
+	for (size_t i=0; i<numThreads; i++) thread[i].alloc = this;
+      }
+      bytesReserve = max(bytesAllocate,bytesReserve);
       size_t bytesReserved = max(bytesReserve,size_t(allocBlockSize*numThreads));
-      if (bytesReserved != size_t(end) || bytesAllocate != bytesAllocated) 
-      {
-        bytesAllocated = bytesAllocate;
-        if (ptr) os_free(ptr,end);
-        ptr = (char*) os_reserve(bytesReserved);
-        os_commit(ptr,bytesAllocated);
-        //memset(ptr,0,bytesAllocated);
-        end = bytesReserved;
-      }
+      block.init(bytesAllocate,bytesReserved);
     }
 
-    /*! initialized the allocator */
-    void init_malloc (size_t bytes) 
-    {
-      clear();
-      if (bytes != size_t(end)) {
-        if (ptr) os_free(ptr,end);
-        ptr = (char*) os_malloc(bytes);
-        end = bytes;
-      }
-    }
-
-    /*! returns number of committed memory */
+    /*! returns number of committed bytes */
     size_t bytes () const {
-      return cur;
+      return block.cur;
     }
 
     void shrink () {
-      if (ptr == NULL) return;
-      os_shrink(ptr,cur,end);
-      end = cur;
-      bytesAllocated = cur;
+      block.shrink();
     }
 
   private:
 
-    /*! Allocates some number of bytes. */
-    void* malloc(size_t bytes) 
+    struct Block 
     {
-      ssize_t i = atomic_add(&cur,bytes);
-      if (unlikely(i > end)) throw std::runtime_error("build out of memory");
-      void* p = &ptr[i];
-      if (i+(ssize_t)bytes > bytesAllocated)
-        os_commit(p,bytes);
-      return p;
-    }
+      Block () 
+      : ptr(NULL), cur(0), end(0), bytesAllocated(bytesAllocated), next(NULL) {}
+      
+      Block (size_t bytes, Block* next = NULL) 
+      : ptr(NULL), cur(0), end(bytes), bytesAllocated(0), next(next) {}
+
+      ~Block () {
+	if (ptr) os_free(ptr,end); ptr = NULL;
+	cur = end = 0;
+	if (next) delete next; next = NULL;
+      }
+
+      __forceinline void init (size_t bytesAllocate, size_t bytesReserved)
+      {
+	if (bytesReserved != size_t(end) || bytesAllocate != bytesAllocated) 
+	{
+	  bytesAllocated = bytesAllocate;
+	  if (ptr) os_free(ptr,end);
+	  ptr = (char*) os_reserve(bytesReserved);
+	  os_commit(ptr,bytesAllocated);
+	  end = bytesReserved;
+	}
+      }
+
+      __forceinline void clear() {
+	cur = 0;
+      }
+
+      /*! Allocates some number of bytes. */
+      void* malloc(size_t bytes) 
+      {
+	ssize_t i = atomic_add(&cur,bytes);
+	if (unlikely(i > end)) throw std::runtime_error("build out of memory");
+	void* p = &ptr[i];
+	if (i+(ssize_t)bytes > bytesAllocated)
+	  os_commit(p,bytes);
+	return p;
+      }
+
+      void shrink () {
+	if (ptr == NULL) return;
+	os_shrink(ptr,cur,end);
+	end = cur;
+	bytesAllocated = cur;
+      }
+
+    public:
+      char*  ptr;                //!< pointer to memory
+      atomic_t cur;              //!< Current location of the allocator.
+      atomic_t end;              //!< End of the memory block.
+      Block* next;
+      atomic_t bytesAllocated;
+    };
 
   private:
-    ThreadAllocator* thread;   //!< one allocator for each thread
-    char*  ptr;                //!< pointer to memory
-    atomic_t cur;              //!< Current location of the allocator.
-    atomic_t end;              //!< End of the memory block.
-    atomic_t bytesAllocated;
+    std::vector<ThreadAllocator> thread;   //!< one allocator for each thread
+    Block block;
   };
 
   class __aligned(64) GlobalAllocator
