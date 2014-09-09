5558627de46b33d317c39916ffdac97b30598c1a
Sven Woop <sven.woop@intel.com>
2014-09-09 07:41:44 +0200

    moved tree restructuring code

--- kernels/xeon/bvh4/bvh4_rotate.h
+++ kernels/xeon/bvh4/bvh4_rotate.h
@@ -20,14 +20,21 @@
 
 namespace embree
 {
-  /* BVH4 Tree Rotations. */
-  class BVH4Rotate
+  namespace isa 
   {
-  public:
-    typedef BVH4::Node Node;
-    typedef BVH4::NodeRef NodeRef;
-
-  public:
-    static size_t rotate(BVH4* bvh, NodeRef parentRef, size_t depth = 1);
-  };
+    /* BVH4 Tree Rotations. */
+    class BVH4Rotate
+    {
+    public:
+      typedef BVH4::Node Node;
+      typedef BVH4::NodeRef NodeRef;
+      
+    public:
+      static size_t rotate     (BVH4* bvh, NodeRef parentRef, size_t depth = 1);
+      
+#if defined(__AVX__)
+      static void   restructure(NodeRef ref, size_t depth = 1);
+#endif
+    };
+  }
 }
