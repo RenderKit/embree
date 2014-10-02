0da531faa0ae5147cc524316774433150db5e5d6
Sven Woop <sven.woop@intel.com>
2014-10-02 09:33:08 +0200

    added quadquad intersector

--- kernels/xeon/geometry/subdivpatchdispl1_intersector1.h
+++ kernels/xeon/geometry/subdivpatchdispl1_intersector1.h
@@ -34,7 +34,7 @@ namespace embree
     };
 
     /*! Intersect a ray with the triangle and updates the hit. */
-    static __forceinline void intersectTriangle(Ray& ray, const Vec3fa& tri_v0, const Vec3fa& tri_v1, const Vec3fa& tri_v2, const unsigned primID, const unsigned geomID)
+    static __forceinline void intersectTriangle(Ray& ray, const Vec3fa& tri_v0, const Vec3fa& tri_v1, const Vec3fa& tri_v2, const unsigned geomID, const unsigned primID)
     {
       /* calculate vertices relative to ray origin */
       STAT3(normal.trav_prims,1,1,1);
@@ -68,7 +68,7 @@ namespace embree
       const float T = xorf(dot(v0,Ng),sgnDen);
       if (unlikely(absDen*float(ray.tfar) < T)) return;
       if (unlikely(T < absDen*float(ray.tnear))) return;
-      
+
       /* calculate hit information */
       const float rcpAbsDen = rcp(absDen);
       const float u = U * rcpAbsDen;
@@ -82,6 +82,82 @@ namespace embree
       ray.Ng  = Ng;
       ray.geomID = geomID;
       ray.primID = primID;
+    }
+
+    static __forceinline void intersectFinish (Ray& ray, 
+                                               const Vec3fa& q, const Vec3fa& e0, const Vec3fa& e1, const Vec3fa& e2, 
+                                               const float U0, const float U1, const float U2,
+                                               const unsigned geomID, const unsigned primID)
+      {
+        const Vec3fa Ng = cross(e1,e0);
+        const float det = dot(ray.dir,Ng);
+        const float T   = dot(q,Ng);
+        if (unlikely(det*ray.tnear <= T && T <= det*ray.tfar)) {
+          const float rcpDet = rcp(det);
+          ray.u    = U0 * rcpDet;
+          ray.v    = U1 * rcpDet;
+          ray.tfar = T * rcpDet;
+          ray.Ng   = Ng;
+          ray.geomID  = geomID;
+          ray.primID  = primID;
+        }
+      }
+
+    static __forceinline void intersectQuadQuad (Ray& ray, 
+                                                 const Vec3fa& v00, const Vec3fa& v01, const Vec3fa& v02,
+                                                 const Vec3fa& v10, const Vec3fa& v11, const Vec3fa& v12,
+                                                 const Vec3fa& v20, const Vec3fa& v21, const Vec3fa& v22,
+                                                 const unsigned geomID, const unsigned primID)
+    {
+      const Vec3fa O = ray.org;
+      const Vec3fa D = ray.dir;
+      
+      const Vec3fa q00 = v00-O, q01 = v01-O, q02 = v02-O;
+      const Vec3fa q10 = v10-O, q11 = v11-O, q12 = v12-O;
+      const Vec3fa q20 = v20-O, q21 = v21-O, q22 = v22-O;
+
+      const Vec3fa e0001 = q00-q01; const float u0001 = dot(cross(q00,e0001),D);
+      const Vec3fa e0102 = q01-q02; const float u0102 = dot(cross(q01,e0102),D);
+      const Vec3fa e1011 = q10-q11; const float u1011 = dot(cross(q10,e1011),D);
+      const Vec3fa e1112 = q11-q12; const float u1112 = dot(cross(q11,e1112),D);
+      const Vec3fa e2021 = q20-q21; const float u2021 = dot(cross(q20,e2021),D);
+      const Vec3fa e2122 = q21-q22; const float u2122 = dot(cross(q21,e2122),D);
+      
+      const Vec3fa e0010 = q00-q10; const float u0010 = dot(cross(q00,e0010),D);
+      const Vec3fa e1020 = q10-q20; const float u1020 = dot(cross(q10,e1020),D);
+      const Vec3fa e0111 = q01-q11; const float u0111 = dot(cross(q01,e0111),D);
+      const Vec3fa e1121 = q11-q21; const float u1121 = dot(cross(q11,e1121),D);
+      const Vec3fa e0212 = q02-q12; const float u0212 = dot(cross(q02,e0212),D);
+      const Vec3fa e1222 = q12-q22; const float u1222 = dot(cross(q12,e1222),D);
+      
+      const Vec3fa e0011 = q00-q11; const float u0011 = dot(cross(q00,e0011),D);
+      const Vec3fa e0112 = q01-q12; const float u0112 = dot(cross(q01,e0112),D);
+      const Vec3fa e1021 = q10-q21; const float u1021 = dot(cross(q10,e1021),D);
+      const Vec3fa e1122 = q11-q22; const float u1122 = dot(cross(q11,e1122),D);
+
+      if (u0011 <= 0.0f) {
+        if (u0001 >= 0.0f && u0111 >= 0.0f) intersectFinish(ray,q00,+e0001,+e0111,-e0011,+u0001,+u0111,-u0011,geomID,primID);
+      } else {
+        if (u1011 <= 0.0f && u0010 <= 0.0f) intersectFinish(ray,q00,+e0011,-e1011,-e0010,+u0011,-u1011,-u0010,geomID,primID);
+      }
+      
+      if (u0112 <= 0.0f) {
+        if (u0102 >= 0.0f && u0212 >= 0.0f) intersectFinish(ray,q01,+e0102,+e0212,-e0112,+u0102,+u0212,-u0112,geomID,primID);
+      } else {
+        if (u1112 <= 0.0f && u0111 <= 0.0f) intersectFinish(ray,q01,+e0112,-e1112,-e0111,+u0112,-u1112,-u0111,geomID,primID);
+      }
+      
+      if (u1021 <= 0.0f) {
+        if (u1011 >= 0.0f && u1121 >= 0.0f) intersectFinish(ray,q10,+e1011,+e1121,-e1021,+u1011,+u1121,-u1021,geomID,primID);
+      } else {
+        if (u2021 <= 0.0f && u1020 <= 0.0f) intersectFinish(ray,q10,+e1021,-e2021,-e1020,+u1021,-u2021,-u1020,geomID,primID);
+      }
+      
+      if (u1122 <= 0.0f) {
+        if (u1112 >= 0.0f && u1222 >= 0.0f) intersectFinish(ray,q11,+e1112,+e1222,-e1122,+u1112,+u1222,-u1122,geomID,primID);
+      } else {
+        if (u2122 <= 0.0f && u1121 <= 0.0f) intersectFinish(ray,q11,+e1122,-e2122,-e1121,+u1122,-u2122,-u1121,geomID,primID);
+      }
     }
     
     /*! Intersect a ray with the triangle and updates the hit. */
@@ -141,14 +217,27 @@ namespace embree
       {
         const size_t x = 2*stack[i].x;
         const size_t y = 2*stack[i].y;
-        intersectTriangle(ray,prim.vertices(x+0,y+0),prim.vertices(x+1,y+0),prim.vertices(x+0,y+1),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+1,y+1),prim.vertices(x+0,y+1),prim.vertices(x+1,y+0),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+1,y+0),prim.vertices(x+2,y+0),prim.vertices(x+1,y+1),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+2,y+1),prim.vertices(x+1,y+1),prim.vertices(x+2,y+0),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+0,y+1),prim.vertices(x+1,y+1),prim.vertices(x+0,y+2),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+1,y+2),prim.vertices(x+0,y+2),prim.vertices(x+1,y+1),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+1,y+1),prim.vertices(x+2,y+1),prim.vertices(x+1,y+2),prim.geomID,prim.primID);
-        intersectTriangle(ray,prim.vertices(x+2,y+2),prim.vertices(x+1,y+2),prim.vertices(x+2,y+1),prim.geomID,prim.primID);
+        const Vec3fa& v00 = prim.vertices(x+0,y+0);
+        const Vec3fa& v10 = prim.vertices(x+1,y+0);
+        const Vec3fa& v20 = prim.vertices(x+2,y+0);
+        const Vec3fa& v01 = prim.vertices(x+0,y+1);
+        const Vec3fa& v11 = prim.vertices(x+1,y+1);
+        const Vec3fa& v21 = prim.vertices(x+2,y+1);
+        const Vec3fa& v02 = prim.vertices(x+0,y+2);
+        const Vec3fa& v12 = prim.vertices(x+1,y+2);
+        const Vec3fa& v22 = prim.vertices(x+2,y+2);
+#if 0
+        intersectTriangle(ray,v00,v11,v10,prim.geomID,prim.primID);
+        intersectTriangle(ray,v00,v01,v11,prim.geomID,prim.primID);
+        intersectTriangle(ray,v10,v21,v20,prim.geomID,prim.primID);
+        intersectTriangle(ray,v10,v11,v21,prim.geomID,prim.primID);
+        intersectTriangle(ray,v01,v12,v11,prim.geomID,prim.primID);
+        intersectTriangle(ray,v01,v02,v12,prim.geomID,prim.primID);
+        intersectTriangle(ray,v11,v22,v21,prim.geomID,prim.primID);
+        intersectTriangle(ray,v11,v12,v22,prim.geomID,prim.primID);
+#else
+        intersectQuadQuad(ray,v00,v01,v02,v10,v11,v12,v20,v21,v22,prim.geomID,prim.primID);
+#endif
       }
       ray_tfar = ray.tfar;
     }
