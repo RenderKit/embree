5a58fd4f878dc3cc7086683e46b48b9090bbb098
Sven Woop <sven.woop@intel.com>
2014-11-17 14:17:15 +0100

    disabling vertex_crease_weight test

--- kernels/common/scene_subdivision.h
+++ kernels/common/scene_subdivision.h
@@ -128,7 +128,7 @@ namespace embree
       vtx = (Vec3fa_t)vertices[ h->getStartVertexIndex() ];
       vertex_crease_weight = h->vertex_crease_weight;
 
-#if 1// test 
+#if 0 // FIXME: remove test 
       vertex_crease_weight = inf;
 #endif
 
