d638e6a8c88363212d427f79a81d6b311d7722f0
Sven Woop <sven.woop@intel.com>
2014-11-19 13:01:07 +0100

    moved parallel_for_for_prefix_sum to separate file

--- kernels/algorithms/parallel_for_for_prefix_sum.cpp
+++ kernels/algorithms/parallel_for_for_prefix_sum.cpp
@@ -14,13 +14,13 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#include "parallel_for_for.h"
+#include "parallel_for_for_prefix_sum.h"
 
 namespace embree
 {
-  struct parallel_for_for_regression_test : public RegressionTest
+  struct parallel_for_for_prefix_sum_regression_test : public RegressionTest
   {
-    parallel_for_for_regression_test(const char* name) : name(name) {
+    parallel_for_for_prefix_sum_regression_test(const char* name) : name(name) {
       registerRegressionTest(this);
     }
     
@@ -31,26 +31,47 @@ namespace embree
       fflush(stdout);
 
       /* create vector with random numbers */
-      size_t sum0 = 0;
-      const size_t M = 1000;
-      std::vector<std::unique_ptr<std::vector<size_t> > > array2(M);
+      const size_t M = 10;
+      std::vector<atomic_t> flattened;
+      typedef std::vector<std::unique_ptr<std::vector<size_t> > > ArrayArray;
+      ArrayArray array2(M);
       for (size_t i=0; i<M; i++) {
-        const size_t N = ::random() % 1024;
+        const size_t N = ::random() % 10;
         array2[i] = std::unique_ptr<std::vector<size_t> >(new std::vector<size_t>(N));
         for (size_t j=0; j<N; j++) 
-          sum0 += (*array2[i])[j] = ::random();
+          (*array2[i])[j] = ::random() % 10;
       }
-
-      /* add all numbers using parallel_for_for */
-      AtomicCounter sum1 = 0;
-      parallel_for_for( array2, size_t(1), [&](std::unique_ptr<std::vector<size_t> >& v, const range<size_t>& r) 
+  
+      ParallelForForPrefixSumState<ArrayArray> state(array2,size_t(1));
+  
+      /* dry run only counts */
+      parallel_for_for_prefix_sum( state, [&](std::unique_ptr<std::vector<size_t> >& v, const range<size_t>& r, const size_t base) 
       {
         size_t s = 0;
-	for (size_t i=r.begin(); i<r.end(); i++) 
-	  s += (*v)[i];
-        sum1 += s;
+	for (size_t i=r.begin(); i<r.end(); i++) s += (*v)[i];
+        return s;
       });
-      passed = sum0 == sum1;
+      
+      /* create properly sized output array */
+      flattened.resize(state.size());
+      memset(&flattened[0],0,sizeof(atomic_t)*flattened.size());
+
+      /* now we actually fill the flattened array */
+      parallel_for_for_prefix_sum( state, [&](std::unique_ptr<std::vector<size_t> >& v, const range<size_t>& r, const size_t base) 
+      {
+        size_t s = 0;
+	for (size_t i=r.begin(); i<r.end(); i++) {
+          for (size_t j=0; j<(*v)[i]; j++) {
+            atomic_add(&flattened[base+s+j],1);
+          }
+          s += (*v)[i];
+        }
+        return s;
+      });
+
+      /* check if each element was assigned exactly once */
+      for (size_t i=0; i<flattened.size(); i++)
+        passed &= (flattened[i] == 1);
       
       /* output if test passed or not */
       if (passed) printf("[passed]\n");
@@ -62,5 +83,5 @@ namespace embree
     const char* name;
   };
 
-  parallel_for_for_regression_test parallel_for_for_regression("parallel_for_for_regression_test");
+  parallel_for_for_prefix_sum_regression_test parallel_for_for_prefix_sum_regression("parallel_for_for_prefix_sum_regression_test");
 }
