4f420c3263dcefc4b565b26b92163a17e59d993b
Carsten Benthin <carsten.benthin@intel.com>
2014-11-30 22:46:15 +0100

    using 3x3 point (= 2x2 quads) grid stored as vertex strips as leaf primitive for cached tree

--- kernels/xeon/geometry/subdivpatch1cached_intersector1.cpp
+++ kernels/xeon/geometry/subdivpatch1cached_intersector1.cpp
@@ -23,6 +23,87 @@ namespace embree
 
 #define FORCE_TRIANGLE_UV 1
 
+  struct __aligned(64) Quad2x2
+  {
+    Quad2x2() {}
+
+    float vtx_x[12];
+    float vtx_y[12];
+    float vtx_z[12];
+    float vtx_u[12];
+    float vtx_v[12];
+    float dummy[3];
+
+    /*  v00 - v01 - v02 */
+    /*  v10 - v11 - v12 */
+    /*  v20 - v21 - v22 */
+
+    __forceinline void initFrom3x3Grid( const float *const source,
+                                        float *const dest,
+                                        const size_t offset_line0,
+                                        const size_t offset_line1,
+                                        const size_t offset_line2)
+    {
+      const float v00 = source[offset_line0 + 0];
+      const float v01 = source[offset_line0 + 1];
+      const float v02 = source[offset_line0 + 2];
+      const float v10 = source[offset_line1 + 0];
+      const float v11 = source[offset_line1 + 1];
+      const float v12 = source[offset_line1 + 2];
+      const float v20 = source[offset_line2 + 0];
+      const float v21 = source[offset_line2 + 1];
+      const float v22 = source[offset_line2 + 2];
+
+      /* v00 - v10 - v01 - v11 - v02 - v12 */
+      dest[ 0] = v00;
+      dest[ 1] = v10;
+      dest[ 2] = v01;
+      dest[ 3] = v11;
+      dest[ 4] = v02;
+      dest[ 5] = v12;
+      /* v10 - v20 - v11 - v21 - v12 - v22 */
+      dest[ 6] = v10;
+      dest[ 7] = v20;
+      dest[ 8] = v11;
+      dest[ 9] = v21;
+      dest[10] = v12;
+      dest[11] = v22;
+    }
+
+    /* init from 3x3 point grid */
+    Quad2x2( const float * const grid_x,
+             const float * const grid_y,
+             const float * const grid_z,
+             const float * const grid_u,
+             const float * const grid_v,
+             const size_t offset_line0,
+             const size_t offset_line1,
+             const size_t offset_line2)
+      {
+        initFrom3x3Grid( grid_x, vtx_x, offset_line0, offset_line1, offset_line2 );
+        initFrom3x3Grid( grid_y, vtx_y, offset_line0, offset_line1, offset_line2 );
+        initFrom3x3Grid( grid_z, vtx_z, offset_line0, offset_line1, offset_line2 );
+        initFrom3x3Grid( grid_u, vtx_u, offset_line0, offset_line1, offset_line2 );
+        initFrom3x3Grid( grid_v, vtx_v, offset_line0, offset_line1, offset_line2 );
+      }
+
+#if defined(__AVX__)
+
+    __forceinline avxf combine( const float *const source, const size_t offset ) const {
+      return avxf( *(ssef*)&source[0+offset], *(ssef*)&source[6+offset] );            
+    }
+
+    __forceinline avx3f getVtx( const size_t offset ) const {
+      return avx3f(  combine(vtx_x,offset), combine(vtx_y,offset), combine(vtx_z,offset) );
+    }
+
+    __forceinline avx2f getUV( const size_t offset ) const {
+      return avx2f(  combine(vtx_u,offset), combine(vtx_v,offset) );
+     }
+
+#endif
+
+  };
 
 #if defined(__AVX__)
   static __forceinline void intersect1_tri8_precise(Ray& ray,
@@ -73,7 +154,7 @@ namespace embree
     valid &= (T >= absDen*ray.tnear) & (absDen*ray.tfar >= T);
     if (unlikely(none(valid))) return;
       
-      /* perform backface culling */
+    /* perform backface culling */
 #if defined(RTCORE_BACKFACE_CULLING)
     valid &= den > avxf(zero);
     if (unlikely(none(valid))) return;
@@ -82,7 +163,7 @@ namespace embree
     if (unlikely(none(valid))) return;
 #endif
             
-      /* calculate hit information */
+    /* calculate hit information */
     const avxf rcpAbsDen = rcp(absDen);
     const avxf u = U*rcpAbsDen;
     const avxf v = V*rcpAbsDen;
@@ -141,6 +222,19 @@ namespace embree
     intersect1_tri8_precise(ray,v3,v1,v2,u,v,offset_v3,offset_v1,offset_v2,m_active,sptr,geom);
 
   }
+
+
+  static __forceinline void intersect1_quad8(Ray& ray,
+                                             const Quad2x2 &quad,
+                                             const SubdivPatch1Cached *const sptr,
+                                             const void* geom)
+  {
+    const avx3f v0 = quad.getVtx( 0 );
+    const avx3f v1 = quad.getVtx( 1 );
+    const avx3f v2 = quad.getVtx( 2 );
+    //intersect1_tri8_precise(ray,v0,v1,v2,u,v,offset_v0,offset_v1,offset_v3,m_active,sptr,geom);
+  }
+
 #endif
 
 
@@ -225,13 +319,13 @@ namespace embree
             
             avxb m_active ( true );
             if (unlikely(x + 8 > (grid_u_res-1))) 
-            {
-              //DBG_PRINT( (grid_u_res-1)%8 );
-              //DBG_PRINT( grid_u_res-1 );
+              {
+                //DBG_PRINT( (grid_u_res-1)%8 );
+                //DBG_PRINT( grid_u_res-1 );
               
-              for (size_t i=(grid_u_res-1)%8;i<8;i++)
-                m_active[i] = 0;
-            }
+                for (size_t i=(grid_u_res-1)%8;i<8;i++)
+                  m_active[i] = 0;
+              }
             //DBG_PRINT( m_active);
             intersect1_quad8(ray,vtx_x,vtx_y,vtx_z,
                              u_array,v_array,
