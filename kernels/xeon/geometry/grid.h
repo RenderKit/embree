4126fca791b4d0f8970d3ab733c0c0795bcb89e6
Sven Woop <sven.woop@intel.com>
2014-12-02 08:23:45 +0100

    bugfix in grid.h

--- kernels/xeon/geometry/grid.h
+++ kernels/xeon/geometry/grid.h
@@ -181,16 +181,16 @@ namespace embree
     {
       struct Quads
       {
-	enum struct Type : char { QUAD, QUADQUAD, NONE };
+	enum struct Type : char { QUAD1X1, QUAD1X2, QUAD2X1, QUAD2X2, NONE };
 	
 	__forceinline Quads () : type(Type::NONE), ofs(0) {}
 	__forceinline Quads (Type type, char ofs) : type(type), ofs(ofs) {}
 	
 	Type type;
-	char ofs;
+	unsigned char ofs;
       };
 
-      __forceinline const BBox3fa getQuadBounds(const size_t x0, const size_t y0) const 
+      __forceinline const BBox3fa getQuad1x1Bounds(const size_t x0, const size_t y0) const 
       {
 	BBox3fa bounds = empty;
 	bounds.extend(grid.point(x0+0,y0+0));
@@ -199,8 +199,32 @@ namespace embree
 	bounds.extend(grid.point(x0+1,y0+1));
 	return bounds;
       }
+      
+      __forceinline const BBox3fa getQuad1x2Bounds(const size_t x0, const size_t y0) const 
+      {
+	BBox3fa bounds = empty;
+	bounds.extend(grid.point(x0+0,y0+0));
+	bounds.extend(grid.point(x0+1,y0+0));
+	bounds.extend(grid.point(x0+0,y0+1));
+	bounds.extend(grid.point(x0+1,y0+1));
+	bounds.extend(grid.point(x0+0,y0+2));
+	bounds.extend(grid.point(x0+1,y0+2));
+	return bounds;
+      }
 
-      __forceinline const BBox3fa getQuadQuadBounds(const size_t x0, const size_t y0) const 
+      __forceinline const BBox3fa getQuad2x1Bounds(const size_t x0, const size_t y0) const 
+      {
+	BBox3fa bounds = empty;
+	bounds.extend(grid.point(x0+0,y0+0));
+	bounds.extend(grid.point(x0+1,y0+0));
+	bounds.extend(grid.point(x0+2,y0+0));
+	bounds.extend(grid.point(x0+0,y0+1));
+	bounds.extend(grid.point(x0+1,y0+1));
+	bounds.extend(grid.point(x0+2,y0+1));
+	return bounds;
+      }
+
+      __forceinline const BBox3fa getQuad2x2Bounds(const size_t x0, const size_t y0) const 
       {
 	BBox3fa bounds = empty;
 	bounds.extend(grid.point(x0+0,y0+0));
@@ -225,19 +249,29 @@ namespace embree
 	{
 	  for (size_t x=x0; x<x1; x+=2)
 	  {
-	    if (x+1 == x1 || y+1 == y1) {
-	      for (size_t ly=y; ly<min(y+2,y1); ly++) {
-		for (size_t lx=x; lx<min(x+2,x1); lx++) { 
-		  new (&quads[i]) Quads(Quads::Type::QUAD,ly*grid.width+lx);
-		  const BBox3fa b = getQuadBounds(lx,ly);
-		  bounds.set(i++,b);
-		  box.extend(b);
-		}
+	    const bool right = x+1 == x1;
+	    const bool bottom = y+1 == y1;
+	    if (right || bottom) {
+	      if (right && bottom) {
+		new (&quads[i]) Quads(Quads::Type::QUAD1X1,y*grid.width+x);
+		const BBox3fa b = getQuad1x1Bounds(x,y);
+		bounds.set(i++,b);
+		box.extend(b);
+	      } else if (right) {
+		new (&quads[i]) Quads(Quads::Type::QUAD1X2,y*grid.width+x);
+		const BBox3fa b = getQuad1x2Bounds(x,y);
+		bounds.set(i++,b);
+		box.extend(b);
+	      } else {
+		new (&quads[i]) Quads(Quads::Type::QUAD2X1,y*grid.width+x);
+		const BBox3fa b = getQuad2x1Bounds(x,y);
+		bounds.set(i++,b);
+		box.extend(b);
 	      }
-	    } 
+	    }
 	    else {
-	      new (&quads[i]) Quads(Quads::Type::QUADQUAD,y*grid.width+x);
-	      const BBox3fa b = getQuadQuadBounds(x,y);
+	      new (&quads[i]) Quads(Quads::Type::QUAD2X2,y*grid.width+x);
+	      const BBox3fa b = getQuad2x2Bounds(x,y);
 	      bounds.set(i++,b);
 	      box.extend(b);
 	    }
