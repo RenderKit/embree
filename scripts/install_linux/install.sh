83c38472bef495aadb385235168394033e20ae16
Sven Woop <sven.woop@intel.com>
2014-12-18 11:46:02 +0100

    updates to release scripts

--- scripts/install_linux/install.sh
+++ scripts/install_linux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Installing Embree v2.4 ... "
+echo "Installing Embree v2.4.0 ... "
 
 INCLUDE_INSTALL_DIR=/usr/local/include
 LIBRARY_INSTALL_DIR=/usr/local/lib
@@ -12,11 +12,11 @@ echo "  installing include files in" $INCLUDE_INSTALL_DIR
 cp -r include/embree2 $INCLUDE_INSTALL_DIR
 
 echo "  installing Xeon library files in" $LIBRARY_INSTALL_DIR
-cp -r lib/x64/libembree.so.2.4 $LIBRARY_INSTALL_DIR
-ln -sf $LIBRARY_INSTALL_DIR/libembree.so.2.4 $LIBRARY_INSTALL_DIR/libembree.so.2
+cp -r lib/x64/libembree.so.2.4.0 $LIBRARY_INSTALL_DIR
+ln -sf $LIBRARY_INSTALL_DIR/libembree.so.2.4.0 $LIBRARY_INSTALL_DIR/libembree.so.2
 ln -sf $LIBRARY_INSTALL_DIR/libembree.so.2     $LIBRARY_INSTALL_DIR/libembree.so
 
 echo "  installing Xeon Phi library files in" $LIBRARY_INSTALL_DIR
-cp -r lib/x64/libembree_xeonphi.so.2.4 $LIBRARY_INSTALL_DIR
-ln -sf $LIBRARY_INSTALL_DIR/libembree_xeonphi.so.2.4 $LIBRARY_INSTALL_DIR/libembree_xeonphi.so.2
+cp -r lib/x64/libembree_xeonphi.so.2.4.0 $LIBRARY_INSTALL_DIR
+ln -sf $LIBRARY_INSTALL_DIR/libembree_xeonphi.so.2.4.0 $LIBRARY_INSTALL_DIR/libembree_xeonphi.so.2
 ln -sf $LIBRARY_INSTALL_DIR/libembree_xeonphi.so.2     $LIBRARY_INSTALL_DIR/libembree_xeonphi.so
