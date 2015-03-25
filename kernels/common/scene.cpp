fd17ec567543b56a240c3eab6f393749e818dc2a
Sven Woop <sven.woop@intel.com>
2015-03-25 10:42:08 +0100

    enabled subdivcache for everything except (incoherent and static) scenes

--- kernels/common/scene.cpp
+++ kernels/common/scene.cpp
@@ -208,19 +208,10 @@ namespace embree
   {
     if (g_subdiv_accel == "default") 
     {
-     
-#if 1 //FIXME: remove
-      if (isIncoherent(flags)) {
-        if (isCompact()) accels.add(BVH4::BVH4SubdivGridLazy(this));
-        else             accels.add(BVH4::BVH4SubdivGridEager(this));
-      }
-      else {
-        //accels.add(BVH4::BVH4SubdivPatch1Cached(this)); // FIXME: enable, does not run through regression tests
+      if (isIncoherent(flags) && isStatic())
         accels.add(BVH4::BVH4SubdivGridEager(this));
-      }
-#else
-      accels.add(BVH4::BVH4SubdivPatch1Cached(this));
-#endif
+      else
+        accels.add(BVH4::BVH4SubdivPatch1Cached(this));
     }
     else if (g_subdiv_accel == "bvh4.subdivpatch1"      ) accels.add(BVH4::BVH4SubdivPatch1(this));
     else if (g_subdiv_accel == "bvh4.subdivpatch1cached") accels.add(BVH4::BVH4SubdivPatch1Cached(this));
