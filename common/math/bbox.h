68d8e0914d31c22ab750f4059844e1baf16fbd7c
Sven Woop <sven.woop@intel.com>
2015-04-07 09:44:28 +0200

    added area(Vec3f) function to bbox.h

--- common/math/bbox.h
+++ common/math/bbox.h
@@ -96,9 +96,14 @@ namespace embree
 
   /*! computes the surface area of a bounding box */
   template<typename T> __forceinline const T area( const BBox<Vec2<T> >& b ) { const Vec2<T> d = size(b); return d.x*d.y; }
+
+  template<typename T> __forceinline const T     area( const BBox<Vec3<T> >& b ) { const Vec3<T> d = size(b); return 2.0f*(d.x*(d.y+d.z)+d.y*d.z); }
+  template<typename T> __forceinline const T halfArea( const BBox<Vec3<T> >& b ) { const Vec3<T> d = size(b); return d.x*(d.y+d.z)+d.y*d.z; }
+
   __forceinline float     area( const BBox<Vec3fa>& b ) { const Vec3fa d = b.size(); return 2.0f*(d.x*(d.y+d.z)+d.y*d.z); }
-  __forceinline float safeArea( const BBox<Vec3fa>& b ) { if (b.empty()) return 0.0f; else return area(b); }
   __forceinline float halfArea( const BBox<Vec3fa>& b ) { const Vec3fa d = b.size(); return d.x*(d.y+d.z)+d.y*d.z; }
+
+  template<typename Vec> __forceinline float safeArea( const BBox<Vec>& b ) { if (b.empty()) return 0.0f; else return area(b); }
 
   /*! merges bounding boxes and points */
   template<typename T> __forceinline const BBox<T> merge( const BBox<T>& a, const       T& b ) { return BBox<T>(min(a.lower, b    ), max(a.upper, b    )); }
