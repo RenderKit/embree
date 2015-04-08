17a3ff89acb33e83255c609f3ff2dc1e31679279
Sven Woop <sven.woop@intel.com>
2015-04-08 10:38:44 +0200

    setting spp to 1 for tutorial06

--- tutorials/tutorial06/tutorial06_device.cpp
+++ tutorials/tutorial06/tutorial06_device.cpp
@@ -28,8 +28,8 @@
 #define TILE_SIZE_Y 4
 
 #define FIX_SAMPLING 0
-//#define SAMPLES_PER_PIXEL 1
-#define SAMPLES_PER_PIXEL 4
+#define SAMPLES_PER_PIXEL 1
+//#define SAMPLES_PER_PIXEL 4
 
 #define ENABLE_TEXTURING 1
 
