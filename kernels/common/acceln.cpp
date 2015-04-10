37694572d9ddbce86056d85362db5435649a39d3
Sven Woop <sven.woop@intel.com>
2015-04-10 09:38:38 +0200

    building different BVH types in parallel

--- kernels/common/acceln.cpp
+++ kernels/common/acceln.cpp
@@ -16,6 +16,7 @@
 
 #include "acceln.h"
 #include "embree2/rtcore_ray.h"
+#include "algorithms/parallel_for.h"
 
 namespace embree
 {
@@ -129,16 +130,17 @@ namespace embree
     for (size_t i=0; i<N; i++)
       accels[i]->immutable();
   }
-
+  
   void AccelN::build (size_t threadIndex, size_t threadCount) 
   {
-    /* build all acceleration structures */
+    /* build all acceleration structures in parallel */
+    parallel_for (N, [&] (size_t i) { 
+        accels[i]->build(threadIndex,threadCount);
+      });
+
+    /* create list of non-empty acceleration structures */
     M = 0;
-
-    for (size_t i=0; i<N; i++) 
-    {
-      accels[i]->build(threadIndex,threadCount);
-
+    for (size_t i=0; i<N; i++) {
       if (accels[i]->bounds.empty()) continue;
       validAccels[M++] = accels[i];
     }
