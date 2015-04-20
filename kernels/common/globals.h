b507e17d46968d3dcfe0865344e870b5ca0b300f
Sven Woop <sven.woop@intel.com>
2015-04-20 12:34:14 +0200

    added missing globals.h file

---
+++ kernels/common/globals.h
@@ -0,0 +1,33 @@
+// ======================================================================== //
+// Copyright 2009-2015 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#pragma once
+
+#include "common/default.h"
+
+namespace embree
+{
+#if defined(__SSE__)
+  extern ssef sse_coeff0[4];
+  extern ssef sse_coeff1[4];
+#endif
+
+#if defined(__AVX__)
+  extern avxf coeff0[4];
+  extern avxf coeff1[4];
+#endif
+}
+
