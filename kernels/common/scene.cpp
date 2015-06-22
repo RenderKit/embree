cbe7b7f2c2cea9d7c59836ec37c158496bfb29f3
Sven Woop <sven.woop@intel.com>
2015-06-22 10:52:35 +0200

    enabling task arena again

--- kernels/common/scene.cpp
+++ kernels/common/scene.cpp
@@ -27,6 +27,10 @@
 
 namespace embree
 {
+#if TASKING_TBB
+  tbb::task_arena arena;
+#endif
+
   /* error raising rtcIntersect and rtcOccluded functions */
   void missing_rtcCommit()     { throw_RTCError(RTC_INVALID_OPERATION,"scene got not committed"); }
   void invalid_rtcIntersect1() { throw_RTCError(RTC_INVALID_OPERATION,"rtcIntersect and rtcOccluded not enabled"); }
@@ -554,13 +558,13 @@ namespace embree
 
     /* join hierarchy build */
     if (!lock.isLocked()) {
-      //arena->execute([&]{ group->wait(); });
-      group->wait();
+      arena.execute([&]{ group->wait(); });
+      //group->wait();
       while (!buildMutex.try_lock()) {
         __pause_cpu();
         yield();
-        //arena->execute([&]{ group->wait(); });
-        group->wait();
+        arena.execute([&]{ group->wait(); });
+        //group->wait();
       }
       buildMutex.unlock();
       return;
@@ -586,13 +590,13 @@ namespace embree
       tbb::task_group_context ctx( tbb::task_group_context::isolated, tbb::task_group_context::default_traits | tbb::task_group_context::fp_settings );
       //ctx.set_priority(tbb::priority_high);
 
-      //arena->execute([&]{
+      arena.execute([&]{
           group->run([&]{
               tbb::parallel_for (size_t(0), size_t(1), [&] (size_t) { build_task(); }, ctx);
             });
           if (threadCount) group_barrier.wait(threadCount);
           group->wait();
-          //}); 
+        }); 
      
       /* reset MXCSR register again */
 #if !defined(__MIC__)
