a0b2cab9ba9786e0634d248d9fcbcdb677fb5d60
cbenthin <carsten.benthin@intel.com>
2015-06-25 03:17:29 -0700

    workaround fix for roadbike artifacts

--- kernels/common/subdiv/subdivpatch1base.cpp
+++ kernels/common/subdiv/subdivpatch1base.cpp
@@ -156,6 +156,12 @@ namespace embree
     grid_u_res = max(level[0],level[2])+1; // n segments -> n+1 points
     grid_v_res = max(level[1],level[3])+1;
     
+    /* workaround for 2x2 / 3x5 intersection stencil */
+#if !defined(__MIC__)    
+    grid_u_res = max(grid_u_res,3);
+    grid_v_res = max(grid_v_res,3);
+#endif
+
 #if defined(__MIC__)    
     grid_size_simd_blocks        = ((grid_u_res*grid_v_res+15)&(-16)) / 16;
     grid_subtree_size_64b_blocks = 5; // single leaf with u,v,x,y,z      
