243a763b78f252242a11b043fc18a033b13ffa03
Sven Woop <sven.woop@intel.com>
2015-07-02 07:08:11 +0200

    cleaups in quad2x2

--- kernels/xeon/geometry/quad2x2.h
+++ kernels/xeon/geometry/quad2x2.h
@@ -18,23 +18,21 @@
 
 #include "../../common/default.h"
 
+#define DISCRETIZED_UV 1
+
 namespace embree
 {
   /* 3x3 point grid => 2x2 quad grid */
-
-
   struct __aligned(64) Quad2x2
   {
-
     /*  v00 - v01 - v02 */
     /*  v10 - v11 - v12 */
     /*  v20 - v21 - v22 */
   
     /* v00 - v10 - v01 - v11 - v02 - v12 */
     /* v10 - v20 - v11 - v21 - v12 - v22 */
-   
-    Quad2x2() {}
 
+  public:
     float vtx_x[12];
     float vtx_y[12];
     float vtx_z[12];
@@ -45,16 +43,16 @@ namespace embree
     float vtx_u[12];
     float vtx_v[12];
 #endif
+
+  public:
+    Quad2x2() {}
     
     static __forceinline float u16_to_float(const unsigned short source) { return (float)source * (1.0f/65535.0f); } 
     static __forceinline unsigned short float_to_u16(const float f) { return (unsigned short)(f*65535.0f); }
     static __forceinline int4 float_to_u16(const float4 &f) { return (int4)(f*65535.0f); }
 
-    __forceinline void initFrom3x3Grid( const float *const source,
-                                        float *const dest,
-                                        const size_t offset_line0,
-                                        const size_t offset_line1,
-                                        const size_t offset_line2)
+    static __forceinline void initFrom3x3Grid( const float* const source, float* const dest,
+                                               const size_t offset_line0, const size_t offset_line1, const size_t offset_line2)
     {
       const float v00 = source[offset_line0 + 0];
       const float v01 = source[offset_line0 + 1];
@@ -65,7 +63,7 @@ namespace embree
       const float v20 = source[offset_line2 + 0];
       const float v21 = source[offset_line2 + 1];
       const float v22 = source[offset_line2 + 2];
-
+      
       /* v00 - v10 - v01 - v11 - v02 - v12 */
       dest[ 0] = v00;
       dest[ 1] = v10;
@@ -73,6 +71,7 @@ namespace embree
       dest[ 3] = v11;
       dest[ 4] = v02;
       dest[ 5] = v12;
+      
       /* v10 - v20 - v11 - v21 - v12 - v22 */
       dest[ 6] = v10;
       dest[ 7] = v20;
@@ -81,39 +80,33 @@ namespace embree
       dest[10] = v12;
       dest[11] = v22;
     }
-
-    __forceinline void initFrom3x3Grid( const float4 source[3], float *const dest)
+    
+    __forceinline void initFrom3x3Grid( const float4 source[3], float* const dest)
     {
       const float4 row0_a = unpacklo(source[0],source[1]); 
       const float4 row0_b = shuffle<0,1,0,1>(unpackhi(source[0],source[1]));
       const float4 row1_a = unpacklo(source[1],source[2]);
       const float4 row1_b = shuffle<0,1,0,1>(unpackhi(source[1],source[2]));
-
       storeu4f(&dest[2], row0_b);
       storeu4f(&dest[8], row1_b);
       storeu4f(&dest[0], row0_a);
       storeu4f(&dest[6], row1_a);
     }
-
-    __forceinline void initFrom3x3Grid_discretized( const float4 source[3], unsigned short *const dest)
+    
+    __forceinline void initFrom3x3Grid_discretized( const float4 source[3], unsigned short* const dest)
     {
       const float4 row0_a = unpacklo(source[0],source[1]); 
       const float4 row0_b = shuffle<0,1,0,1>(unpackhi(source[0],source[1]));
       const float4 row1_a = unpacklo(source[1],source[2]);
       const float4 row1_b = shuffle<0,1,0,1>(unpackhi(source[1],source[2]));
-
+      
       //FIXME: use intrinsics for conversion
-      for (size_t i=0;i<4;i++)
-        dest[2+i] = (unsigned short)(row0_b[i]*65535.0f);
-      for (size_t i=0;i<4;i++)
-        dest[8+i] = (unsigned short)(row1_b[i]*65535.0f);
-      for (size_t i=0;i<4;i++)
-        dest[0+i] = (unsigned short)(row0_a[i]*65535.0f);
-      for (size_t i=0;i<4;i++)
-        dest[6+i] = (unsigned short)(row1_a[i]*65535.0f);
-
+      for (size_t i=0;i<4;i++) dest[2+i] = (unsigned short)(row0_b[i]*65535.0f);
+      for (size_t i=0;i<4;i++) dest[8+i] = (unsigned short)(row1_b[i]*65535.0f);
+      for (size_t i=0;i<4;i++) dest[0+i] = (unsigned short)(row0_a[i]*65535.0f);
+      for (size_t i=0;i<4;i++) dest[6+i] = (unsigned short)(row1_a[i]*65535.0f);
     }
-
+    
     __forceinline void initFrom3x3Grid_discretized( const float *const source, unsigned short *const dest,
                                                     const size_t offset_line0, const size_t offset_line1, const size_t offset_line2)
     {
@@ -126,7 +119,7 @@ namespace embree
       const float v20 = source[offset_line2 + 0];
       const float v21 = source[offset_line2 + 1];
       const float v22 = source[offset_line2 + 2];
-
+      
       /* v00 - v10 - v01 - v11 - v02 - v12 */
       dest[ 0] = float_to_u16(v00);
       dest[ 1] = float_to_u16(v10);
@@ -134,6 +127,7 @@ namespace embree
       dest[ 3] = float_to_u16(v11);
       dest[ 4] = float_to_u16(v02);
       dest[ 5] = float_to_u16(v12);
+
       /* v10 - v20 - v11 - v21 - v12 - v22 */
       dest[ 6] = float_to_u16(v10);
       dest[ 7] = float_to_u16(v20);
@@ -142,7 +136,7 @@ namespace embree
       dest[10] = float_to_u16(v12);
       dest[11] = float_to_u16(v22);
     }
-
+    
     /* init from 3x3 point grid */
     void init( const float * const grid_x,
                const float * const grid_y,
@@ -164,7 +158,7 @@ namespace embree
       initFrom3x3Grid( grid_v, vtx_v, offset_line0, offset_line1, offset_line2 );
 #endif
     }
-
+    
     /* init from 3x3 point grid */
     void init( const float4 grid_x[3], const float4 grid_y[3], const float4 grid_z[3], const float4 grid_u[3], const float4 grid_v[3])
     {
@@ -179,7 +173,7 @@ namespace embree
       initFrom3x3Grid( grid_v, vtx_v );
 #endif
     }
-
+    
     /* init from 3x3 point grid */
     void init_xyz( const float4 grid_x[3], const float4 grid_y[3], const float4 grid_z[3])
     {
@@ -187,22 +181,22 @@ namespace embree
       initFrom3x3Grid( grid_y, vtx_y);
       initFrom3x3Grid( grid_z, vtx_z);
     }
-
-
+    
+    
 #if defined(__AVX__)
-
+    
     __forceinline float8 combine( const float *const source, const size_t offset) const {
       return float8( float4::loadu(&source[0+offset]), float4::loadu(&source[6+offset]) ); // FIXME: unaligned loads
     }
-
+    
     __forceinline int8 combine_discretized( const unsigned short *const source, const size_t offset) const {  
       return int8( int4::load(&source[0+offset]), int4::load(&source[6+offset]) );            
     }
-
+    
     __forceinline Vec3f8 getVtx( const size_t offset, const size_t delta = 0 ) const {
       return Vec3f8(  combine(vtx_x,offset), combine(vtx_y,offset), combine(vtx_z,offset) );
     }
-
+    
     __forceinline Vec2f8 getUV( const size_t offset, const size_t delta = 0 ) const {
 #if DISCRETIZED_UV == 1
       return Vec2f8(  float8(combine_discretized(vtx_u,offset)) * (1.0f/65535.0f), float8(combine_discretized(vtx_v,offset)) * (1.0f/65535.0f) )  ;
@@ -210,13 +204,13 @@ namespace embree
       return Vec2f8(  combine(vtx_u,offset), combine(vtx_v,offset) );
 #endif
     }
-
+    
 #else
-
+    
     __forceinline Vec3f4 getVtx( const size_t offset, const size_t delta = 0 ) const {
       return Vec3f4( loadu4f(&vtx_x[offset+delta]), loadu4f(&vtx_y[offset+delta]), loadu4f(&vtx_z[offset+delta]) );
     }
-
+    
     __forceinline Vec2f4 getUV( const size_t offset, const size_t delta = 0 ) const {
 #if DISCRETIZED_UV == 1
       return Vec2f4( float4::load(&vtx_u[offset+delta]), float4::load(&vtx_v[offset+delta]) );
@@ -226,7 +220,7 @@ namespace embree
     }
     
 #endif
-
+    
     
     __forceinline BBox3fa bounds() const 
     {
@@ -239,13 +233,13 @@ namespace embree
     __forceinline Vec3fa getVec3fa_xyz(const size_t i) const {
       return Vec3fa( vtx_x[i], vtx_y[i], vtx_z[i] );
     }
-
+    
     __forceinline Vec2f getVec2f_uv(const size_t i) const {
       return Vec2f( vtx_u[i], vtx_v[i] );
     }
-
+    
   };
-
+  
   inline std::ostream& operator<<(std::ostream& cout, const Quad2x2& qquad) {
     for (size_t i=0;i<12;i++)
       cout << "i = " << i << " -> xyz = " << qquad.getVec3fa_xyz(i) << " uv = " << qquad.getVec2f_uv(i) << std::endl;
