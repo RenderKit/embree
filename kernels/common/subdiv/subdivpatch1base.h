eab112722582e977077b2e6ed836c7a807ef0f9d
Sven Woop <sven.woop@intel.com>
2015-07-02 08:00:20 +0200

    moved gridrange to separate file

--- kernels/common/subdiv/subdivpatch1base.h
+++ kernels/common/subdiv/subdivpatch1base.h
@@ -24,6 +24,7 @@
 #include "gregory_triangle_patch.h"
 #include "tessellation.h"
 #include "tessellation_cache.h"
+#include "gridrange.h"
 
 #define FORCE_TESSELLATION_BOUNDS 1
 #define USE_DISPLACEMENT_FOR_TESSELLATION_BOUNDS 1
@@ -40,99 +41,7 @@
 
 namespace embree
 {
-  struct __aligned(16) GridRange
-  {
-    unsigned int u_start;
-    unsigned int u_end;
-    unsigned int v_start;
-    unsigned int v_end;
-
-    __forceinline GridRange() {}
-
-#if defined(__MIC__)    
-    __forceinline void operator=(const GridRange& v) {
-      store4f((float*)this,broadcast4to16f((float*)&v));
-    };
-#endif
-
-    __forceinline GridRange(unsigned int u_start, unsigned int u_end, unsigned int v_start, unsigned int v_end) 
-      : u_start(u_start), u_end(u_end), v_start(v_start), v_end(v_end) {}
-
-    __forceinline bool hasLeafSize() const
-    {
-#if defined(__MIC__)
-      return u_end-u_start <= 1 && v_end-v_start <= 1; // FIXME: what does this ???
-#else
-      const unsigned int u_size = u_end-u_start+1;
-      const unsigned int v_size = v_end-v_start+1;
-      assert(u_size >= 1);
-      assert(v_size >= 1);
-      return u_size <= 3 && v_size <= 3;
-#endif
-    }
-
-    static __forceinline unsigned int split(unsigned int start,unsigned int end)
-    {
-      const unsigned int center = (start+end)/2;
-#if defined(__MIC__)
-      assert(center<end);
-#else
-      assert (center > start);
-      assert (center < end);
-#endif
-      return center;
-    }
-
-    __forceinline void split(GridRange& r0, GridRange& r1) const
-    {
-      assert( hasLeafSize() == false );
-      const unsigned int u_size = u_end-u_start+1;
-      const unsigned int v_size = v_end-v_start+1;
-      r0 = *this;
-      r1 = *this;
-
-      if (u_size >= v_size)
-      {
-        const unsigned int u_mid = split(u_start,u_end);
-        r0.u_end   = u_mid;
-        r1.u_start = u_mid;
-      }
-      else
-      {
-        const unsigned int v_mid = split(v_start,v_end);
-        r0.v_end   = v_mid;
-        r1.v_start = v_mid;
-      }
-    }
-
-    __forceinline unsigned int splitIntoSubRanges(GridRange r[4]) const
-    {
-      assert( !hasLeafSize() );
-      size_t children = 0;
-      GridRange first,second;
-      split(first,second);
-
-      if (first.hasLeafSize()) {
-        r[0] = first;
-        children++;
-      } 
-      else {
-        first.split(r[0],r[1]);
-        children += 2;
-      }
-
-      if (second.hasLeafSize())	{
-        r[children] = second;
-        children++;
-      }
-      else {
-        second.split(r[children+0],r[children+1]);
-        children += 2;
-      }
-      return children;      
-    }
-  };
-
+  
   struct __aligned(64) SubdivPatch1Base
   {
   public:
