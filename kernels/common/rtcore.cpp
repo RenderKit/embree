29e2c02abb0ece8024897e9a367f9215d331f2ed
Sven Woop <sven.woop@intel.com>
2015-09-16 13:10:32 +0200

    compile fix for clang

--- kernels/common/rtcore.cpp
+++ kernels/common/rtcore.cpp
@@ -236,8 +236,7 @@ namespace embree
     /* for best performance set FTZ and DAZ flags in the MXCSR control and status register */
 #if !defined(__MIC__)
     unsigned int mxcsr = _mm_getcsr();
-    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+    _mm_setcsr(mxcsr | /* FTZ */ (1<<15) | /* DAZ */ (1<<6));
 #endif
     
      /* perform scene build */
