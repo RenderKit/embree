5d7cd9c43f3f779b83aa468cef8979d5d4ac7975
Sven Woop <sven.woop@intel.com>
2015-09-16 14:43:15 +0200

    compile fixes for windows

--- kernels/xeon/bvh8/bvh8_intersector8_hybrid.cpp
+++ kernels/xeon/bvh8/bvh8_intersector8_hybrid.cpp
@@ -135,7 +135,7 @@ namespace embree
           }
 
           /*! use 8-wide sorting network in the AVX2 */
-#if defined(__AVX2__) && 1
+#if defined(__AVX2__) && 0
 
           const bool8 mask8 = !vmask;
           const size_t hits = __popcnt(movemask(mask8));
