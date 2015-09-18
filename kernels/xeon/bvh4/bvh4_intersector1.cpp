75ba6cfaaa235097badefde4b7a20e708605c1fb
Sven Woop <sven.woop@intel.com>
2015-09-18 12:23:21 +0200

    removed xfm cache again

--- kernels/xeon/bvh4/bvh4_intersector1.cpp
+++ kernels/xeon/bvh4/bvh4_intersector1.cpp
@@ -97,10 +97,6 @@ namespace embree
       float4 ray_near(ray.tnear);
       float4 ray_far (ray.tfar);
 
-      unsigned int cacheSlot = 0;
-      __aligned(16) int4 cache_tag = -1;
-      __aligned(32) TravRay cache_entry[4];
-
       /* pop loop */
       while (true) pop:
       {
@@ -199,23 +195,11 @@ namespace embree
         {
           //STAT3(normal.transform_nodes,1,1,1);
           const BVH4::TransformNode* node = cur.transformNode();
-          bool4 h = cache_tag == int4(node->xfmID);
-          if (likely(any(h))) {
-            const int slot = __bsf(movemask(h));
-            vray = cache_entry[slot];
-            ray.org = vray.org_xyz;
-            ray.dir = vray.dir_xyz;
-          } 
-          else {
-            const Vec3fa ray_org = xfmPoint (node->world2local,((TravRay&)tlray).org_xyz);
-            const Vec3fa ray_dir = xfmVector(node->world2local,((TravRay&)tlray).dir_xyz);
-            new (&vray) TravRay(ray_org,ray_dir);
-            cache_tag[cacheSlot%4] = node->xfmID;
-            cache_entry[cacheSlot%4] = vray;
-            cacheSlot++;
-            ray.org = ray_org;
-            ray.dir = ray_dir;
-          }
+          const Vec3fa ray_org = xfmPoint (node->world2local,((TravRay&)tlray).org_xyz);
+          const Vec3fa ray_dir = xfmVector(node->world2local,((TravRay&)tlray).dir_xyz);
+          new (&vray) TravRay(ray_org,ray_dir);
+          ray.org = ray_org;
+          ray.dir = ray_dir;
           ((TravRay&)tlray).geomID = ray.geomID; ray.geomID = -1;
           ((TravRay&)tlray).instID = ray.instID; ray.instID = node->instID;
           
