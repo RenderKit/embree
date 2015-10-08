e62c8588fa69308cafdeedaa1330eb668fea2bd6
Sven Woop <sven.woop@intel.com>
2015-10-08 11:47:25 +0200

    adding script

---
+++ scripts/benchmark_instancing2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for scene in ~/models/embree/instancing/instancing_tree.ecs  ~/models/embree/instancing/instancing_barbarian.ecs ~/models/embree/instancing/instancing_barbarian_mblur.ecs ~/models/embree/instancing/instancing002.ecs ~/models/embree/instancing/instancing003.ecs ~/models/rivl/xfrog.ecs ~/models/embree/instancing/instancing000.ecs ~/models/embree/instancing/instancing001.ecs;
+do
+  echo $scene
+  ./pathtracer -c $scene -instancing scene -benchmark 4 4 $* > scene_inst.log
+  FPS=`sed scene_inst.log -n -e "s/BENCHMARK_RENDER \(.*\)/\1/p"`
+  echo "  scene_instancing   : FPS=$FPS"
+
+  ./pathtracer -c $scene -instancing geometry -benchmark 4 4 $* > geom_inst.log
+  FPS=`sed geom_inst.log -n -e "s/BENCHMARK_RENDER \(.*\)/\1/p"`
+  PRIMS=`sed geom_inst.log -n -e "s/BENCHMARK_INSTANCED_PRIMITIVES \(.*\)/\1/p"`
+  INSTS=`sed geom_inst.log -n -e "s/[\[\.]*BENCHMARK_INSTANCES \(.*\)/\1/p"`
+  OINSTS=`sed geom_inst.log -n -e "s/BENCHMARK_OPENED_INSTANCES \(.*\)/\1/p"`
+  echo "  geometry_instancing: FPS=$FPS, INSTS=$INSTS, OINSTS=$OINSTS, PRIMS=$PRIMS" 
+done
