fe39c5f8313641421f7c9d64c2d2164b3428f674
Attila T. Áfra <attila.t.afra@intel.com>
2015-11-25 17:56:42 +0200

    line segment intersector uses epilogs

--- kernels/xeon/geometry/line_intersector.h
+++ kernels/xeon/geometry/line_intersector.h
@@ -25,6 +25,25 @@ namespace embree
   namespace isa
   {
     template<int M>
+    struct LineIntersectorHitM
+    {
+      __forceinline LineIntersectorHitM(const vfloat<M>& u, const vfloat<M>& v, const vfloat<M>& t, const Vec3<vfloat<M>>& Ng)
+        : vu(u), vv(v), vt(t), vNg(Ng) {}
+
+      __forceinline void finalize() {}
+
+      __forceinline Vec2f uv (const size_t i) const { return Vec2f(vu[i],vv[i]); }
+      __forceinline float t  (const size_t i) const { return vt[i]; }
+      __forceinline Vec3fa Ng(const size_t i) const { return Vec3fa(vNg.x[i],vNg.y[i],vNg.z[i]); }
+
+    public:
+      vfloat<M> vu;
+      vfloat<M> vv;
+      vfloat<M> vt;
+      Vec3<vfloat<M>> vNg;
+    };
+
+    template<int M>
     struct LineIntersector1
     {
       typedef Vec3<vfloat<M>> Vec3vfM;
@@ -32,24 +51,25 @@ namespace embree
 
       struct Precalculations
       {
-        __forceinline Precalculations (const Ray& ray, const void *ptr)
-          : depth_scale(rsqrt(dot(ray.dir,ray.dir))), ray_space(frame(depth_scale*ray.dir).transposed()) {}
+        __forceinline Precalculations (const Ray& ray, const void* ptr)
+        {
+          const float s = rsqrt(dot(ray.dir,ray.dir));
+          depth_scale = s;
+          ray_space = frame(s*ray.dir).transposed();
+        }
 
-        float depth_scale;
+        vfloat<M> depth_scale;
         LinearSpace3<Vec3vfM> ray_space;
       };
 
-      static __forceinline void intersect(Ray& ray, const Precalculations& pre,
-                                          const Vec4vfM& v0, const Vec4vfM& v1, const vint<M>& geomID, const vint<M>& primID,
-                                          Scene* scene, const unsigned* geomID_to_instID)
+      template<typename Epilog>
+      static __forceinline bool intersect(Ray& ray, const Precalculations& pre,
+                                          const Vec4vfM& v0, const Vec4vfM& v1,
+                                          const Epilog& epilog)
       {
-        /* transform control points into ray space */
-        STAT3(normal.trav_prims,1,1,1);
-        Vec3vfM w0 = xfmVector(pre.ray_space,Vec3vfM(v0.x,v0.y,v0.z)-Vec3vfM(ray.org));
-        Vec3vfM w1 = xfmVector(pre.ray_space,Vec3vfM(v1.x,v1.y,v1.z)-Vec3vfM(ray.org));
-
-        Vec4vfM p0(w0.x, w0.y, w0.z, v0.w);
-        Vec4vfM p1(w1.x, w1.y, w1.z, v1.w);
+        /* transform end points into ray space */
+        Vec4vfM p0(xfmVector(pre.ray_space,v0.xyz()-Vec3vfM(ray.org)), v0.w);
+        Vec4vfM p1(xfmVector(pre.ray_space,v1.xyz()-Vec3vfM(ray.org)), v1.w);
 
         /* approximative intersection with cone */
         const Vec4vfM v = p1-p0;
@@ -63,55 +83,16 @@ namespace embree
         const vfloat<M> r = p.w;
         const vfloat<M> r2 = r*r;
         vbool<M> valid = d2 <= r2 & vfloat<M>(ray.tnear) < t & t < vfloat<M>(ray.tfar);
+        if (unlikely(none(valid))) return false;
 
-        //int instID = geomID_to_instID ? geomID_to_instID[0] : geomID;
-      retry:
-        if (unlikely(none(valid))) return;
-        STAT3(normal.trav_prim_hits,1,1,1);
-        size_t i = select_min(valid,t);
+        /* ignore denormalized segments */
+        const Vec3vfM T = v1.xyz()-v0.xyz();
+        valid &= T.x != vfloat<M>(zero) | T.y != vfloat<M>(zero) | T.z != vfloat<M>(zero);
+        if (unlikely(none(valid))) return false;
 
-        /* ray masking test */
-#if defined(RTCORE_RAY_MASK)
-        BezierCurves* g = scene->getBezierCurves(geomID);
-        if (unlikely(g->mask & ray.mask) == 0) return;
-#endif
-
-        /* intersection filter test */
-        /*
-#if defined(RTCORE_INTERSECTION_FILTER)
-        Geometry* geometry = scene->get(geomID);
-        if (!likely(geometry->hasIntersectionFilter1()))
-        {
-#endif
-*/
-          /* update hit information */
-          Vec3fa T = Vec3fa(v1.x[i], v1.y[i], v1.z[i]) - Vec3fa(v0.x[i], v0.y[i], v0.z[i]);
-          if (T == Vec3fa(zero)) { valid[i] = 0; goto retry; } // ignore denormalized curves
-          ray.u = u[i];
-          ray.v = 0.0f;
-          ray.tfar = t[i];
-          ray.Ng = T;
-          ray.geomID = geomID[i];
-          ray.primID = primID[i];
-          /*
-#if defined(RTCORE_INTERSECTION_FILTER)
-          return;
-        }
-
-        while (true)
-        {
-          const float uu = (float(i)+u[i])*one_over_width;
-          const BezierCurve3fa curve3D(v0,v1,v2,v3,0.0f,1.0f,0);
-          Vec3fa P,T; curve3D.eval(uu,P,T);
-          if (T != Vec3fa(zero))
-            if (runIntersectionFilter1(geometry,ray,uu,0.0f,t[i],T,instID,primID)) return;
-          valid[i] = 0;
-          if (none(valid)) return;
-          i = select_min(valid,t);
-          STAT3(normal.trav_prim_hits,1,1,1);
-        }
-#endif
-        */
+        /* update hit information */
+        LineIntersectorHitM<M> hit(u,zero,t,T);
+        return epilog(valid,hit);
       }
     };
   }
--- kernels/xeon/geometry/linei_intersector.h
+++ kernels/xeon/geometry/linei_intersector.h
@@ -18,12 +18,13 @@
 
 #include "linei.h"
 #include "line_intersector.h"
+#include "intersector_epilog.h"
 
 namespace embree
 {
   namespace isa
   {
-    template<int M, int Mx>
+    template<int M, int Mx, bool filter>
     struct Line4iIntersector1
     {
       typedef Line4i Primitive;
@@ -31,17 +32,16 @@ namespace embree
 
       static __forceinline void intersect(Precalculations& pre, Ray& ray, const Primitive& line, Scene* scene, const unsigned* geomID_to_instID)
       {
-        //STAT3(normal.trav_prims,1,1,1);
-        Vec4vf4 v0, v1; line.gather(v0,v1);
-        LineIntersector1<Mx>::intersect(ray,pre,v0,v1,line.geomID(),line.primID(),scene,geomID_to_instID);
+        STAT3(normal.trav_prims,1,1,1);
+        Vec4vf4 v0,v1; line.gather(v0,v1);
+        LineIntersector1<Mx>::intersect(ray,pre,v0,v1,Intersect1Epilog<M,Mx,filter>(ray,line.geomIDs,line.primIDs,scene,geomID_to_instID));
       }
 
       static __forceinline bool occluded(Precalculations& pre, Ray& ray, const Primitive& line, Scene* scene, const unsigned* geomID_to_instID)
       {
-        //STAT3(shadow.trav_prims,1,1,1);
-        Vec4vf4 v0, v1; line.gather(v0,v1);
-        //return LineIntersector1<Mx>::intersect(ray,pre,v0,v1,line.geomID(),line.primID(),scene,geomID_to_instID);
-        return false;
+        STAT3(shadow.trav_prims,1,1,1);
+        Vec4vf4 v0,v1; line.gather(v0,v1);
+        return LineIntersector1<Mx>::intersect(ray,pre,v0,v1,Occluded1Epilog<M,Mx,filter>(ray,line.geomIDs,line.primIDs,scene,geomID_to_instID));
       }
     };
   }
