c8c5b72bc21e139124c6f115f604475bd001fdd5
Sven Woop <sven.woop@intel.com>
2016-01-07 10:34:51 +0100

    first geometric hair intersection with sphere

--- kernels/xeon/geometry/line_intersector.h
+++ kernels/xeon/geometry/line_intersector.h
@@ -60,13 +60,96 @@ namespace embree
           vfloat<M> depth_scale;
           LinearSpace3<Vec3vfM> ray_space;
         };
+
+        static __forceinline vbool<M> intersect_sphere(vbool<M> valid, 
+                                                       Ray& ray, const Vec4vfM& v0,
+                                                       vfloat<M>& t_o,
+                                                       Vec3<vfloat<M>>& Ng_o)
+        {
+          Vec3<vfloat<M>> P0 = Vec3<vfloat<M>>(v0.x,v0.y,v0.z);
+          vfloat<M> r0 = v0.w;
+
+          const Vec3<vfloat<M>> O = Vec3<vfloat<M>>(ray.org)-P0, dO = ray.dir;
+          
+          const vfloat<M> A = dot(dO,dO);
+          const vfloat<M> B = 2.0f * dot(dO,O);
+          const vfloat<M> C = dot(O,O) - sqr(r0);
+          
+          const vfloat<M> D = B*B - 4.0f*A*C;
+          valid &= D >= 0.0f;
+          if (none(valid)) return false;
+          
+          const vfloat<M> Q = sqrt(D);
+          //const vfloat<M> t0 = (-B-Q)*rcp2A;
+          //const vfloat<M> t1 = (-B+Q)*rcp2A;
+          const vfloat<M> t0 = (-B-Q)/(2.0f*A);
+          const vfloat<M> t = t0;
+          valid &= (ray.tnear < t) & (t < ray.tfar);
+          if (unlikely(none(valid))) return false;
+          
+          /* update hit information */
+          const Vec3<vfloat<M>> Pr = Vec3<vfloat<M>>(ray.org) + t*Vec3<vfloat<M>>(ray.dir);
+          t_o = t;
+          Ng_o = Pr-P0;
+          return valid;
+        }
+
+        static __forceinline vbool<M> intersect_cone(vbool<M> valid, 
+                                                     Ray& ray, const Vec4vfM& v0, const Vec4vfM& v1,
+                                                     vfloat<M>& t_o,
+                                                     vfloat<M>& u_o,
+                                                     Vec3<vfloat<M>>& Ng_o)
+        {
+          Vec4<vfloat<M>> a = v0;
+          Vec4<vfloat<M>> b = v1;
+          Vec3<vfloat<M>> a3(v0.x,v0.y,v0.z);
+          Vec3<vfloat<M>> b3(v1.x,v1.y,v1.z);
+
+          const vfloat<M> rl = rcp_length(b3-a3);
+          const Vec3<vfloat<M>> P0 = a3, dP = (b3-a3)*rl;
+          const vfloat<M> r0 = a.w, dr = (b.w-a.w)*rl;
+          const Vec3<vfloat<M>> O = Vec3<vfloat<M>>(ray.org)-P0, dO = ray.dir;
+          
+          const vfloat<M> dOdO = dot(dO,dO);
+          const vfloat<M> OdO = dot(dO,O);
+          const vfloat<M> OO = dot(O,O);
+          const vfloat<M> dOz = dot(dP,dO);
+          const vfloat<M> Oz = dot(dP,O);
+
+          const vfloat<M> R = r0 + Oz*dr;          
+          const vfloat<M> A = dOdO - sqr(dOz) * (1.0f+sqr(dr));
+          const vfloat<M> B = 2.0f * (OdO - dOz*(Oz + R*dr));
+          const vfloat<M> C = OO - (sqr(Oz) + sqr(R));
+          
+          const vfloat<M> D = B*B - 4.0f*A*C;
+          valid &= D >= 0.0f;
+          if (none(valid)) return false;
+          
+          const vfloat<M> Q = sqrt(D);
+          //const vfloat<M> t0 = (-B-Q)*rcp2A;
+          //const vfloat<M> t1 = (-B+Q)*rcp2A;
+          const vfloat<M> t0 = (-B-Q)/(2.0f*A);
+          const vfloat<M> u0 = Oz+t0*dOz;
+          const vfloat<M> t = t0;
+          const vfloat<M> u = u0*rl;
+          valid &= (ray.tnear < t) & (t < ray.tfar) & (0.0f <= u) & (u <= 1.0f);
+          if (unlikely(none(valid))) return false;
+          
+          /* update hit information */
+          const Vec3<vfloat<M>> Pr = Vec3<vfloat<M>>(ray.org) + t*Vec3<vfloat<M>>(ray.dir);
+          const Vec3<vfloat<M>> Pl = a3 + u*(b3-a3);
+          t_o = t;
+          u_o = u;
+          Ng_o = Pr-Pl;
+          return valid;
+        }
         
         template<typename Epilog>
         static __forceinline bool intersect(Ray& ray, const Precalculations& pre,
                                             const vbool<M>& valid_i, const Vec4vfM& v0, const Vec4vfM& v1,
                                             const Epilog& epilog)
         {
-#if 0    
+#if 0
           /* transform end points into ray space */
           Vec4vfM p0(xfmVector(pre.ray_space,v0.xyz()-Vec3vfM(ray.org)), v0.w);
           Vec4vfM p1(xfmVector(pre.ray_space,v1.xyz()-Vec3vfM(ray.org)), v1.w);
@@ -94,46 +177,40 @@ namespace embree
           LineIntersectorHitM<M> hit(u,zero,t,T);
           return epilog(valid,hit);
 #else
+          vfloat<M> t0 = inf,u0; Vec3<vfloat<M>> Ng0; vbool<M> valid0 = intersect_cone(valid_i,ray,v0,v1,t0,u0,Ng0);
+          vfloat<M> tl,ul; Vec3<vfloat<M>> Ngl; vbool<M> validl = intersect_sphere(valid_i,ray,v0,tl,Ngl); ul = 0.0f;
+          vfloat<M> tr,ur; Vec3<vfloat<M>> Ngr; vbool<M> validr = intersect_sphere(valid_i,ray,v1,tr,Ngr); ur = 1.0f;
 
-          Vec4<vfloat<M>> a = v0;
-          Vec4<vfloat<M>> b = v1;
-          Vec3<vfloat<M>> a3(v0.x,v0.y,v0.z);
-          Vec3<vfloat<M>> b3(v1.x,v1.y,v1.z);
+          //if (none(validr)) return false;
+          //LineIntersectorHitM<M> hitr(ur,zero,tr,Ngr);
+          //return epilog(validr,hitr);
 
-          const vfloat<M> rl = rcp_length(b3-a3);
-          const Vec3<vfloat<M>> P0 = a3, dP = (b3-a3)*rl;
-          const vfloat<M> r0 = a.w, dr = (b.w-a.w)*rl;
-          const Vec3<vfloat<M>> O = Vec3<vfloat<M>>(ray.org)-P0, dO = ray.dir;
-          
-          const vfloat<M> dOdO = dot(dO,dO);
-          const vfloat<M> OdO = dot(dO,O);
-          const vfloat<M> OO = dot(O,O);
-          const vfloat<M> dOz = dot(dP,dO);
-          const vfloat<M> Oz = dot(dP,O);
+          vbool<M> valid;
+          vfloat<M> t,u; Vec3<vfloat<M>> Ng;
+          valid = valid0; t = t0; u = u0; Ng = Ng0;
 
-          const vfloat<M> R = r0 + Oz*dr;          
-          const vfloat<M> A = dOdO - sqr(dOz) * (1.0f+sqr(dr));
-          const vfloat<M> B = 2.0f * (OdO - dOz*(Oz + R*dr));
-          const vfloat<M> C = OO - (sqr(Oz) + sqr(R));
-          
-          const vfloat<M> D = B*B - 4.0f*A*C;
-          vbool<M> valid = D >= 0.0f;
+          vbool<M> va = !valid0 & validl & (tl < t);
+          t = select(va, tl, t);
+          vbool<M> vb = !valid0 & validr & (tr < t);
+          t = select(vb, tr, t);
+
+          u = select(va, ul, u);
+          u = select(vb, ur, u);
+
+          Ng.x = select(va, Ngl.x, Ng.x);
+          Ng.x = select(vb, Ngr.x, Ng.x);
+
+          Ng.y = select(va, Ngl.y, Ng.y);
+          Ng.y = select(vb, Ngr.y, Ng.y);
+
+          Ng.z = select(va, Ngl.z, Ng.z);
+          Ng.z = select(vb, Ngr.z, Ng.z);
+
+          valid = select(va, validl, valid);
+          valid = select(vb, validr, valid);
           if (none(valid)) return false;
           
-          const vfloat<M> Q = sqrt(D);
-          //const vfloat<M> t0 = (-B-Q)*rcp2A;
-          //const vfloat<M> t1 = (-B+Q)*rcp2A;
-          const vfloat<M> t0 = (-B-Q)/(2.0f*A);
-          const vfloat<M> u0 = Oz+t0*dOz;
-          const vfloat<M> t = t0;
-          const vfloat<M> u = u0*rl;
-          valid &= (ray.tnear < t) & (t < ray.tfar) & (0.0f <= u) & (u <= 1.0f);
-          if (unlikely(none(valid))) return false;
-          
-          /* update hit information */
-          const Vec3<vfloat<M>> Pr = Vec3<vfloat<M>>(ray.org) + t*Vec3<vfloat<M>>(ray.dir);
-          const Vec3<vfloat<M>> Pl = a3 + u*(b3-a3);
-          LineIntersectorHitM<M> hit(u,zero,t,Pr-Pl);
+          LineIntersectorHitM<M> hit(u,zero,t,Ng);
           return epilog(valid,hit);
 
 #endif
