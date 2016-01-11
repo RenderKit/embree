1049c76c7417cbc9795c5da40db17c45fb132ea0
Sven Woop <sven.woop@intel.com>
2016-01-11 08:06:11 +0100

    properly using sphere to bound capped cylinders

--- kernels/xeon/geometry/line_intersector.h
+++ kernels/xeon/geometry/line_intersector.h
@@ -132,7 +132,7 @@ namespace embree
           const vfloat<M> u0 = Oz+t0*dOz;
           const vfloat<M> t = t0;
           const vfloat<M> u = u0*rl;
-          //valid &= /*(ray.tnear < t) & (t < ray.tfar) &*/ (0.0f <= u) & (u <= 1.0f); // FIXME: should not do uv test
+          valid &= (ray.tnear < t) & (t < ray.tfar);// & (0.0f <= u) & (u <= 1.0f); 
           if (unlikely(none(valid))) return false;
           
           /* update hit information */
@@ -193,33 +193,51 @@ namespace embree
           Vec3<vfloat<M>> q1(v1.x,v1.y,v1.z);
           Vec3<vfloat<M>> q2(v2.x,v2.y,v2.z);
           Vec3<vfloat<M>> q3(v3.x,v3.y,v3.z);
-          
-          auto tpl0 = intersect_half_plane(ray,q2-q0,q1);
-          auto tpl1 = intersect_half_plane(ray,q1-q2,q1);
 
-          auto tpr0 = intersect_half_plane(ray,q1-q3,q2);
-          auto tpr1 = intersect_half_plane(ray,q2-q1,q2);
+          auto Hl = normalize_safe(q1-q0) + normalize_safe(q2-q1);
+          auto Hr = normalize_safe(q1-q2) + normalize_safe(q2-q3);
+          
+          //auto tpl0 = intersect_half_plane(ray,-Hl,q1);
+          //auto tpl1 = intersect_half_plane(ray,q1-q2,q1);
+
+          //auto tpr0 = intersect_half_plane(ray,-Hr,q2);
+          //auto tpr1 = intersect_half_plane(ray,q2-q1,q2);
+
+         
+          vfloat<M> t1,u1; Vec3<vfloat<M>> Ng1; vbool<M> valid1 = intersect_cone(valid_i,ray,v1,v2,t1,u1,Ng1);
+          //valid1 &= (0.0f <= u1) & (u1 <= 1.0f);
 
           vfloat<M> tl,ul; Vec3<vfloat<M>> Ngl; vbool<M> validl = intersect_sphere(valid_i,ray,v1,tl,Ngl); ul = 0.0f;
-          validl &= max(tpl0.first,tpl1.first) <= tl & tl <= min(tpl0.second,tpl1.second);
+          //validl &= max(tpl0.first,tpl1.first) <= tl & tl <= min(tpl0.second,tpl1.second);
+          auto left = u1 < 0.0f;
+          valid1 = select(left,validl,valid1);
+          t1 = select(left, tl, t1);
+          u1 = select(left, ul, u1);
+          Ng1.x = select(left, Ngl.x, Ng1.x);
+          Ng1.y = select(left, Ngl.y, Ng1.y);
+          Ng1.z = select(left, Ngl.z, Ng1.z);
 
-          //vfloat<M> t0,u0; Vec3<vfloat<M>> Ng0; vbool<M> valid0 = intersect_cone(valid_i,ray,v0,v1,t0,u0,Ng0);
-          vfloat<M> t1,u1; Vec3<vfloat<M>> Ng1; vbool<M> valid1 = intersect_cone(valid_i,ray,v1,v2,t1,u1,Ng1);
-          //vfloat<M> t2,u2; Vec3<vfloat<M>> Ng2; vbool<M> valid2 = intersect_cone(valid_i,ray,v2,v3,t2,u2,Ng2);
-          //valid1 &= !valid0 | (t0 > t1);
-          //valid1 &= !valid2 | (t2 > t1);
-          valid1 &= (ray.tnear < t1) & (t1 < ray.tfar);
+          vfloat<M> tr,ur; Vec3<vfloat<M>> Ngr; vbool<M> validr = intersect_sphere(valid_i,ray,v2,tr,Ngr); ur = 1.0f;
+          //validr &= max(tpr0.first,tpr1.first) <= tr & tr <= min(tpr0.second,tpr1.second);
+          auto right = u1 > 1.0f;
+          valid1 = select(right,validr,valid1);
+          t1 = select(right, tr, t1);
+          u1 = select(right, ur, u1);
+          Ng1.x = select(right, Ngr.x, Ng1.x);
+          Ng1.y = select(right, Ngr.y, Ng1.y);
+          Ng1.z = select(right, Ngr.z, Ng1.z);
 
-          auto thl = intersect_half_plane(ray,normalize_safe(q1-q0) + normalize_safe(q2-q1),q1);
-          //valid1 &= thl.first <= t1 & t1 <= thl.second;
-          auto thr = intersect_half_plane(ray,normalize_safe(q1-q2) + normalize_safe(q2-q3),q2);
-          //valid1 &= thr.first <= t1 & t1 <= thr.second;
+          auto thl = intersect_half_plane(ray,Hl,q1);
+          auto thr = intersect_half_plane(ray,Hr,q2);
           valid1 &= max(thl.first,thr.first) <= t1 & t1 <= min(thl.second,thr.second);
           
           if (none(valid1)) return false;
           LineIntersectorHitM<M> hit1(u1,zero,t1,Ng1);
           return epilog(valid1,hit1);
 
+#endif
+
+#if 0
           vfloat<M> tr,ur; Vec3<vfloat<M>> Ngr; vbool<M> validr = intersect_sphere(valid_i,ray,v2,tr,Ngr); ur = 1.0f;
           validr &= max(tpr0.first,tpr1.first) <= tr & tr <= min(tpr0.second,tpr1.second);
 
@@ -254,7 +272,6 @@ namespace embree
           
           LineIntersectorHitM<M> hit(u,zero,t,Ng);
           return epilog(valid,hit);
-
 #endif
         }
       };
