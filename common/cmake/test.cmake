e661574bc68b3d20e555d1c9ab6c5660f4b56bf8
Sven Woop <sven.woop@intel.com>
2016-01-13 12:10:02 +0100

    updates to ctest

--- common/cmake/test.cmake
+++ common/cmake/test.cmake
@@ -14,6 +14,18 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-INCLUDE(tutorial)
-ADD_TUTORIAL(triangle_geometry)
-ADD_EMBREE_TEST(triangle_geometry triangle_geometry)
+ENABLE_TESTING()
+INCLUDE(CTest)
+
+MACRO (ADD_EMBREE_TEST name executable)
+  ADD_TEST(NAME triangle_geometry
+           WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+           COMMAND ${PROJECT_SOURCE_DIR}/scripts/invoke_test.py
+                   --name ${name}
+                   --modeldir /Users/swoop/Work/models/embree-models
+                   --model default
+                   --execute ${PROJECT_BINARY_DIR}/${executable})
+
+#SET_TESTS_PROPERTIES(triangle_geometry PROPERTIES ATTACHED_FILES "triangle_geometry.tga")
+ENDMACRO()
+
