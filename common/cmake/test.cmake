9f63212b78f8e8c8d638ee4a6860dc82cdf98a66
Sven Woop <sven.woop@intel.com>
2016-01-14 07:44:16 +0100

    disabling testing by default, added config option for model repository

--- common/cmake/test.cmake
+++ common/cmake/test.cmake
@@ -14,52 +14,76 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-ENABLE_TESTING()
+IF(NOT BUILD_TESTING)
+  SET(BUILD_TESTING OFF CACHE BOOL "Build the testing tree.")
+ENDIF()
+
 INCLUDE(CTest)
 
-SET(EMBREE_MODEL_DIR /Users/swoop/Work/models/embree-models)
+IF (BUILD_TESTING)
+  
+  SET(BUILD_TESTING_MODEL_DIR "${PROJECT_SOURCE_DIR}/models" CACHE FILEPATH "Path to the folder containing the Embree models for regression testing.")
 
-FILE(READ "${EMBREE_MODEL_DIR}/embree-models-subdiv.txt" models_subdiv)
-STRING(REGEX REPLACE "\n" ";" models_subdiv "${models_subdiv}")
-
-FILE(READ "${EMBREE_MODEL_DIR}/embree-models-small-win32.txt" models_small_win32)
-STRING(REGEX REPLACE "\n" ";" models_small_win32 "${models_small_win32}")
-
-FILE(READ "${EMBREE_MODEL_DIR}/embree-models-small-x64.txt" models_small_x64)
-STRING(REGEX REPLACE "\n" ";" models_small_x64 "${models_small_x64}")
-
-FILE(READ "${EMBREE_MODEL_DIR}/embree-models-large.txt" models_large)
-STRING(REGEX REPLACE "\n" ";" models_large "${models_large}")
-
-SET(models ${models_small_win32}) # FIXME: configure all models
-
-#if platform == 'Win32': models += models_small_win32
-#else:                   models += models_small_x64
-#if platform == 'x64' and OS != 'macosx':
-#  models += models_large
-
-MACRO (ADD_EMBREE_TEST name executable)
-  ADD_TEST(NAME ${name}
-           WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-           COMMAND ${PROJECT_SOURCE_DIR}/scripts/invoke_test.py
-                   --name ${name}
-                   --modeldir ${EMBREE_MODEL_DIR}
-                   --model default
-                   --execute ${PROJECT_BINARY_DIR}/${executable})
-
-ENDMACRO()
-
-MACRO (ADD_EMBREE_MODELS_TEST name executable)
-  FOREACH (model ${models})
-    STRING(REGEX REPLACE "/" "_" modelname "${model}")
-    STRING(REGEX REPLACE ".ecs" "" modelname "${modelname}")
-    ADD_TEST(NAME "${name}_${modelname}"
+  IF(   NOT EXISTS "${BUILD_TESTING_MODEL_DIR}/embree-models-subdiv.txt"
+     OR NOT EXISTS "${BUILD_TESTING_MODEL_DIR}/embree-models-small-win32.txt"
+     OR NOT EXISTS "${BUILD_TESTING_MODEL_DIR}/embree-models-small-x64.txt"
+     OR NOT EXISTS "${BUILD_TESTING_MODEL_DIR}/embree-models-large.txt")
+    MESSAGE(FATAL_ERROR "Invalid Embree testing model repository. Either disable BUILD_TESTING or properly set BUILD_TESTING_MODEL_DIR.")
+  ENDIF()
+  
+  FILE(READ "${BUILD_TESTING_MODEL_DIR}/embree-models-subdiv.txt" models_subdiv)
+  STRING(REGEX REPLACE "\n" ";" models_subdiv "${models_subdiv}")
+  
+  FILE(READ "${BUILD_TESTING_MODEL_DIR}/embree-models-small-win32.txt" models_small_win32)
+  STRING(REGEX REPLACE "\n" ";" models_small_win32 "${models_small_win32}")
+  
+  FILE(READ "${BUILD_TESTING_MODEL_DIR}/embree-models-small-x64.txt" models_small_x64)
+  STRING(REGEX REPLACE "\n" ";" models_small_x64 "${models_small_x64}")
+  
+  FILE(READ "${BUILD_TESTING_MODEL_DIR}/embree-models-large.txt" models_large)
+  STRING(REGEX REPLACE "\n" ";" models_large "${models_large}")
+  
+  SET(models ${models_small_win32}) # FIXME: configure all models
+  
+  #if platform == 'Win32': models += models_small_win32
+  #else:                   models += models_small_x64
+  #if platform == 'x64' and OS != 'macosx':
+  #  models += models_large
+    
+  MACRO (ADD_EMBREE_TEST name executable)
+    ADD_TEST(NAME ${name}
              WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
              COMMAND ${PROJECT_SOURCE_DIR}/scripts/invoke_test.py
-                     --name "${name}_${modelname}"
-                     --modeldir ${EMBREE_MODEL_DIR}
-                     --model ${model}
+                     --name ${name}
+                     --modeldir ${BUILD_TESTING_MODEL_DIR}
+                     --model default
                      --execute ${PROJECT_BINARY_DIR}/${executable})
-  ENDFOREACH()
-  SET_TESTS_PROPERTIES("${name}_${modelname}" PROPERTIES ATTACHED_FILES "${name}_${modelname}.jpg")
-ENDMACRO()
+  
+    SET_TESTS_PROPERTIES(${name} PROPERTIES ATTACHED_FILES "${name}.jpg")
+  
+  ENDMACRO()
+  
+  MACRO (ADD_EMBREE_MODELS_TEST name executable)
+    FOREACH (model ${models})
+      STRING(REGEX REPLACE "/" "_" modelname "${model}")
+      STRING(REGEX REPLACE ".ecs" "" modelname "${modelname}")
+      ADD_TEST(NAME "${name}_${modelname}"
+               WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+               COMMAND ${PROJECT_SOURCE_DIR}/scripts/invoke_test.py
+                       --name "${name}_${modelname}"
+                       --modeldir ${BUILD_TESTING_MODEL_DIR}
+                       --model ${model}
+                       --execute ${PROJECT_BINARY_DIR}/${executable})
+    ENDFOREACH()
+  #  SET_TESTS_PROPERTIES("${name}_${modelname}" PROPERTIES ATTACHED_FILES "${name}_${modelname}.jpg")
+  ENDMACRO()
+  
+ELSE()
+
+  MACRO (ADD_EMBREE_TEST name executable)
+  ENDMACRO()
+  
+  MACRO (ADD_EMBREE_MODELS_TEST name executable)
+  ENDMACRO()
+
+ENDIF()
