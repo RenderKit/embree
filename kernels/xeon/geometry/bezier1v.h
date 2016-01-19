6b64e7182b863b192c9cea1e6d45edd72bd8866a
Sven Woop <sven.woop@intel.com>
2016-01-19 12:22:59 +0100

    cleanups in bezier1v.h

--- kernels/xeon/geometry/bezier1v.h
+++ kernels/xeon/geometry/bezier1v.h
@@ -22,7 +22,7 @@
 
 namespace embree
 {
-   struct BezierPrim
+  struct BezierPrim // FIXME: rename to BezierRef
   {
   public:
 
@@ -124,14 +124,15 @@ namespace embree
     __forceinline Bezier1v () {}
 
     /*! Construction from vertices and IDs. */
-    __forceinline Bezier1v (const Vec3fa& p0, const Vec3fa& p1, const Vec3fa& p2, const Vec3fa& p3, const float t0, const float t1,
-                           const unsigned int geomID, const unsigned int primID, const bool last)
-      : p0(p0), p1(p1), p2(p2), p3(p3), t0(t0), t1(t1), geom(geomID), prim(primID | (last << 31)) {}
+    __forceinline Bezier1v (const Vec3fa& p0, const Vec3fa& p1, const Vec3fa& p2, const Vec3fa& p3, const unsigned int geomID, const unsigned int primID)
+      : p0(p0), p1(p1), p2(p2), p3(p3), geom(geomID), prim(primID) {}
 
-    /*! access hidden members */
+    /*! return primitive ID */
     __forceinline unsigned int primID() const { 
       return prim;
     }
+
+    /*! return geometry ID */
     __forceinline unsigned int geomID() const { 
       return geom; 
     }
@@ -154,22 +155,16 @@ namespace embree
       const Vec3fa& p1 = curves->vertex(id+1);
       const Vec3fa& p2 = curves->vertex(id+2);
       const Vec3fa& p3 = curves->vertex(id+3);
-      new (this) Bezier1v(p0,p1,p2,p3,0.0f,1.0f,geomID,primID,list && i>=end);
+      new (this) Bezier1v(p0,p1,p2,p3,geomID,primID);
     }
 
     /*! fill triangle from triangle list */
     __forceinline void fill(const BezierPrim* prims, size_t& i, size_t end, Scene* scene, const bool list) 
     {
-      //*this = prims[i];
-      new (this) Bezier1v(prims[i].p0,prims[i].p1,prims[i].p2,prims[i].p3,0.0f,1.0f,prims[i].geom,prims[i].prim,false);
+      new (this) Bezier1v(prims[i].p0,prims[i].p1,prims[i].p2,prims[i].p3,prims[i].geom,prims[i].prim);
       i++;
     }
 
-    /*! returns size of t range */
-    __forceinline float dt() const {
-      return t1-t0;
-    }
-      
     /*! calculate the center of the curve */
     __forceinline const Vec3fa center2() const {
       return p0+p3;
@@ -178,93 +173,6 @@ namespace embree
     /*! calculate the center of the curve */
     __forceinline const Vec3fa center2(const AffineSpace3fa& space) const {
       return xfmPoint(space,p0)+xfmPoint(space,p3);
-    }
-
-    
-    /*! subdivide the bezier curve */
-    __forceinline void subdivide(Bezier1v& left_o, Bezier1v& right_o, const float T = 0.5f) const
-    {
-      const Vec3fa p00 = p0;
-      const Vec3fa p01 = p1;
-      const Vec3fa p02 = p2;
-      const Vec3fa p03 = p3;
-      
-      const float T0 = 1.0f - T, T1 = T;
-      const Vec3fa p10 = T0*p00 + T1*p01;
-      const Vec3fa p11 = T0*p01 + T1*p02;
-      const Vec3fa p12 = T0*p02 + T1*p03;
-      const Vec3fa p20 = T0*p10 + T1*p11;
-      const Vec3fa p21 = T0*p11 + T1*p12;
-      const Vec3fa p30 = T0*p20 + T1*p21;
-      
-      const float t01 = T0*t0 + T1*t1;
-      const unsigned int geomID = this->geomID();
-      const unsigned int primID = this->primID();
-      
-      new (&left_o ) Bezier1v(p00,p10,p20,p30,t0,t01,geomID,primID,false);
-      new (&right_o) Bezier1v(p30,p21,p12,p03,t01,t1,geomID,primID,false);
-    }
-    
-    /*! split the hair using splitting plane */
-    bool split(const Vec3fa& plane, Bezier1v& left_o, Bezier1v& right_o) const
-    {
-      /*! test if start and end points lie on different sides of plane */
-      const float p0p = dot(p0,plane)+plane.w;
-      const float p3p = dot(p3,plane)+plane.w;
-      if (p0p == 0.0f || p3p == 0.0f) return false;
-      if (p0p < 0.0f && p3p < 0.0f) return false;
-      if (p0p > 0.0f && p3p > 0.0f) return false;
-      
-      /*! search for the t-value that splits the curve into one part
-       *  left and right of the plane */
-      float u0 = 0.0f, u1 = 1.0f;
-      while (u1-u0 > 0.01f) 
-      //while (u1-u0 > 0.0001f) 
-      {
-        const float tc = 0.5f*(u0+u1);
-        Bezier1v left,right; subdivide(left,right,tc);
-        const float lp0p = dot(left.p0,plane)+plane.w;
-        const float lp3p = dot(left.p3,plane)+plane.w;
-        if (lp0p <= 0.0f && lp3p >= 0.0f) { u1 = tc; continue; }
-        if (lp0p >= 0.0f && lp3p <= 0.0f) { u1 = tc; continue; }
-        u0 = tc; 
-      }
-      
-      /*! return the split curve */
-      if (p0p < 0.0f) subdivide(left_o,right_o,0.5f*(u0+u1));
-      else            subdivide(right_o,left_o,0.5f*(u0+u1));
-      return true;
-    }
-
-    /*! split the hair using splitting plane */
-    bool split(const int dim, const float pos, Bezier1v& left_o, Bezier1v& right_o) const
-    {
-      /*! test if start and end points lie on different sides of plane */
-      const float p0p = p0[dim];
-      const float p3p = p3[dim];
-      if (p0p == pos || p3p == pos) return false;
-      if (p0p < pos && p3p < pos) return false;
-      if (p0p > pos && p3p > pos) return false;
-      
-      /*! search for the t-value that splits the curve into one part
-       *  left and right of the plane */
-      float u0 = 0.0f, u1 = 1.0f;
-      while (u1-u0 > 0.01f) 
-      //while (u1-u0 > 0.0001f) 
-      {
-        const float tc = 0.5f*(u0+u1);
-        Bezier1v left,right; subdivide(left,right,tc);
-        const float lp0p = left.p0[dim];
-        const float lp3p = left.p3[dim];
-        if (lp0p <= pos && lp3p >= pos) { u1 = tc; continue; }
-        if (lp0p >= pos && lp3p <= pos) { u1 = tc; continue; }
-        u0 = tc; 
-      }
-      
-      /*! return the split curve */
-      if (p0p < pos) subdivide(left_o,right_o,0.5f*(u0+u1));
-      else           subdivide(right_o,left_o,0.5f*(u0+u1));
-      return true;
     }
 
     __forceinline uint64_t id64() const {
@@ -282,7 +190,6 @@ namespace embree
         " p1 = " << b.p1 << ", " << std::endl <<
         " p2 = " << b.p2 << ", " << std::endl <<
         " p3 = " << b.p3 << ",  " << std::endl <<
-        " t0 = " << b.t0 << ",  t1 = " << b.t1 << ", " << std::endl <<
         " geomID = " << b.geomID() << ", primID = " << b.primID() << std::endl << 
       "}";
     }
@@ -292,15 +199,7 @@ namespace embree
     Vec3fa p1;            //!< 2nd control point (x,y,z,r)
     Vec3fa p2;            //!< 3rd control point (x,y,z,r)
     Vec3fa p3;            //!< 4th control point (x,y,z,r)
-    float t0,t1;          //!< t range of this sub-curve // FIXME: required?
     unsigned geom;      //!< geometry ID
     unsigned prim;      //!< primitive ID
   };
-
-
-
-
- 
-
-  //typedef Bezier1v BezierPrim; // FIXME: rename to BezierRef
 }
