1ce6b367dfa0b7da8758398e143315360b4cb66a
Sven Woop <sven.woop@intel.com>
2016-01-27 11:15:26 +0100

    updates

--- kernels/common/stat.cpp
+++ kernels/common/stat.cpp
@@ -116,9 +116,9 @@ namespace embree
     for (size_t i=0; i<10; i++)
       cout << "#user" << i << " = " << float(cntrs.user[i])/float(cntrs.all.normal.travs+cntrs.all.shadow.travs) << " per traversal" << std::endl;
 
-    cout << "#user5/user4 " << 100.0f*float(cntrs.user[5])/float(cntrs.user[4]) << "%" << std::endl;
-    cout << "#user6/user4 " << 100.0f*float(cntrs.user[6])/float(cntrs.user[4]) << "%" << std::endl;
-    cout << "#user7/user4 " << 100.0f*float(cntrs.user[7])/float(cntrs.user[4]) << "%" << std::endl;
+    cout << "#user5/user3 " << 100.0f*float(cntrs.user[5])/float(cntrs.user[3]) << "%" << std::endl;
+    cout << "#user6/user3 " << 100.0f*float(cntrs.user[6])/float(cntrs.user[3]) << "%" << std::endl;
+    cout << "#user7/user3 " << 100.0f*float(cntrs.user[7])/float(cntrs.user[3]) << "%" << std::endl;
     cout << std::endl;
   }
 }
