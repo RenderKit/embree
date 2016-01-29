38e8bd382dcf7a147b467643b358f1b6e161b331
Sven Woop <sven.woop@intel.com>
2016-01-29 13:54:04 +0100

    proper geom normals for ground plane

--- tutorials/common/tutorial/tutorial_device.cpp
+++ tutorials/common/tutorial/tutorial_device.cpp
@@ -144,8 +144,8 @@ Vec3fa renderPixelNg(float x, float y, const Vec3fa& vx, const Vec3fa& vy, const
   /* shade pixel */
   if (ray.geomID == RTC_INVALID_GEOMETRY_ID) return Vec3fa(0.0f);
   else {
-    //if (dot(ray.dir,ray.Ng) > 0.0f) return Vec3fa(zero); else
-    return normalize(Vec3fa(ray.Ng.x,ray.Ng.y,ray.Ng.z));
+    if (dot(ray.dir,ray.Ng) > 0.0f) return Vec3fa(zero); else
+    return normalize(abs(Vec3fa(ray.Ng.x,ray.Ng.y,ray.Ng.z)));
   }
 }
 
@@ -378,6 +378,7 @@ extern "C" bool device_pick(const float x,
   PRINT(ray.primID);
   Vec3fa hit_point = ray.org + ray.tfar*ray.dir;
   PRINT(hit_point);
+  PRINT(ray);
 
   /* shade pixel */
   if (ray.geomID == RTC_INVALID_GEOMETRY_ID) {
