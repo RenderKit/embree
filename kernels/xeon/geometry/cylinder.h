c1b8fc015489b437362fa024e4e5a95eda970b56
Sven Woop <sven.woop@intel.com>
2016-03-04 13:24:16 +0100

    cleanups

--- kernels/xeon/geometry/cylinder.h
+++ kernels/xeon/geometry/cylinder.h
@@ -68,49 +68,6 @@ namespace embree
         t_o.upper += tb;
         return true;
       }
-
-      __forceinline bool intersect(const Vec3fa& dir, 
-                                   BBox1f& t_o
-                                   //float& u0_o,
-                                   //Vec3fa& Ng_o
-        ) const
-      {
-        const Vec3fa v0 = p0;
-        const Vec3fa v1 = p1;
-        const float rl = rcp_length(v1-v0);
-        const Vec3fa P0 = v0, dP = (v1-v0)*rl;
-        const Vec3fa O = -P0, dO = dir;
-       
-        const float dOdO = dot(dO,dO);
-        const float OdO = dot(dO,O);
-        const float OO = dot(O,O);
-        const float dOz = dot(dP,dO);
-        const float Oz = dot(dP,O);
-        
-        const float A = dOdO - sqr(dOz);
-        const float B = 2.0f * (OdO - dOz*Oz);
-        const float C = OO - sqr(Oz) - sqr(r);
-        
-        const float D = B*B - 4.0f*A*C;
-        if (D < 0.0f) return false;
-        
-        const float Q = sqrt(D);
-        if (unlikely(A < min_rcp_input)) {
-          t_o.lower = float(neg_inf);
-          t_o.upper = float(pos_inf);
-          return true;
-        }
-        
-        const float rcp_2A = 0.5f*rcp(A);
-        t_o.lower = (-B-Q)*rcp_2A;
-        t_o.upper = (-B+Q)*rcp_2A;
-        
-        //u0_o = (Oz+t_o.lower*dOz)*rl;
-        //const Vec3fa Pr = t_o.lower*dir;
-        //const Vec3fa Pl = v0 + u0_o*(v1-v0);
-        //Ng_o = Pr-Pl;
-        return true;
-      }
     };
 
     template<int N>
