0bff78d23bf124f5d71f1bac7884bac5604ed0b2
Sven Woop <sven.woop@intel.com>
2016-03-11 09:36:08 +0100

    first version of bezier intersector that supports large radii

--- tutorials/viewer/viewer_device.cpp
+++ tutorials/viewer/viewer_device.cpp
@@ -655,7 +655,11 @@ void renderTile(int taskIndex, int* pixels,
 
   for (int y = y0; y<y1; y++) for (int x = x0; x<x1; x++)
   {
+    //if (x != 491 || y != 511-336) continue;
+    //PRINT2(x,y);
     Vec3fa color = renderPixel(x,y,vx,vy,vz,p);
+    //PRINT(color);
+    //exit(1);
 
     /* write color to framebuffer */
     unsigned int r = (unsigned int) (255.0f * clamp(color.x,0.0f,1.0f));
