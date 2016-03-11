4eb77b8fccf8c99427020a333181e4884683682e
Sven Woop <sven.woop@intel.com>
2016-03-11 14:13:09 +0100

    added initial viewer_stream tutorial, not yet fully implemented

---
+++ tutorials/viewer/viewer_device_stream.cpp
@@ -0,0 +1,288 @@
+// ======================================================================== //
+// Copyright 2009-2015 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include "../common/tutorial/tutorial_device.h"
+#include "../common/tutorial/scene_device.h"
+#include "../common/tutorial/random_sampler.h"
+
+extern "C" ISPCScene* g_ispc_scene;
+
+/* scene data */
+RTCDevice g_device = nullptr;
+RTCScene g_scene = nullptr;
+
+/* render function to use */
+renderPixelFunc renderPixel;
+
+/* error reporting function */
+void error_handler(const RTCError code, const char* str = nullptr)
+{
+  if (code == RTC_NO_ERROR) 
+    return;
+
+  printf("Embree: ");
+  switch (code) {
+  case RTC_UNKNOWN_ERROR    : printf("RTC_UNKNOWN_ERROR"); break;
+  case RTC_INVALID_ARGUMENT : printf("RTC_INVALID_ARGUMENT"); break;
+  case RTC_INVALID_OPERATION: printf("RTC_INVALID_OPERATION"); break;
+  case RTC_OUT_OF_MEMORY    : printf("RTC_OUT_OF_MEMORY"); break;
+  case RTC_UNSUPPORTED_CPU  : printf("RTC_UNSUPPORTED_CPU"); break;
+  case RTC_CANCELLED        : printf("RTC_CANCELLED"); break;
+  default                   : printf("invalid error code"); break;
+  }
+  if (str) { 
+    printf(" ("); 
+    while (*str) putchar(*str++); 
+    printf(")\n"); 
+  }
+  exit(1);
+}
+
+/* called by the C++ code for initialization */
+extern "C" void device_init (char* cfg)
+{
+  /* create new Embree device */
+  g_device = rtcNewDevice(cfg);
+  error_handler(rtcDeviceGetError(g_device));
+
+  /* set error handler */
+  rtcDeviceSetErrorFunction(g_device,error_handler);
+
+  /* set start render mode */
+  renderPixel = renderPixelStandard;
+  //renderPixel = renderPixelEyeLight;	
+}
+
+unsigned int convertTriangleMesh(ISPCTriangleMesh* mesh, RTCScene scene_out)
+{
+  unsigned int geomID = rtcNewTriangleMesh (scene_out, RTC_GEOMETRY_STATIC, mesh->numTriangles, mesh->numVertices, mesh->positions2 ? 2 : 1);
+  rtcSetBuffer(scene_out, geomID, RTC_VERTEX_BUFFER, mesh->positions, 0, sizeof(Vec3fa      ));
+  if (mesh->positions2) rtcSetBuffer(scene_out, geomID, RTC_VERTEX_BUFFER1, mesh->positions2, 0, sizeof(Vec3fa      ));
+  rtcSetBuffer(scene_out, geomID, RTC_INDEX_BUFFER,  mesh->triangles, 0, sizeof(ISPCTriangle));
+  mesh->geomID = geomID;
+  return geomID;
+}
+
+unsigned int convertQuadMesh(ISPCQuadMesh* mesh, RTCScene scene_out)
+{
+  unsigned int geomID = rtcNewQuadMesh (scene_out, RTC_GEOMETRY_STATIC, mesh->numQuads, mesh->numVertices, mesh->positions2 ? 2 : 1);
+  rtcSetBuffer(scene_out, geomID, RTC_VERTEX_BUFFER, mesh->positions, 0, sizeof(Vec3fa      ));
+  if (mesh->positions2) rtcSetBuffer(scene_out, geomID, RTC_VERTEX_BUFFER1, mesh->positions2, 0, sizeof(Vec3fa      ));
+  rtcSetBuffer(scene_out, geomID, RTC_INDEX_BUFFER,  mesh->quads, 0, sizeof(ISPCQuad));
+  mesh->geomID = geomID;
+  return geomID;
+}
+
+unsigned int convertSubdivMesh(ISPCSubdivMesh* mesh, RTCScene scene_out)
+{
+  unsigned int geomID = rtcNewSubdivisionMesh(scene_out, RTC_GEOMETRY_STATIC, mesh->numFaces, mesh->numEdges, mesh->numVertices, 
+                                                      mesh->numEdgeCreases, mesh->numVertexCreases, mesh->numHoles);
+  mesh->geomID = geomID;												
+  for (size_t i=0; i<mesh->numEdges; i++) mesh->subdivlevel[i] = 8.0f;
+  rtcSetBuffer(scene_out, geomID, RTC_VERTEX_BUFFER, mesh->positions, 0, sizeof(Vec3fa  ));
+  rtcSetBuffer(scene_out, geomID, RTC_LEVEL_BUFFER,  mesh->subdivlevel, 0, sizeof(float));
+  rtcSetBuffer(scene_out, geomID, RTC_INDEX_BUFFER,  mesh->position_indices  , 0, sizeof(unsigned int));
+  rtcSetBuffer(scene_out, geomID, RTC_FACE_BUFFER,   mesh->verticesPerFace, 0, sizeof(unsigned int));
+  rtcSetBuffer(scene_out, geomID, RTC_HOLE_BUFFER,   mesh->holes, 0, sizeof(unsigned int));
+  rtcSetBuffer(scene_out, geomID, RTC_EDGE_CREASE_INDEX_BUFFER,    mesh->edge_creases,          0, 2*sizeof(unsigned int));
+  rtcSetBuffer(scene_out, geomID, RTC_EDGE_CREASE_WEIGHT_BUFFER,   mesh->edge_crease_weights,   0, sizeof(float));
+  rtcSetBuffer(scene_out, geomID, RTC_VERTEX_CREASE_INDEX_BUFFER,  mesh->vertex_creases,        0, sizeof(unsigned int));
+  rtcSetBuffer(scene_out, geomID, RTC_VERTEX_CREASE_WEIGHT_BUFFER, mesh->vertex_crease_weights, 0, sizeof(float));
+  return geomID;
+} 
+
+unsigned int convertLineSegments(ISPCLineSegments* mesh, RTCScene scene_out)
+{
+  unsigned int geomID = rtcNewLineSegments (scene_out, RTC_GEOMETRY_STATIC, mesh->numSegments, mesh->numVertices, mesh->v2 ? 2 : 1);
+  rtcSetBuffer(scene_out,geomID,RTC_VERTEX_BUFFER,mesh->v,0,sizeof(Vertex));
+  if (mesh->v2) rtcSetBuffer(scene_out,geomID,RTC_VERTEX_BUFFER1,mesh->v2,0,sizeof(Vertex));
+  rtcSetBuffer(scene_out,geomID,RTC_INDEX_BUFFER,mesh->indices,0,sizeof(int));
+  return geomID;
+}
+
+unsigned int convertHairSet(ISPCHairSet* hair, RTCScene scene_out)
+{
+  unsigned int geomID = rtcNewHairGeometry (scene_out, RTC_GEOMETRY_STATIC, hair->numHairs, hair->numVertices, hair->v2 ? 2 : 1);
+  rtcSetBuffer(scene_out,geomID,RTC_VERTEX_BUFFER,hair->v,0,sizeof(Vertex));
+  if (hair->v2) rtcSetBuffer(scene_out,geomID,RTC_VERTEX_BUFFER1,hair->v2,0,sizeof(Vertex));
+  rtcSetBuffer(scene_out,geomID,RTC_INDEX_BUFFER,hair->hairs,0,sizeof(ISPCHair));
+  return geomID;
+}
+
+unsigned int convertCurveGeometry(ISPCHairSet* hair, RTCScene scene_out)
+{
+  unsigned int geomID = rtcNewCurveGeometry (scene_out, RTC_GEOMETRY_STATIC, hair->numHairs, hair->numVertices, hair->v2 ? 2 : 1);
+  rtcSetBuffer(scene_out,geomID,RTC_VERTEX_BUFFER,hair->v,0,sizeof(Vertex));
+  if (hair->v2) rtcSetBuffer(scene_out,geomID,RTC_VERTEX_BUFFER1,hair->v2,0,sizeof(Vertex));
+  rtcSetBuffer(scene_out,geomID,RTC_INDEX_BUFFER,hair->hairs,0,sizeof(ISPCHair));
+  return geomID;
+}
+
+RTCScene convertScene(ISPCScene* scene_in)
+{
+  int scene_flags = RTC_SCENE_STATIC | RTC_SCENE_INCOHERENT;
+  int scene_aflags = RTC_INTERSECT1 | RTC_INTERPOLATE | RTC_INTERSECTN;
+  RTCScene scene_out = rtcDeviceNewScene(g_device, (RTCSceneFlags)scene_flags,(RTCAlgorithmFlags) scene_aflags);
+
+  for (size_t i=0; i<scene_in->numGeometries; i++)
+  {
+    ISPCGeometry* geometry = scene_in->geometries[i];
+    if (geometry->type == SUBDIV_MESH) {
+      unsigned int geomID = convertSubdivMesh((ISPCSubdivMesh*) geometry, scene_out);
+      assert(geomID == i);
+    }
+    else if (geometry->type == TRIANGLE_MESH) {
+      unsigned int geomID = convertTriangleMesh((ISPCTriangleMesh*) geometry, scene_out);
+      assert(geomID == i);
+    }
+    else if (geometry->type == QUAD_MESH) {
+      unsigned int geomID = convertQuadMesh((ISPCQuadMesh*) geometry, scene_out);
+      assert(geomID == i);
+    }
+    else if (geometry->type == LINE_SEGMENTS) {
+      unsigned int geomID = convertLineSegments((ISPCLineSegments*) geometry, scene_out);
+      assert(geomID == i);
+    }
+    else if (geometry->type == HAIR_SET) {
+      unsigned int geomID = convertHairSet((ISPCHairSet*) geometry, scene_out);
+      assert(geomID == i);
+    }
+    else if (geometry->type == CURVES) {
+      unsigned int geomID = convertCurveGeometry((ISPCHairSet*) geometry, scene_out);
+      assert(geomID == i);
+    }
+    else
+      assert(false);
+  }
+  
+  return scene_out;
+}
+
+/* task that renders a single screen tile */
+Vec3fa renderPixelStandard(float x, float y, const Vec3fa& vx, const Vec3fa& vy, const Vec3fa& vz, const Vec3fa& p)
+{
+ 
+}
+
+struct RangeIterator
+{
+  __forceinline RangeIterator(int x0, int x1, int y0, int y1)
+    : x0(x0), x1(x1), y0(y0), y1(y1), cx(x0), cy(y0) {}
+
+  bool next(std::pair<int,int>& r)
+  {
+    if (cx>=x1) { cx = x0; cy++; }
+    if (cy>=y1) return false;
+    r = make_pair(cx,cy);
+    return true;
+  }
+
+public:
+  int x0,x1,y0,y1;
+  int cx,cy;
+};
+
+/* task that renders a single screen tile */
+void renderTile(int taskIndex, int* pixels,
+                     const int width,
+                     const int height, 
+                     const float time,
+                     const Vec3fa& vx, 
+                     const Vec3fa& vy, 
+                     const Vec3fa& vz, 
+                     const Vec3fa& p,
+                     const int numTilesX, 
+                     const int numTilesY)
+{
+  const int t = taskIndex;
+  const int tileY = t / numTilesX;
+  const int tileX = t - tileY * numTilesX;
+  const int x0 = tileX * TILE_SIZE_X;
+  const int x1 = min(x0+TILE_SIZE_X,width);
+  const int y0 = tileY * TILE_SIZE_Y;
+  const int y1 = min(y0+TILE_SIZE_Y,height);
+
+  Ray rays[8];
+  RangeIterator iter(x0,x1,y0,y1);
+
+  for (size_t i=0; i<8; i++)
+  {
+    std::pair<int,int> p;
+    if (!iter.next(p)) break;
+    int x = p.first;
+    int y = p.second;
+
+    /* initialize sampler */
+    RandomSampler sampler;
+    RandomSampler_init(sampler, x, y, 0);
+    
+    /* initialize ray */
+    ray.org = p;
+    ray.dir = normalize(x*vx + y*vy + vz);
+    ray.tnear = 0.0f;
+    ray.tfar = inf;
+    ray.geomID = RTC_INVALID_GEOMETRY_ID;
+    ray.primID = RTC_INVALID_GEOMETRY_ID;
+    ray.mask = -1;
+    ray.time = RandomSampler_get1D(sampler);
+    
+    /* intersect ray with scene */
+    rtcIntersect(g_scene,ray);
+    
+    /* shade background black */
+    if (ray.geomID == RTC_INVALID_GEOMETRY_ID)
+      return Vec3fa(0.0f);
+    
+    /* shade all rays that hit something */
+    return Vec3fa(abs(dot(ray.dir,normalize(ray.Ng))));
+    
+    /* write color to framebuffer */
+    unsigned int r = (unsigned int) (255.0f * clamp(color.x,0.0f,1.0f));
+    unsigned int g = (unsigned int) (255.0f * clamp(color.y,0.0f,1.0f));
+    unsigned int b = (unsigned int) (255.0f * clamp(color.z,0.0f,1.0f));
+    pixels[y*width+x] = (b << 16) + (g << 8) + r;
+  }
+#endif
+}
+
+/* called by the C++ code to render */
+extern "C" void device_render (int* pixels,
+                               const int width,
+                               const int height, 
+                               const float time,
+                               const Vec3fa& vx, 
+                               const Vec3fa& vy, 
+                               const Vec3fa& vz, 
+                               const Vec3fa& p)
+{
+  /* create scene */
+  if (g_scene == nullptr) { 
+    g_scene = convertScene(g_ispc_scene);
+    rtcCommit (g_scene);
+  }
+
+  /* render image */
+  const int numTilesX = (width +TILE_SIZE_X-1)/TILE_SIZE_X;
+  const int numTilesY = (height+TILE_SIZE_Y-1)/TILE_SIZE_Y;
+  launch_renderTile(numTilesX*numTilesY,pixels,width,height,time,vx,vy,vz,p,numTilesX,numTilesY); 
+}
+
+/* called by the C++ code for cleanup */
+extern "C" void device_cleanup ()
+{
+  rtcDeleteScene (g_scene);
+  rtcDeleteDevice(g_device);
+}
