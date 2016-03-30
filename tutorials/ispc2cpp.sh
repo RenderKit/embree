1478790c66746ac81a6730fbec880c476f79c164
Sven Woop <sven.woop@intel.com>
2016-03-30 15:46:32 +0200

    ispc2cpp updates

--- tutorials/ispc2cpp.sh
+++ tutorials/ispc2cpp.sh
@@ -61,9 +61,7 @@ sed -i.backup  's/RTC_INTERSECT_VARYING/RTC_INTERSECT1/g' $2
 sed -i.backup  's/RTC_MATRIX_COLUMN_MAJOR/RTC_MATRIX_COLUMN_MAJOR_ALIGNED16/g' $2
 sed -i.backup  's/RTC_MATRIX_COLUMN_MAJOR_ALIGNED16_ALIGNED16/RTC_MATRIX_COLUMN_MAJOR_ALIGNED16/g' $2
 
-#sed -i.backup  's/task void renderTileTask(int\* pixels,/void renderTileTask(int taskIndex, int\* pixels,/g' $2
-#sed -i.backup  's/task void animateSphere (Vertex\* vertices,/void animateSphere (int taskIndex, Vertex\* vertices,/g' $2
-sed -i.backup  's/Vec3f renderPixelStandard(float x, float y, const Vec3f\& vx, const Vec3f\& vy, const Vec3f\& vz, const Vec3f\& p)/Vec3fa renderPixelStandard(float x, float y, const Vec3fa\& vx, const Vec3fa\& vy, const Vec3fa\& vz, const Vec3fa\& p)/g' $2
+#sed -i.backup  's/Vec3f renderPixelStandard(float x, float y, const Vec3f\& vx, const Vec3f\& vy, const Vec3f\& vz, const Vec3f\& p)/Vec3fa renderPixelStandard(float x, float y, const Vec3fa\& vx, const Vec3fa\& vy, const Vec3fa\& vz, const Vec3fa\& p)/g' $2
 sed -i.backup  's/RTCIntersectFuncVarying/RTCIntersectFunc/g' $2
 sed -i.backup  's/RTCOccludedFuncVarying/RTCOccludedFunc/g' $2
 #sed -i.backup  's/\#if 1 \/\/ enables parallel execution/\#if 0/g' $2
