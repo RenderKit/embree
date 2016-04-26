fa0a83b7c1c8e4b4863c8ecb1025c1d347a86825
cbenthin <carsten.benthin@intel.com>
2016-04-26 12:23:07 +0200

    added support for 64bit pointers in quantized nodes, enabled by default, quantized node size increases slightly by 4 * N bytes

--- kernels/xeon/bvh/bvh_builder_sah.cpp
+++ kernels/xeon/bvh/bvh_builder_sah.cpp
@@ -88,12 +88,15 @@ namespace embree
         for (size_t i=0; i<items; i++) {
           accel[i].fill(prims,start,current.prims.end(),bvh->scene,false);
         }
-        //*current.parent = node;
 
+#if ENABLE_32BIT_OFFSETS_FOR_QUANTIZED_NODES == 1 
         typename BVH::QuantizedNode *parent = (typename BVH::QuantizedNode *)((size_t)current.parent & (~0x7));
         const size_t index = (size_t)current.parent & 0x7;
         parent->childOffset(index) = bvh->encodeQuantizedLeaf((size_t)parent,(size_t)node);
         assert((ssize_t)node == (ssize_t)parent + parent->childOffset(index));
+#else
+        *current.parent = node;
+#endif
 	return n;
       }
 
