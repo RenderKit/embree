350854c7d4119b9b7d09916ae7865ea0573e6778
Sven Woop <sven.woop@intel.com>
2016-05-06 11:29:48 +0200

    bugfix in pmap regression test

--- kernels/algorithms/pmap.cpp
+++ kernels/algorithms/pmap.cpp
@@ -32,10 +32,9 @@ namespace embree
       const size_t N = 10000;
       std::vector<uint32_t> keys(N);
       std::vector<uint32_t> vals(N);
-      for (size_t i=0; i<N; i++) {
-	keys[i] = 2*rand();
-	vals[i] = 2*rand();
-      }
+      for (size_t i=0; i<N; i++) keys[i] = 2*i*647382649;
+      for (size_t i=0; i<N; i++) std::swap(keys[i],keys[rand()%N]);
+      for (size_t i=0; i<N; i++) vals[i] = 2*rand();
       
       /* create map */
       pmap<uint32_t,uint32_t> map;
@@ -43,13 +42,13 @@ namespace embree
 
       /* check that all keys are properly mapped */
       for (size_t i=0; i<N; i++) {
-	const uint32_t* val = map.lookup(keys[i]);
-	passed &= val && (*val == vals[i]);
+        const uint32_t* val = map.lookup(keys[i]);
+        passed &= val && (*val == vals[i]);
       }
 
       /* check that these keys are not in the map */
       for (size_t i=0; i<N; i++) {
-	passed &= !map.lookup(keys[i]+1);
+        passed &= !map.lookup(keys[i]+1);
       }
 
       return passed;
