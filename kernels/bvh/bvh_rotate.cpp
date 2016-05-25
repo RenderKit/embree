f780386379e45e981c1815de4357f1a0c26959c9
Sven Woop <sven.woop@intel.com>
2016-05-25 07:36:43 +0200

    fixed FeeBSD linking issue for 32 bit, fixes issue #85

--- kernels/bvh/bvh_rotate.cpp
+++ kernels/bvh/bvh_rotate.cpp
@@ -104,7 +104,8 @@ namespace embree
 	/*! find best other child */
 	vfloat4 area0123 = vfloat4(extract<0>(min0),extract<0>(min1),extract<0>(min2),extract<0>(min3)) - vfloat4(childArea[c2]);
 	int pos[4] = { pos0,pos1,pos2,pos3 };
-	vbool4 valid = vint4(int(depth+1))+cdepth <= vint4(BVH4::maxBuildDepth); // only select swaps that fulfill depth constraints
+	const size_t mbd = BVH4::maxBuildDepth;
+	vbool4 valid = vint4(int(depth+1))+cdepth <= vint4(mbd); // only select swaps that fulfill depth constraints
 	valid &= vint4(c2) != vint4(step);
 	if (none(valid)) continue;
 	size_t c1 = select_min(valid,area0123);
