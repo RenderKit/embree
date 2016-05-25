ecdef5b4e78f90a775cd78a0102a9204b4d51d19
Sven Woop <sven.woop@intel.com>
2016-05-25 07:31:20 +0200

    added support to set thread affinity for FreeBSD, fixes issue #84

--- common/sys/thread.cpp
+++ common/sys/thread.cpp
@@ -239,6 +239,29 @@ namespace embree
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////
+/// FreeBSD Platform
+////////////////////////////////////////////////////////////////////////////////
+
+#if defined(__FreeBSD__)
+
+#include <pthread_np.h>
+
+namespace embree
+{
+  /*! set affinity of the calling thread */
+  void setAffinity(ssize_t affinity)
+  {
+    cpuset_t cset;
+    CPU_ZERO(&cset);
+    CPU_SET(affinity, &cset);
+
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cset), &cset) != 0)
+      WARNING("pthread_setaffinity_np failed"); // on purpose only a warning
+  }
+}
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
 /// MacOSX Platform
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -290,8 +313,9 @@ namespace embree
   static void* threadStartup(ThreadStartupData* parg)
   {
     _mm_setcsr(_mm_getcsr() | /*FTZ:*/ (1<<15) | /*DAZ:*/ (1<<6));
-
-#if !defined(__LINUX__)
+    
+    /*! Mac OS X does not support setting affinity at thread creation time */
+#if defined(__MACOSX__)
     if (parg->affinity >= 0)
 	setAffinity(parg->affinity);
 #endif
@@ -320,7 +344,15 @@ namespace embree
       cpu_set_t cset;
       CPU_ZERO(&cset);
       CPU_SET(mapThreadID(threadID), &cset);
-      if (pthread_setaffinity_np(*tid,sizeof(cpu_set_t),&cset))
+      if (pthread_setaffinity_np(*tid, sizeof(cset), &cset))
+        WARNING("pthread_setaffinity_np failed"); // on purpose only a warning
+    }
+#elif defined(__FreeBSD__)
+    if (threadID >= 0) {
+      cpuset_t cset;
+      CPU_ZERO(&cset);
+      CPU_SET(threadID, &cset);
+      if (pthread_setaffinity_np(*tid, sizeof(cset), &cset))
         WARNING("pthread_setaffinity_np failed"); // on purpose only a warning
     }
 #endif
