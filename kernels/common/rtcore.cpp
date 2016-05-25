069cc9424471569fea39864773a0d61148831bbe
Sven Woop <sven.woop@intel.com>
2016-05-25 11:15:22 +0200

    removed ray stream logger

--- kernels/common/rtcore.cpp
+++ kernels/common/rtcore.cpp
@@ -229,14 +229,7 @@ namespace embree
     RTCORE_CATCH_BEGIN;
     RTCORE_TRACE(rtcCommit);
     RTCORE_VERIFY_HANDLE(hscene);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.dumpGeometry(scene);
-#endif
-
-    /* perform scene build */
     scene->build(0,0);
-    
     RTCORE_CATCH_END(scene->device);
   }
 
@@ -291,17 +284,8 @@ namespace embree
     if (scene->isModified()) throw_RTCError(RTC_INVALID_OPERATION,"scene got not committed");
     if (((size_t)&ray) & 0x0F        ) throw_RTCError(RTC_INVALID_ARGUMENT, "ray not aligned to 16 bytes");   
 #endif
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay old_ray = ray;
-#endif
-
     STAT3(normal.travs,1,1,1);
     scene->intersect(ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay1Intersect(scene,old_ray,ray);
-#endif
     RTCORE_CATCH_END(scene->device);
   }
 
@@ -320,17 +304,7 @@ namespace embree
 #endif
     STAT(size_t cnt=0; for (size_t i=0; i<4; i++) cnt += ((int*)valid)[i] == -1;);
     STAT3(normal.travs,1,cnt,4);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay4 old_ray = ray;
-#endif
-
     scene->intersect4(valid,ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay4Intersect(valid,scene,old_ray,ray);
-#endif
-
 #else
     throw_RTCError(RTC_INVALID_OPERATION,"rtcIntersect4 not supported");  
 #endif
@@ -352,17 +326,7 @@ namespace embree
 #endif
     STAT(size_t cnt=0; for (size_t i=0; i<8; i++) cnt += ((int*)valid)[i] == -1;);
     STAT3(normal.travs,1,cnt,8);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay8 old_ray = ray;
-#endif
-
     scene->intersect8(valid,ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay8Intersect(valid,scene,old_ray,ray);
-#endif
-
 #else
     throw_RTCError(RTC_INVALID_OPERATION,"rtcIntersect8 not supported");      
 #endif
@@ -384,17 +348,7 @@ namespace embree
 #endif
     STAT(size_t cnt=0; for (size_t i=0; i<16; i++) cnt += ((int*)valid)[i] == -1;);
     STAT3(normal.travs,1,cnt,16);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay16 old_ray = ray;
-#endif
-
     scene->intersect16(valid,ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay16Intersect(valid,scene,old_ray,ray);
-#endif
-
 #else
     throw_RTCError(RTC_INVALID_OPERATION,"rtcIntersect16 not supported");
 #endif
@@ -517,16 +471,7 @@ namespace embree
     if (scene->isModified()) throw_RTCError(RTC_INVALID_OPERATION,"scene got not committed");
     if (((size_t)&ray) & 0x0F        ) throw_RTCError(RTC_INVALID_ARGUMENT, "ray not aligned to 16 bytes");   
 #endif
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay old_ray = ray;
-#endif
-
     scene->occluded(ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay1Occluded(scene,old_ray,ray);
-#endif
     RTCORE_CATCH_END(scene->device);
   }
   
@@ -545,17 +490,7 @@ namespace embree
 #endif
     STAT(size_t cnt=0; for (size_t i=0; i<4; i++) cnt += ((int*)valid)[i] == -1;);
     STAT3(shadow.travs,1,cnt,4);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay4 old_ray = ray;
-#endif
-
     scene->occluded4(valid,ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay4Occluded(valid,scene,old_ray,ray);
-#endif
-
 #else
     throw_RTCError(RTC_INVALID_OPERATION,"rtcOccluded4 not supported");
 #endif
@@ -577,17 +512,7 @@ namespace embree
 #endif
     STAT(size_t cnt=0; for (size_t i=0; i<8; i++) cnt += ((int*)valid)[i] == -1;);
     STAT3(shadow.travs,1,cnt,8);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay8 old_ray = ray;
-#endif
-
     scene->occluded8(valid,ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay8Occluded(valid,scene,old_ray,ray);
-#endif
-
 #else
     throw_RTCError(RTC_INVALID_OPERATION,"rtcOccluded8 not supported");
 #endif
@@ -609,17 +534,7 @@ namespace embree
 #endif
     STAT(size_t cnt=0; for (size_t i=0; i<16; i++) cnt += ((int*)valid)[i] == -1;);
     STAT3(shadow.travs,1,cnt,16);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RTCRay16 old_ray = ray;
-#endif
-
     scene->occluded16(valid,ray,nullptr);
-
-#if defined(EMBREE_RAYSTREAM_LOGGER)
-    RayStreamLogger::rayStreamLogger.logRay16Occluded(valid,scene,old_ray,ray);
-#endif
-    
 #else
     throw_RTCError(RTC_INVALID_OPERATION,"rtcOccluded16 not supported");
 #endif
