36bedee90b242f27ceedf4fe3e7f6174b0fc402d
Sven Woop <sven.woop@intel.com>
2016-05-27 14:43:27 +0200

    cleanups

--- kernels/geometry/triangle4i_intersector_pluecker.h
+++ kernels/geometry/triangle4i_intersector_pluecker.h
@@ -29,32 +29,32 @@ namespace embree
     /*! Intersector1 for Triangle4i */
     template<int M, int Mx, bool filter>
     struct Triangle4iIntersector1Pluecker
+    {
+      typedef Triangle4i Primitive;
+      typedef PlueckerIntersector1<Mx> Precalculations;
+      
+      static __forceinline void intersect(const Precalculations& pre, Ray& ray, const RTCIntersectContext* context, const Primitive& tri, Scene* scene, const unsigned* geomID_to_instID)
       {
-        typedef Triangle4i Primitive;
-        typedef PlueckerIntersector1<Mx> Precalculations;
-        
-        static __forceinline void intersect(const Precalculations& pre, Ray& ray, const RTCIntersectContext* context, const Primitive& tri, Scene* scene, const unsigned* geomID_to_instID)
-        {
-          STAT3(normal.trav_prims,1,1,1);
-          Vec3vf4 v0, v1, v2; tri.gather(v0,v1,v2);
-          pre.intersect(ray,v0,v1,v2,UVIdentity<Mx>(),Intersect1EpilogM<M,Mx,filter>(ray,context,tri.geomIDs,tri.primIDs,scene,geomID_to_instID)); 
-        }
-        
-        static __forceinline bool occluded(const Precalculations& pre, Ray& ray, const RTCIntersectContext* context, const Primitive& tri, Scene* scene, const unsigned* geomID_to_instID)
-        {
-          STAT3(shadow.trav_prims,1,1,1);
-          Vec3vf4 v0, v1, v2; tri.gather(v0,v1,v2);
-          return pre.intersect(ray,v0,v1,v2,UVIdentity<Mx>(),Occluded1EpilogM<M,Mx,filter>(ray,context,tri.geomIDs,tri.primIDs,scene,geomID_to_instID)); 
-        }
-      };
-
+        STAT3(normal.trav_prims,1,1,1);
+        Vec3vf4 v0, v1, v2; tri.gather(v0,v1,v2);
+        pre.intersect(ray,v0,v1,v2,UVIdentity<Mx>(),Intersect1EpilogM<M,Mx,filter>(ray,context,tri.geomIDs,tri.primIDs,scene,geomID_to_instID)); 
+      }
+      
+      static __forceinline bool occluded(const Precalculations& pre, Ray& ray, const RTCIntersectContext* context, const Primitive& tri, Scene* scene, const unsigned* geomID_to_instID)
+      {
+        STAT3(shadow.trav_prims,1,1,1);
+        Vec3vf4 v0, v1, v2; tri.gather(v0,v1,v2);
+        return pre.intersect(ray,v0,v1,v2,UVIdentity<Mx>(),Occluded1EpilogM<M,Mx,filter>(ray,context,tri.geomIDs,tri.primIDs,scene,geomID_to_instID)); 
+      }
+    };
+    
     /*! Triangle4i intersector for K rays */
     template<int M, int Mx, int K, bool filter>
       struct Triangle4iIntersectorKPluecker
       {
         typedef Triangle4i Primitive;
         typedef PlueckerIntersectorK<Mx,K> Precalculations;
- 
+        
         static __forceinline void intersect(const vbool<K>& valid_i, Precalculations& pre, RayK<K>& ray, const RTCIntersectContext* context, const Primitive& tri, Scene* scene)
         {
           for (size_t i=0; i<Triangle4i::max_size(); i++)
