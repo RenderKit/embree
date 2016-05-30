6ec01506c4375910a7b466e42d5be4258ec8f35c
Sven Woop <sven.woop@intel.com>
2016-05-30 11:00:01 +0200

    enabled GCC -Wnarrowing warning

--- common/sys/platform.h
+++ common/sys/platform.h
@@ -296,7 +296,7 @@ __forceinline std::string toString(long long value) {
 #endif
 
 #if defined(__GNUC__) && !defined(__INTEL_COMPILER)
-#pragma GCC diagnostic ignored "-Wnarrowing"
+//#pragma GCC diagnostic ignored "-Wnarrowing"
 //#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 //#pragma GCC diagnostic ignored "-Warray-bounds"
 #endif
