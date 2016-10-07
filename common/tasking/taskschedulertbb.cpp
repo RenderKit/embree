8fd44db180ef5d26a42b3e244ba1d7b047ba3e24
Sven Woop <sven.woop@intel.com>
2016-10-07 10:32:39 +0200

    starting TBB threads if affinity got set

--- common/tasking/taskschedulertbb.cpp
+++ common/tasking/taskschedulertbb.cpp
@@ -73,7 +73,9 @@ namespace embree
     /* also start worker threads in affinity mode */
     if (set_affinity) {
       tbb::parallel_for(size_t(0), size_t(g_numThreads), size_t(1), [] ( size_t i ) {
-          while (tbb_affinity.threadCount != g_numThreads) {};
+          while (tbb_affinity.threadCount != g_numThreads) {
+            yield();
+          };
         });
     }
   }
