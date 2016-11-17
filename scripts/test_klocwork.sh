ef414e21d28ec990628dd974c11851db457d67b6
Sven Woop <sven.woop@intel.com>
2016-11-17 14:27:57 +0100

    updates to test_klocwork.sh

--- scripts/test_klocwork.sh
+++ scripts/test_klocwork.sh
@@ -4,3 +4,5 @@ kwinject -w -o buildspec.txt make -j 8
 kwcheck create -b buildspec.txt --url http://localhost:8080/embree
 kwcheck run
 kwcheck list -F detailed
+kwcheck list -F detailed > klocwork.log
+
