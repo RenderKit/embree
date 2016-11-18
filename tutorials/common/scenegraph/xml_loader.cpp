14ccd7d3f2fdce1f1e3890699a97aca757f956c5
Sven Woop <sven.woop@intel.com>
2016-11-18 14:48:11 +0100

    added security check of width and height when loading textures from bin file

--- tutorials/common/scenegraph/xml_loader.cpp
+++ tutorials/common/scenegraph/xml_loader.cpp
@@ -680,6 +680,9 @@ namespace embree
       const unsigned height = stoi(xml->parm("height"));
       const Texture::Format format = Texture::string_to_format(xml->parm("format"));
       const unsigned bytesPerTexel = Texture::getFormatBytesPerTexel(format);
+      if (ftell(binFile) + width*height*bytesPerTexel > binFileSize)
+        THROW_RUNTIME_ERROR("error reading from binary file: "+binFileName.str());
+      
       texture = std::make_shared<Texture>(width,height,format);
       if (width*height != fread(texture->data, bytesPerTexel, width*height, binFile)) 
         THROW_RUNTIME_ERROR("error reading from binary file: "+binFileName.str());
