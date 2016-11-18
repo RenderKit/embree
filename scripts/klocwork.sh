ae64f5a63c10f43d80b29117c82b823c7dc86db8
Sven Woop <sven.woop@intel.com>
2016-11-18 09:06:15 +0100

    updates to klockwork invokation

--- scripts/klocwork.sh
+++ scripts/klocwork.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-make clean
-kwinject -w -o buildspec.txt make -j 8
+make clean > /dev/null
+kwinject -w -o buildspec.txt make -j 8 > /dev/null
 kwcheck create -b buildspec.txt --url http://localhost:8080/embree
 kwcheck run
 kwcheck list -F detailed
