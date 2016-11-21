bbace893ae8de78a2d3276ed73eeacf78961772c
Sven Woop <sven.woop@intel.com>
2016-11-21 13:18:33 +0100

    proper error handling in png loader

--- common/sys/platform.h
+++ common/sys/platform.h
@@ -343,3 +343,24 @@ __asm__ __volatile__ (									\
 					IACA_SSC_MARK(111)}
 #define IACA_END {IACA_SSC_MARK(222) \
 					IACA_UD_BYTES}
+
+namespace embree
+{
+  template<typename Closure>
+    struct OnScopeExitHelper
+  {
+    OnScopeExitHelper (const Closure f) : f(f) {}
+    ~OnScopeExitHelper() { f(); }
+    const Closure f;
+  };
+  
+  template <typename Closure>
+    OnScopeExitHelper<Closure> OnScopeExit(const Closure f) {
+    return OnScopeExitHelper<Closure>(f);
+  };
+
+#define STRING_JOIN2(arg1, arg2) DO_STRING_JOIN2(arg1, arg2)
+#define DO_STRING_JOIN2(arg1, arg2) arg1 ## arg2
+#define ON_SCOPE_EXIT(code)                                             \
+  auto STRING_JOIN2(on_scope_exit_, __LINE__) = OnScopeExit([&](){code;})
+}
