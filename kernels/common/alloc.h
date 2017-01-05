9b0480f68e67e0070ec7774f79c2d0ebdaf3b5cf
cbenthin <carsten.benthin@intel.com>
2017-01-05 13:59:57 +0100

    reverted alignedMalloc alignment back to 64 bytes

--- kernels/common/alloc.h
+++ kernels/common/alloc.h
@@ -463,20 +463,11 @@ namespace embree
         /* either use alignedMalloc or os_reserve/os_commit */
         if (!osAllocation)
         {
-          const size_t defaultAlignment = 64;
-#if defined(__AVX512F__)
-          /* need full page alignment on xeon phi to ensure 2M pages, better waste some space than to lose lot of performance */
-          const size_t alignmentSizeThreshold = 2 * PAGE_SIZE_2M; // 2 * 2M = 4M
-          const size_t alignment = (bytesReserve >= alignmentSizeThreshold) ? PAGE_SIZE_2M : defaultAlignment;
-          ptr = alignedMalloc(bytesReserve,alignment);         
-#else
-          /* slightly reduces rendering performance on xeon 3-5% */
-          const size_t alignmentSizeThreshold = 8 * PAGE_SIZE; // 8 * 4k = 32k
-          const size_t alignment = (bytesReserve >= alignmentSizeThreshold) ? PAGE_SIZE : defaultAlignment;
-          ptr = alignedMalloc(bytesReserve,alignment);         
-#endif
+          ptr = alignedMalloc(bytesReserve,CACHELINE_SIZE);         
           assert(ptr);
-          os_advise(ptr,bytesReserve); 
+#if defined(__AVX512F__)
+          //os_advise(ptr,bytesReserve); 
+#endif
         }
         else
         {
