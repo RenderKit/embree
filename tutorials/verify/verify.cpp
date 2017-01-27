dbda8400e5cc8186ccf4c6a062f0df912bf9ec42
Sven Woop <sven.woop@intel.com>
2017-01-27 12:30:10 +0100

    enabled modifications also for static meshes inside dynamic scene for regression_dynamic test of verify

--- tutorials/verify/verify.cpp
+++ tutorials/verify/verify.cpp
@@ -2995,14 +2995,6 @@ namespace embree
         else 
         {
           switch (types[index]) {
-          case 0:
-          case 3:
-          case 6:
-	  case 9:
-          case 12:
-          case 15:
-          case 16:
-          case 17:
           case 18:
           case 19:
           case 20:
@@ -3013,16 +3005,24 @@ namespace embree
             geom[index].second = nullptr; 
             break;
           }
+          case 0:
           case 1: 
           case 2:
+          case 3:
           case 4: 
           case 5:
+          case 6:
 	  case 7: 
           case 8:
+          case 9:
           case 10:
           case 11:
+          case 12:
           case 13:
           case 14:
+          case 15:
+          case 16:
+          case 17:
           {
             int op = RandomSampler_getInt(task->sampler)%3;
             switch (op) {
