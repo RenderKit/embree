09ee4fed8a44607b66535748f45d0e28f2886a53
Sven Woop <sven.woop@intel.com>
2017-02-02 08:43:48 +0100

    cleanups

--- common/simd/sse.cpp
+++ common/simd/sse.cpp
@@ -16,7 +16,8 @@
 
 #include "sse.h"
 
-namespace embree {
+namespace embree 
+{
   const __m128 _mm_lookupmask_ps[16] = {
     _mm_castsi128_ps(_mm_set_epi32( 0, 0, 0, 0)),
     _mm_castsi128_ps(_mm_set_epi32( 0, 0, 0,-1)),
@@ -42,5 +43,4 @@ namespace embree {
     _mm_castsi128_pd(_mm_set_epi32(-1,-1, 0, 0)),
     _mm_castsi128_pd(_mm_set_epi32(-1,-1,-1,-1))
   };
-
 }
