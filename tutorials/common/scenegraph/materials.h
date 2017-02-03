d6bbfd9965c5f571d9d3a666800052e4307442cb
Sven Woop <sven.woop@intel.com>
2017-02-03 13:07:57 +0100

    updates to materials.h

--- tutorials/common/scenegraph/materials.h
+++ tutorials/common/scenegraph/materials.h
@@ -35,67 +35,63 @@ namespace embree
     MATERIAL_HAIR
   };
 
-  class MaterialBase
-  {
-  public:
-    MaterialBase() {}
-    MaterialBase(MaterialType type) : type(type) {}
-
-  public:
-    int type;
-  private:
-    int align[3];
-  };
-
-  class MatteMaterial : public MaterialBase
+  class MatteMaterial 
   {
   public:
     MatteMaterial (const Vec3fa& reflectance)
-      : MaterialBase(MATERIAL_MATTE), reflectance(reflectance) {}
+      : type(MATERIAL_MATTE), reflectance(reflectance) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa reflectance;
   };
 
-  class MirrorMaterial : public MaterialBase
+  class MirrorMaterial
   {
   public:
     MirrorMaterial (const Vec3fa& reflectance)
-      : MaterialBase(MATERIAL_MIRROR), reflectance(reflectance) {}
+      : type(MATERIAL_MIRROR), reflectance(reflectance) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa reflectance;
   };
 
-  class ThinDielectricMaterial : public MaterialBase
+  class ThinDielectricMaterial
   {
   public:
     ThinDielectricMaterial (const Vec3fa& transmission, const float eta, const float thickness)
-      : MaterialBase(MATERIAL_THIN_DIELECTRIC), transmission(transmission), transmissionFactor(log(transmission)*thickness), eta(eta), thickness(thickness) {}
+      : type(MATERIAL_THIN_DIELECTRIC), transmission(transmission), transmissionFactor(log(transmission)*thickness), eta(eta), thickness(thickness) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa transmission;
     Vec3fa transmissionFactor;
     float eta;
     float thickness;
   };
 
-  class OBJMaterial : public MaterialBase
+  class OBJMaterial
   {
   public:
     OBJMaterial ()
-      : MaterialBase(MATERIAL_OBJ), illum(0), d(1.f), Ns(1.f), Ni(1.f), Ka(0.f), Kd(1.f), Ks(0.f), Kt(1.0f), map_d(nullptr), map_Kd(nullptr), map_Displ(nullptr) {}
+      : type(MATERIAL_OBJ), illum(0), d(1.f), Ns(1.f), Ni(1.f), Ka(0.f), Kd(1.f), Ks(0.f), Kt(1.0f), map_d(nullptr), map_Kd(nullptr), map_Displ(nullptr) {}
 
     OBJMaterial (float d, const Vec3fa& Kd, const Vec3fa& Ks, const float Ns)
-      : MaterialBase(MATERIAL_OBJ), illum(0), d(d), Ns(Ns), Ni(1.f), Ka(0.f), Kd(Kd), Ks(Ks), Kt(1.0f), map_d(nullptr), map_Kd(nullptr), map_Displ(nullptr) {}
+      : type(MATERIAL_OBJ), illum(0), d(d), Ns(Ns), Ni(1.f), Ka(0.f), Kd(Kd), Ks(Ks), Kt(1.0f), map_d(nullptr), map_Kd(nullptr), map_Displ(nullptr) {}
 
     OBJMaterial (float d, const Texture* map_d, const Vec3fa& Kd, const Texture* map_Kd, const Vec3fa& Ks, const Texture* map_Ks, const float Ns, const Texture* map_Ns, const Texture* map_Bump)
-      : MaterialBase(MATERIAL_OBJ), illum(0), d(d), Ns(Ns), Ni(1.f), Ka(0.f), Kd(Kd), Ks(Ks), Kt(1.0f), map_d(map_d), map_Kd(map_Kd), map_Displ(nullptr) {}
+      : type(MATERIAL_OBJ), illum(0), d(d), Ns(Ns), Ni(1.f), Ka(0.f), Kd(Kd), Ks(Ks), Kt(1.0f), map_d(map_d), map_Kd(map_Kd), map_Displ(nullptr) {}
 
     ~OBJMaterial() { // FIXME: destructor never called!
     }
 
   public:
+    int type;
+    int align[3];
     int illum;             /*< illumination model */
     float d;               /*< dissolve factor, 1=opaque, 0=transparent */
     float Ns;              /*< specular exponent */
@@ -111,16 +107,18 @@ namespace embree
     const Texture* map_Displ;        /*< Displ texture */
   };
 
-  class MetalMaterial : public MaterialBase
+  class MetalMaterial
   {
   public:
     MetalMaterial (const Vec3fa& reflectance, const Vec3fa& eta, const Vec3fa& k)
-      : MaterialBase(MATERIAL_REFLECTIVE_METAL), reflectance(reflectance), eta(eta), k(k), roughness(0.0f) {}
+      : type(MATERIAL_REFLECTIVE_METAL), reflectance(reflectance), eta(eta), k(k), roughness(0.0f) {}
 
     MetalMaterial (const Vec3fa& reflectance, const Vec3fa& eta, const Vec3fa& k, const float roughness)
-      : MaterialBase(MATERIAL_METAL), reflectance(reflectance), eta(eta), k(k), roughness(roughness) {}
+      : type(MATERIAL_METAL), reflectance(reflectance), eta(eta), k(k), roughness(roughness) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa reflectance;
     Vec3fa eta;
     Vec3fa k;
@@ -129,66 +127,76 @@ namespace embree
 
   typedef MetalMaterial ReflectiveMetalMaterial;
 
-  class VelvetMaterial : public MaterialBase
+  class VelvetMaterial
   {
   public:
     VelvetMaterial (const Vec3fa& reflectance, const float backScattering, const Vec3fa& horizonScatteringColor, const float horizonScatteringFallOff)
-      : MaterialBase(MATERIAL_VELVET), reflectance(reflectance), horizonScatteringColor(horizonScatteringColor), backScattering(backScattering), horizonScatteringFallOff(horizonScatteringFallOff) {}
+      : type(MATERIAL_VELVET), reflectance(reflectance), horizonScatteringColor(horizonScatteringColor), backScattering(backScattering), horizonScatteringFallOff(horizonScatteringFallOff) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa reflectance;
     Vec3fa horizonScatteringColor;
     float backScattering;
     float horizonScatteringFallOff;
   };
 
-  class DielectricMaterial : public MaterialBase
+  class DielectricMaterial
   {
   public:
     DielectricMaterial (const Vec3fa& transmissionOutside, const Vec3fa& transmissionInside, const float etaOutside, const float etaInside)
-      : MaterialBase(MATERIAL_DIELECTRIC), transmissionOutside(transmissionOutside), transmissionInside(transmissionInside), etaOutside(etaOutside), etaInside(etaInside) {}
+      : type(MATERIAL_DIELECTRIC), transmissionOutside(transmissionOutside), transmissionInside(transmissionInside), etaOutside(etaOutside), etaInside(etaInside) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa transmissionOutside;
     Vec3fa transmissionInside;
     float etaOutside;
     float etaInside;
   };
 
-  class MetallicPaintMaterial : public MaterialBase
+  class MetallicPaintMaterial
   {
   public:
     MetallicPaintMaterial (const Vec3fa& shadeColor, const Vec3fa& glitterColor, float glitterSpread, float eta)
-      : MaterialBase(MATERIAL_METALLIC_PAINT), shadeColor(shadeColor), glitterColor(glitterColor), glitterSpread(glitterSpread), eta(eta) {}
+      : type(MATERIAL_METALLIC_PAINT), shadeColor(shadeColor), glitterColor(glitterColor), glitterSpread(glitterSpread), eta(eta) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa shadeColor;
     Vec3fa glitterColor;
     float glitterSpread;
     float eta;
   };
 
-  class HairMaterial : public MaterialBase
+  class HairMaterial
   {
   public:
     HairMaterial (const Vec3fa& Kr, const Vec3fa& Kt, float nx, float ny)
-      : MaterialBase(MATERIAL_HAIR), Kr(Kr), Kt(Kt), nx(nx), ny(ny) {}
+      : type(MATERIAL_HAIR), Kr(Kr), Kt(Kt), nx(nx), ny(ny) {}
 
   public:
+    int type;
+    int align[3];
     Vec3fa Kr;
     Vec3fa Kt;
     float nx;
     float ny;
   };
 
-  class Material : public MaterialBase
+  class Material
   {
   public:
     Material () { for (auto& x : v) x = Vec3fa(zero); }
     Material (const OBJMaterial& in) { *((OBJMaterial*)this) = in; }
     OBJMaterial& obj() { return *(OBJMaterial*)this; }
 
-  private:
+  public:
+    int type;
+    int align[3];
     Vec3fa v[7];
   };
 }
