91e9ca3457d25a3b3b7ce22e115a71c51741b82f
Sven Woop <sven.woop@intel.com>
2017-02-20 12:44:21 +0100

    removed old BVH builder interface from bvh_builder tutorial

--- tutorials/bvh_builder/bvh_builder_device.cpp
+++ tutorials/bvh_builder/bvh_builder_device.cpp
@@ -14,15 +14,7 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#define INTERNAL_BVH_BUILD 1
-
 #include "../common/tutorial/tutorial_device.h"
-
-#if INTERNAL_BVH_BUILD
-#include "../../kernels/common/alloc.h"
-#include "../../kernels/builders/bvh_builder_sah.h"
-#include "../../kernels/builders/bvh_builder_morton.h"
-#endif
 
 namespace embree
 {
@@ -93,97 +85,10 @@ namespace embree
     }
   };
 
-#if INTERNAL_BVH_BUILD
-  void build_sah(PrimRef* prims, size_t N)
-  {
-    /* fast allocator that supports thread local operation */
-    FastAllocator allocator(nullptr,false);
-    
-    for (size_t i=0; i<10; i++)
-    {
-      std::cout << "iteration " << i << ": building BVH over " << N << " primitives, " << std::flush;
-      double t0 = getSeconds();
-      
-      allocator.reset();
-      allocator.init(N);
-
-      /* calculate priminfo */
-      auto computeBounds = [&](const range<size_t>& r) -> isa::CentGeomBBox3fa
-        {
-          isa::CentGeomBBox3fa bounds(empty);
-          for (size_t j=r.begin(); j<r.end(); j++)
-            bounds.extend(prims[j].bounds());
-          return bounds;
-        };
-      const isa::CentGeomBBox3fa bounds = 
-        parallel_reduce(size_t(0),N,size_t(1024),size_t(1024),isa::CentGeomBBox3fa(empty), computeBounds, isa::CentGeomBBox3fa::merge2);
-      
-      const isa::PrimInfo pinfo(0,N,bounds.geomBounds,bounds.centBounds);
-
-       /* settings for BVH build */
-      isa::GeneralBVHBuilder::Settings settings;
-      settings.branchingFactor = 2;
-      settings.maxDepth = 1024;
-      settings.logBlockSize = 0;
-      settings.minLeafSize = 1;
-      settings.maxLeafSize = 1;
-      settings.travCost = 1.0f;
-      settings.intCost = 1.0f;
-      settings.singleThreadThreshold = Builder::DEFAULT_SINGLE_THREAD_THRESHOLD;
-      
-      Node* root = isa::BVHBuilderBinnedSAH::build<Node*>(
-
-        /* thread local allocator for fast allocations */
-        [&] () -> FastAllocator::ThreadLocal* { 
-          return allocator.threadLocal(); 
-        },
-
-        /* lambda function that creates BVH nodes */
-        [&](isa::BVHBuilderBinnedSAH::BuildRecord* children, const size_t N, FastAllocator::ThreadLocal* alloc) -> Node*
-        {
-          assert(N <= 2);
-          InnerNode* node = new (alloc->malloc(sizeof(InnerNode))) InnerNode;
-          for (size_t i=0; i<N; i++)
-            node->bounds[i] = children[i].prims.geomBounds;
-          return node;
-        },
-
-        /* lambda function that updates BVH nodes */
-        [&](Node* ref, Node** children, const size_t N) -> Node*
-        {
-          assert(N <= 2);
-          InnerNode* node = (InnerNode*) ref;
-          for (size_t i=0; i<N; i++)
-            node->children[i] = children[i];
-          return ref;
-        },
-        
-        /* lambda function that creates BVH leaves */
-        [&](const isa::BVHBuilderBinnedSAH::BuildRecord& current, FastAllocator::ThreadLocal* alloc) -> Node*
-        {
-          assert(current.prims.size() == 1);
-          Node* node = new (alloc->malloc(sizeof(LeafNode))) LeafNode(prims[current.prims.begin()].primID(),prims[current.prims.begin()].bounds());
-          return node;
-        },
-        
-        /* progress monitor function */
-        [&] (size_t dn) { 
-          // throw an exception here to cancel the build operation
-        },
-        
-        prims,pinfo,settings);
-      
-      double t1 = getSeconds();
-      
-      std::cout << 1000.0f*(t1-t0) << "ms, " << 1E-6*double(N)/(t1-t0) << " Mprims/s, sah = " << root->sah() << " [DONE]" << std::endl;
-    }
-  }
-#endif
-
   void buildProgress (size_t dn, void* userPtr) {
   }
 
-  void build_sah_api(RTCBuildPrimitive* prims, size_t N, char* cfg)
+  void build_sah(RTCBuildPrimitive* prims, size_t N, char* cfg)
   {
     RTCDevice device = rtcNewDevice(cfg);
     rtcDeviceSetMemoryMonitorFunction2(device,memoryMonitor,nullptr);
@@ -215,87 +120,7 @@ namespace embree
     rtcDeleteBVH(bvh);
   }
 
-#if INTERNAL_BVH_BUILD
-  void build_morton(PrimRef* prims, size_t N)
-  {
-    /* array for morton builder */
-    avector<isa::BVHBuilderMorton::BuildPrim> morton_src(N);
-    avector<isa::BVHBuilderMorton::BuildPrim> morton_tmp(N);
-    for (unsigned i=0; i<N; i++) 
-      morton_src[i].index = i;
-    
-    /* fast allocator that supports thread local operation */
-    FastAllocator allocator(nullptr,true);
-    
-    for (size_t i=0; i<10; i++)
-    {
-      std::cout << "iteration " << i << ": building BVH over " << N << " primitives, " << std::flush;
-      double t0 = getSeconds();
-      
-      allocator.reset();
-      allocator.init(16*N);
-      
-      std::pair<Node*,BBox3fa> node_bounds = isa::BVHBuilderMorton::build<std::pair<Node*,BBox3fa>>(
-        
-        /* thread local allocator for fast allocations */
-        [&] () -> FastAllocator::ThreadLocal* { 
-          return allocator.threadLocal(); 
-        },
-        
-        /* lambda function that allocates BVH nodes */
-        [&] ( FastAllocator::ThreadLocal* alloc, size_t N ) -> Node*
-        {
-          assert(N <= 2);
-          InnerNode* node = new (alloc->malloc(sizeof(InnerNode))) InnerNode;
-          return node;
-        },
-        
-        /* lambda function that sets bounds */
-        [&] (Node* ref, const std::pair<Node*,BBox3fa>* children, size_t N) -> std::pair<Node*,BBox3fa>
-        {
-          InnerNode* node = (InnerNode*) ref;
-          BBox3fa bounds = empty;
-          for (size_t i=0; i<N; i++) {
-            node->children[i] = children[i].first;
-            node->bounds[i] = children[i].second;
-            bounds.extend(children[i].second);
-          }
-          return std::make_pair(ref,bounds);
-        },
-        
-        /* lambda function that creates BVH leaves */
-        [&]( const range<unsigned>& current, FastAllocator::ThreadLocal* alloc) -> std::pair<Node*,BBox3fa>
-        {
-          assert(current.size() == 1);
-          const size_t id = morton_src[current.begin()].index;
-          const BBox3fa bounds = prims[id].bounds(); 
-          Node* node = new (alloc->malloc(sizeof(LeafNode))) LeafNode(id,bounds);
-          return std::make_pair(node,bounds);
-        },
-        
-        /* lambda that calculates the bounds for some primitive */
-        [&] (const isa::BVHBuilderMorton::BuildPrim& morton) -> BBox3fa {
-          return prims[morton.index].bounds();
-        },
-        
-        /* progress monitor function */
-        [&] (size_t dn) { 
-          // throw an exception here to cancel the build operation
-        },
-        
-        morton_src.data(),morton_tmp.data(),N,
-        isa::BVHBuilderMorton::Settings(2,1024,1,1,Builder::DEFAULT_SINGLE_THREAD_THRESHOLD));
-      
-      Node* root = node_bounds.first;
-      
-      double t1 = getSeconds();
-      
-      std::cout << 1000.0f*(t1-t0) << "ms, " << 1E-6*double(N)/(t1-t0) << " Mprims/s, sah = " << root->sah() << " [DONE]" << std::endl;
-    }
-  }
-#endif
-
-  void build_morton_api(RTCBuildPrimitive* prims, size_t N, char* cfg)
+  void build_morton(RTCBuildPrimitive* prims, size_t N, char* cfg)
   {
     RTCDevice device = rtcNewDevice(cfg);
     rtcDeviceSetMemoryMonitorFunction2(device,memoryMonitor,nullptr);
@@ -364,20 +189,10 @@ namespace embree
     }
 
     std::cout << "SAH builder:" << std::endl;
-    build_sah_api(prims.data(),prims.size(),cfg);
-
-#if INTERNAL_BVH_BUILD
-    std::cout << "internal SAH builder:" << std::endl;
-    build_sah((PrimRef*)prims.data(),prims.size());
-#endif
+    build_sah(prims.data(),prims.size(),cfg);
 
     std::cout << "Morton builder:" << std::endl;
-    build_morton_api(prims.data(),prims.size(),cfg);
-
-#if INTERNAL_BVH_BUILD
-    std::cout << "internal Morton builder:" << std::endl;
-    build_morton((PrimRef*)prims.data(),prims.size());
-#endif
+    build_morton(prims.data(),prims.size(),cfg);
   }
   
   /* task that renders a single screen tile */
