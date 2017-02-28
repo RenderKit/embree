30ebdc09b3230429155167a69d7297ed230b9289
Sven Woop <sven.woop@intel.com>
2017-02-28 13:29:13 +0100

    parallelized basis conversion

--- kernels/common/scene_bezier_curves.cpp
+++ kernels/common/scene_bezier_curves.cpp
@@ -228,36 +228,41 @@ namespace embree
     if (native_curves.size() != curves.size()) 
     {
       native_curves = APIBuffer<unsigned>(parent->device,curves.size(),sizeof(unsigned int),true);
-      for (size_t i=0; i<curves.size(); i++) {
-        if (curves[i]+3 >= numVertices()) native_curves[i] = 0xFFFFFFF0; // invalid curves stay invalid this way
-        else                              native_curves[i] = 4*i;
-      }
+      parallel_for(size_t(0), curves.size(), size_t(1024), [&] ( const range<size_t> r) {
+          for (size_t i=r.begin(); i<r.end(); i++) {
+            if (curves[i]+3 >= numVertices()) native_curves[i] = 0xFFFFFFF0; // invalid curves stay invalid this way
+            else                              native_curves[i] = 4*i;
+          }
+        });
     }
 
     if (native_vertices.size() != vertices.size())
       native_vertices.resize(vertices.size());
 
-    for (size_t i=0; i<vertices.size(); i++)
-    {
-      if (native_vertices[i].size() != 4*curves.size())
-        native_vertices[i] = APIBuffer<Vec3fa>(parent->device,4*curves.size(),sizeof(Vec3fa),true);
-      
-      for (size_t j=0; j<curves.size(); j++) 
-      {
-        const unsigned id = curves[j];
-        if (id+3 >= numVertices()) continue; // ignore invalid curves
-        const Vec3fa v0 = vertices[i][id+0];
-        const Vec3fa v1 = vertices[i][id+1];
-        const Vec3fa v2 = vertices[i][id+2];
-        const Vec3fa v3 = vertices[i][id+3];
-        const InputCurve3fa icurve(v0,v1,v2,v3);
-        Curve3fa ocurve; convert<Vec3fa>(icurve,ocurve);
-        native_vertices[i].store(4*j+0,ocurve.v0);
-        native_vertices[i].store(4*j+1,ocurve.v1);
-        native_vertices[i].store(4*j+2,ocurve.v2);
-        native_vertices[i].store(4*j+3,ocurve.v3);
-      }
-    }
+    parallel_for(vertices.size(), [&] ( const size_t i ) {
+        
+        if (native_vertices[i].size() != 4*curves.size())
+          native_vertices[i] = APIBuffer<Vec3fa>(parent->device,4*curves.size(),sizeof(Vec3fa),true);
+        
+        parallel_for(size_t(0), curves.size(), size_t(1024), [&] ( const range<size_t> rj ) {
+
+            for (size_t j=rj.begin(); j<rj.end(); j++)
+            {
+              const unsigned id = curves[j];
+              if (id+3 >= numVertices()) continue; // ignore invalid curves
+              const Vec3fa v0 = vertices[i][id+0];
+              const Vec3fa v1 = vertices[i][id+1];
+              const Vec3fa v2 = vertices[i][id+2];
+              const Vec3fa v3 = vertices[i][id+3];
+              const InputCurve3fa icurve(v0,v1,v2,v3);
+              Curve3fa ocurve; convert<Vec3fa>(icurve,ocurve);
+              native_vertices[i].store(4*j+0,ocurve.v0);
+              native_vertices[i].store(4*j+1,ocurve.v1);
+              native_vertices[i].store(4*j+2,ocurve.v2);
+              native_vertices[i].store(4*j+3,ocurve.v3);
+            }
+        });
+      });
     native_vertices0 = native_vertices[0];
   }
 
