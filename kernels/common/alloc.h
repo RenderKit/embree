32f812e5760a789048df7af9eb6ac2a8bc7fc64b
Sven Woop <sven.woop@intel.com>
2017-04-13 14:34:26 +0200

    bugfix

--- kernels/common/alloc.h
+++ kernels/common/alloc.h
@@ -312,7 +312,7 @@ namespace embree
     {
       size_t blockSize = alignSize(bytesEstimated/(fast ? 4 : 20));
       growSize = maxGrowSize = clamp(blockSize,size_t(10*128),maxAllocationSize);
-      defaultBlockSize       = clamp(blockSize,size_t( 1*128),size_t(PAGE_SIZE));
+      defaultBlockSize       = clamp(blockSize,size_t(10*128),size_t(PAGE_SIZE));
       use_single_mode = !fast && (2*defaultBlockSize >= bytesEstimated/100);
       if (bytesEstimated == 0) maxGrowSize = maxAllocationSize; // special mode if builder cannot estimate tree size
       log2_grow_size_scale = 0;
