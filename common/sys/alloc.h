3ca71edf58de0faea69c7dc3611229d49031813f
Sven Woop <sven.woop@intel.com>
2017-05-03 09:28:12 +0200

    cleanups

--- common/sys/alloc.h
+++ common/sys/alloc.h
@@ -149,10 +149,6 @@ namespace embree
         IDs.push_back(id);
       }
 
-      __forceinline size_t size() const {
-        return nextID;
-      }
-
     private:
       std::vector<T> IDs;   //!< stores deallocated IDs to be reused
       T nextID;        //!< next ID to use when IDs vector is empty
