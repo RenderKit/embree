dc750313a38ed5cf2030ac29497cd58db8bad319
Sven Woop <sven.woop@intel.com>
2017-05-12 14:55:43 +0200

    updates to primref geomID and primID assignment

--- kernels/common/rtcore_builder.cpp
+++ kernels/common/rtcore_builder.cpp
@@ -244,10 +244,10 @@ namespace embree
         
         __forceinline void operator() (PrimRef& prim, const size_t dim, const float pos, PrimRef& left_o, PrimRef& right_o) const 
         {
-          prim.geomID() &= BVHBuilderBinnedFastSpatialSAH::GEOMID_MASK;
+          prim.geomIDref() &= BVHBuilderBinnedFastSpatialSAH::GEOMID_MASK;
           splitPrimitive((RTCBuildPrimitive&)prim,(unsigned)dim,pos,(RTCBounds&)left_o,(RTCBounds&)right_o,userPtr);
-          left_o.geomID()  = geomID; left_o.primID()  = primID;
-          right_o.geomID() = geomID; right_o.primID() = primID;
+          left_o.geomIDref()  = geomID; left_o.primIDref()  = primID;
+          right_o.geomIDref() = geomID; right_o.primIDref() = primID;
         }
 
         __forceinline void operator() (const BBox3fa& box, const size_t dim, const float pos, BBox3fa& left_o, BBox3fa& right_o) const 
