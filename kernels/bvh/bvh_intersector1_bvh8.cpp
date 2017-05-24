b204147a5f36e619e3d63db282dcb31b4ab73576
Sven Woop <sven.woop@intel.com>
2017-05-24 13:27:25 +0200

    updates

--- kernels/bvh/bvh_intersector1_bvh8.cpp
+++ kernels/bvh/bvh_intersector1_bvh8.cpp
@@ -75,6 +75,7 @@ namespace embree
     DEFINE_INTERSECTOR1(BVH8MultiFastIntersector1     ,BVHNIntersector1<8 COMMA BVH_AN1                  COMMA false COMMA Virtual6LeafIntersector1<LEAF_INTERSECTORS_FAST> >);
     DEFINE_INTERSECTOR1(BVH8MultiFastMBIntersector1   ,BVHNIntersector1<8 COMMA BVH_AN1_AN2_AN4D         COMMA false COMMA Virtual6LeafIntersector1<LEAF_INTERSECTORS_FAST> >);
     DEFINE_INTERSECTOR1(BVH8MultiFastOBBIntersector1  ,BVHNIntersector1<8 COMMA BVH_AN1_UN1              COMMA false COMMA Virtual6LeafIntersector1<LEAF_INTERSECTORS_FAST> >);
-    DEFINE_INTERSECTOR1(BVH8MultiFastOBBMBIntersector1,BVHNIntersector1<8 COMMA BVH_AN1_AN2_AN4D_UN1_UN2 COMMA false COMMA Virtual6LeafIntersector1<LEAF_INTERSECTORS_FAST> >);
+    //DEFINE_INTERSECTOR1(BVH8MultiFastOBBMBIntersector1,BVHNIntersector1<8 COMMA BVH_AN1_AN2_AN4D_UN1_UN2 COMMA false COMMA Virtual6LeafIntersector1<LEAF_INTERSECTORS_FAST> >);
+    DEFINE_INTERSECTOR1(BVH8MultiFastOBBMBIntersector1,BVHNIntersector1<8 COMMA BVH_AN2_AN4D_UN2 COMMA false COMMA Virtual6LeafIntersector1<LEAF_INTERSECTORS_FAST> >);
   }
 }
