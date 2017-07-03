abdedb8680b16099b53962d08792f994a804c553
Sven Woop <sven.woop@intel.com>
2017-07-03 11:10:05 +0200

    curve intersector filters out NaNs caused in special cases such as ray oriented along a cylinder

--- kernels/geometry/bezier_curve_intersector.h
+++ kernels/geometry/bezier_curve_intersector.h
@@ -113,8 +113,8 @@ namespace embree
         if (converged_u && converged_t) 
         {
           t+=dt;
-          if (t <= ray.tnear || t > ray.tfar) return false;
-          if (u < 0.0f || u > 1.0f) return false;
+          if (!(t > ray.tnear && t < ray.tfar)) return false; // rejects NaNs
+          if (!(u >= 0.0f && u <= 1.0f)) return false; // rejects NaNs
           const Vec3fa R = normalize(Q-P);
           const Vec3fa U = madd(Vec3fa(dPdu.w),R,dPdu);
           const Vec3fa V = cross(dPdu,R);
