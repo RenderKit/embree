159e4400145c804e223427859b28dc7c0debc170
Sven Woop <sven.woop@intel.com>
2017-09-08 07:24:07 +0200

    updates klocwork server

--- scripts/klocwork.sh
+++ scripts/klocwork.sh
@@ -3,7 +3,7 @@ make clean > /dev/null
 kwinject -w -o buildspec.txt make -j 8 > /dev/null
 #kwcheck create -b buildspec.txt --url http://localhost:8080/embree
 #kwcheck run
-kwbuildproject --force --url http://localhost:8080/embree buildspec.txt --tables-directory mytables
-kwadmin --url http://localhost:8080 load embree mytables
+kwbuildproject --force --url http://10.123.110.111:80/embree buildspec.txt --tables-directory mytables
+kwadmin --url http://10.123.110.111:80 load embree mytables
 #kwcheck list -F detailed
 #kwcheck list -F detailed > klocwork.log
