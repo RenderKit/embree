24e7d5482738c82c1522809d1d3d680ee0e8ed43
Sven Woop <sven.woop@intel.com>
2017-10-12 12:43:40 +0200

    retry in klocwork.sh

--- scripts/klocwork.sh
+++ scripts/klocwork.sh
@@ -45,8 +45,8 @@ set -e
 echo "$KW_SERVER_IP;$KW_SERVER_PORT;$KW_USER;$KW_LTOKEN" > $KLOCWORK_LTOKEN
 make clean > /dev/null
 $KW_CLIENT_PATH/bin/kwinject -w -o buildspec.txt make -j 8
-$KW_SERVER_PATH/bin/kwbuildproject --force --url http://$KW_SERVER_IP:$KW_SERVER_PORT/embree buildspec.txt --tables-directory mytables
-$KW_SERVER_PATH/bin/kwadmin --url http://$KW_SERVER_IP:$KW_SERVER_PORT load embree mytables
+retry_cmd 60 $KW_SERVER_PATH/bin/kwbuildproject --force --url http://$KW_SERVER_IP:$KW_SERVER_PORT/embree buildspec.txt --tables-directory mytables
+retry_cmd 60 $KW_SERVER_PATH/bin/kwadmin --url http://$KW_SERVER_IP:$KW_SERVER_PORT load embree mytables
 
 #if ! [ -x "$(command -v kwinject)" ]; then
 #    echo "kwinject not found"
