48eb104282d8c6dca72a7c3ad170b39208b4e71b
Sven Woop <sven.woop@intel.com>
2018-02-06 10:42:45 +0100

    implemented occlusion query for compact hair

--- kernels/geometry/bezierNi_intersector.h
+++ kernels/geometry/bezierNi_intersector.h
@@ -139,7 +139,106 @@ namespace embree
         }
       }
       
-      static __forceinline bool occluded(const Precalculations& pre, Ray& ray, IntersectContext* context, const Primitive& prim) {
+      static __forceinline bool occluded(const Precalculations& pre, Ray& ray, IntersectContext* context, const Primitive& prim)
+      {
+#if EMBREE_HAIR_LEAF_MODE == 0
+
+        const size_t N = prim.N;
+        AffineSpace3vf<8> space(Vec3vf8(vfloat<8>::loadu(prim.vx_x(N)),vfloat<8>::loadu(prim.vx_y(N)),vfloat<8>::loadu(prim.vx_z(N))),
+                                Vec3vf8(vfloat<8>::loadu(prim.vy_x(N)),vfloat<8>::loadu(prim.vy_y(N)),vfloat<8>::loadu(prim.vy_z(N))),
+                                Vec3vf8(vfloat<8>::loadu(prim.vz_x(N)),vfloat<8>::loadu(prim.vz_y(N)),vfloat<8>::loadu(prim.vz_z(N))),
+                                Vec3vf8(vfloat<8>::loadu(prim.p_x (N)),vfloat<8>::loadu(prim.p_y (N)),vfloat<8>::loadu(prim.p_z (N))));
+        
+        const Vec3vf8 dir1 = xfmVector(space,Vec3vf8(ray.dir));
+        const Vec3vf8 org1 = xfmPoint (space,Vec3vf8(ray.org));
+        const Vec3vf8 nrcp_dir1 = -rcp(dir1);
+        
+        const vfloat8 t_lower_x = org1.x*nrcp_dir1.x;
+        const vfloat8 t_lower_y = org1.y*nrcp_dir1.y;
+        const vfloat8 t_lower_z = org1.z*nrcp_dir1.z;
+        const vfloat8 t_upper_x = t_lower_x - nrcp_dir1.x;
+        const vfloat8 t_upper_y = t_lower_y - nrcp_dir1.y;
+        const vfloat8 t_upper_z = t_lower_z - nrcp_dir1.z;
+
+        const vfloat8 tNear = max(mini(t_lower_x,t_upper_x),mini(t_lower_y,t_upper_y),mini(t_lower_z,t_upper_z),vfloat8(ray.tnear()));
+        const vfloat8 tFar  = min(maxi(t_lower_x,t_upper_x),maxi(t_lower_y,t_upper_y),maxi(t_lower_z,t_upper_z),vfloat8(ray.tfar));
+        vbool8 valid = (vint8(step) < vint8(prim.N)) & (tNear <= tFar);
+
+#endif
+
+#if EMBREE_HAIR_LEAF_MODE == 1
+
+        const AffineSpace3fa space(Vec3fa::loadu(&prim.space.l.vx),
+                                   Vec3fa::loadu(&prim.space.l.vy),
+                                   Vec3fa::loadu(&prim.space.l.vz),
+                                   Vec3fa::loadu(&prim.space.p));
+        
+        const Vec3fa org1 = xfmPoint (space,ray.org);
+        const Vec3fa dir1 = xfmVector(space,ray.dir);
+        const Vec3fa rcp_dir1 = rcp(dir1);
+        const size_t N = prim.N;
+        
+        const vfloat8 t_lower_x = (vfloat8::load(prim.lower_x(N))-vfloat8(org1.x))*vfloat8(rcp_dir1.x);
+        const vfloat8 t_upper_x = (vfloat8::load(prim.upper_x(N))-vfloat8(org1.x))*vfloat8(rcp_dir1.x);
+        const vfloat8 t_lower_y = (vfloat8::load(prim.lower_y(N))-vfloat8(org1.y))*vfloat8(rcp_dir1.y);
+        const vfloat8 t_upper_y = (vfloat8::load(prim.upper_y(N))-vfloat8(org1.y))*vfloat8(rcp_dir1.y);
+        const vfloat8 t_lower_z = (vfloat8::load(prim.lower_z(N))-vfloat8(org1.z))*vfloat8(rcp_dir1.z);
+        const vfloat8 t_upper_z = (vfloat8::load(prim.upper_z(N))-vfloat8(org1.z))*vfloat8(rcp_dir1.z);
+
+        const vfloat8 tNear = max(mini(t_lower_x,t_upper_x),mini(t_lower_y,t_upper_y),mini(t_lower_z,t_upper_z),vfloat8(ray.tnear()));
+        const vfloat8 tFar  = min(maxi(t_lower_x,t_upper_x),maxi(t_lower_y,t_upper_y),maxi(t_lower_z,t_upper_z),vfloat8(ray.tfar));
+        vbool8 valid = (vint8(step) < vint8(prim.N)) & (tNear <= tFar);
+#endif
+
+#if EMBREE_HAIR_LEAF_MODE == 2
+
+        const size_t N = prim.N;
+        const Vec3fa offset = Vec3fa(vfloat4::loadu(prim.offset(N)));
+        const Vec3fa scale = Vec3fa(vfloat4::loadu(prim.scale(N)));
+        const Vec3fa org1 = (ray.org-offset)*scale;
+        const Vec3fa dir1 = ray.dir*scale;
+
+        const LinearSpace3vf8 space(vfloat8::load(prim.bounds_vx_x(N)), vfloat8::load(prim.bounds_vx_y(N)), vfloat8::load(prim.bounds_vx_z(N)),
+                                    vfloat8::load(prim.bounds_vy_x(N)), vfloat8::load(prim.bounds_vy_y(N)), vfloat8::load(prim.bounds_vy_z(N)),
+                                    vfloat8::load(prim.bounds_vz_x(N)), vfloat8::load(prim.bounds_vz_y(N)), vfloat8::load(prim.bounds_vz_z(N)));
+
+        const Vec3vf8 dir2 = xfmVector(space,Vec3vf8(dir1));
+        const Vec3vf8 org2 = xfmPoint (space,Vec3vf8(org1));
+        const Vec3vf8 rcp_dir2 = rcp(dir2);
+       
+        const vfloat8 t_lower_x = (vfloat8::load(prim.bounds_vx_lower(N))-vfloat8(org2.x))*vfloat8(rcp_dir2.x);
+        const vfloat8 t_upper_x = (vfloat8::load(prim.bounds_vx_upper(N))-vfloat8(org2.x))*vfloat8(rcp_dir2.x);
+        const vfloat8 t_lower_y = (vfloat8::load(prim.bounds_vy_lower(N))-vfloat8(org2.y))*vfloat8(rcp_dir2.y);
+        const vfloat8 t_upper_y = (vfloat8::load(prim.bounds_vy_upper(N))-vfloat8(org2.y))*vfloat8(rcp_dir2.y);
+        const vfloat8 t_lower_z = (vfloat8::load(prim.bounds_vz_lower(N))-vfloat8(org2.z))*vfloat8(rcp_dir2.z);
+        const vfloat8 t_upper_z = (vfloat8::load(prim.bounds_vz_upper(N))-vfloat8(org2.z))*vfloat8(rcp_dir2.z);
+
+        const vfloat8 tNear = max(mini(t_lower_x,t_upper_x),mini(t_lower_y,t_upper_y),mini(t_lower_z,t_upper_z),vfloat8(ray.tnear()));
+        const vfloat8 tFar  = min(maxi(t_lower_x,t_upper_x),maxi(t_lower_y,t_upper_y),maxi(t_lower_z,t_upper_z),vfloat8(ray.tfar));
+        vbool8 valid = (vint8(step) < vint8(prim.N)) & (tNear <= tFar);
+        
+#endif
+        
+        while (any(valid))
+        {
+          size_t i = select_min(valid,tNear);
+          clear(valid,i);
+        
+          STAT3(normal.trav_prims,1,1,1);
+          const unsigned int geomID = prim.geomID(N)[i];
+          const unsigned int primID = prim.primID(N)[i];
+          const NativeCurves* geom = (NativeCurves*) context->scene->get(geomID);
+          Vec3fa a0,a1,a2,a3; geom->gather(a0,a1,a2,a3,geom->curve(primID));
+          if (likely(geom->subtype == FLAT_CURVE)) {
+            if (pre.intersectorHair.intersect(ray,a0,a1,a2,a3,geom->tessellationRate,Occluded1EpilogMU<VSIZEX,true>(ray,context,geomID,primID)))
+              return true;
+          } else {
+            if (pre.intersectorCurve.intersect(ray,a0,a1,a2,a3,Occluded1Epilog1<true>(ray,context,geomID,primID)))
+              return true;
+          }
+
+          valid &= tNear <= vfloat8(ray.tfar);
+        }
         return false;
       }
     };
