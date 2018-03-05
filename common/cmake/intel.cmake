3648854745e685cefba0fea58d6080966a986407
Chuck Atkins <chuck.atkins@kitware.com>
2018-03-05 09:26:30 -0600

    Move SSE2 options from CMAKE_CXX_FLAGS to common target properties

--- common/cmake/intel.cmake
+++ common/cmake/intel.cmake
@@ -100,7 +100,7 @@ ELSE()
     SET(CMAKE_CXX_FLAGS "")
   ENDIF()
 
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAGS_SSE2}") 
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}") 
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")                       # enables most warnings
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wformat -Wformat-security")  # enables string format vulnerability warnings
   IF (NOT APPLE)
