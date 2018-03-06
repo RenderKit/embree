cb61322db3bb7082caed21913ad14869b436fe78
Sven Woop <sven.woop@intel.com>
2018-03-06 11:54:41 +0100

    compile fix for GCC 8

--- common/simd/vboolf4_sse2.h
+++ common/simd/vboolf4_sse2.h
@@ -119,7 +119,7 @@ namespace embree
 
   template<int i0, int i1, int i2, int i3>
   __forceinline vboolf4 shuffle(const vboolf4& v) {
-    return _mm_shuffle_epi32(v, _MM_SHUFFLE(i3, i2, i1, i0));
+    return _mm_castsi128_ps(_mm_shuffle_epi32(v, _MM_SHUFFLE(i3, i2, i1, i0)));
   }
 
   template<int i0, int i1, int i2, int i3>
