df9fee100e0ae12b203820d137a517a9ce489159
Sven Woop <sven.woop@intel.com>
2018-03-08 14:48:00 +0100

    fixed windows compile warnings

--- kernels/bvh/bvh_builder_sah.cpp
+++ kernels/bvh/bvh_builder_sah.cpp
@@ -433,7 +433,7 @@ namespace embree
                                                       {
                                                         BBox3fa bounds = empty;
                                                         if (!mesh->buildBounds(g,x,y,&bounds)) continue; // get bounds of subgrid
-                                                        const PrimRef prim(bounds,mesh->geomID,p_index);
+                                                        const PrimRef prim(bounds,mesh->geomID,unsigned(p_index));
                                                         pinfo.add_center2(prim);
                                                         sgrids[p_index] = SubGridBuildData(x | g.get3x3FlagsX(x), y | g.get3x3FlagsY(y), j);
                                                         prims[p_index++] = prim;                
@@ -477,7 +477,7 @@ namespace embree
                                              {
                                                BBox3fa bounds = empty;
                                                if (!mesh->buildBounds(g,x,y,&bounds)) continue; // get bounds of subgrid
-                                               const PrimRef prim(bounds,mesh->geomID,p_index);
+                                               const PrimRef prim(bounds,mesh->geomID,unsigned(p_index));
                                                pinfo.add_center2(prim);
                                                sgrids[p_index] = SubGridBuildData(x | g.get3x3FlagsX(x), y | g.get3x3FlagsY(y), j);
                                                prims[p_index++] = prim;                
