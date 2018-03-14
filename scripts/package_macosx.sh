e4476f3c1f7fd098fdb5f1b5a7053bce381a23ac
Sven Woop <sven.woop@intel.com>
2018-03-14 07:31:42 +0100

    bugfix in signing PKG

--- scripts/package_macosx.sh
+++ scripts/package_macosx.sh
@@ -5,27 +5,12 @@ set -e
 
 CONFIG=$1
 PACKAGE=$2
-PACKAGE_BASE=`basename $PACKAGE .dmg`
 EMBREE_SIGN_FILE=$3
 
 # create package
 cmake --build . --config $CONFIG --target package
 
-if [ $PACKAGE_BASE == $PACKAGE ]; then
-    exit 0
+# sign package
+if [ -n "${EMBREE_SIGN_FILE}" ]; then
+  ${EMBREE_SIGN_FILE} -c embree -vv $PACKAGE
 fi
-
-if [ -z "${EMBREE_SIGN_FILE}" ]; then
-    exit 0
-fi
-
-if [ -d "$PACKAGE.pkg" ]; then
-    rm -r $PACKAGE.pkg
-fi
-
-hdiutil attach $PACKAGE
-cp -r /Volumes/$PACKAGE_BASE/$PACKAGE_BASE.pkg $PACKAGE_BASE.pkg
-hdiutil detach /Volumes/$PACKAGE_BASE
-${EMBREE_SIGN_FILE} -c embree -vv $PACKAGE_BASE.pkg
-rm $PACKAGE
-hdiutil create -fs HFS+ -srcfolder "$PACKAGE_BASE.pkg" -volname "$PACKAGE_BASE" "$PACKAGE_BASE.dmg"
