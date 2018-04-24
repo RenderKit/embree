d8bb8524d9f5608c8ee29a22981aa7a6de5e41eb
Sven Woop <sven.woop@intel.com>
2018-04-24 08:41:05 +0200

    added hermite basis to XML loader

--- tutorials/common/scenegraph/scenegraph.cpp
+++ tutorials/common/scenegraph/scenegraph.cpp
@@ -426,7 +426,9 @@ namespace embree
       if (p.size() != N) 
         THROW_RUNTIME_ERROR("incompatible vertex array sizes");
 
-    if (type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_BEZIER_CURVE || type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_BSPLINE_CURVE)
+    if (type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_BEZIER_CURVE ||
+        type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_BSPLINE_CURVE ||
+        type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_HERMITE_CURVE)
     {
       for (const auto& n : normals) 
         if (n.size() != N) 
@@ -438,10 +440,38 @@ namespace embree
         THROW_RUNTIME_ERROR("normal array not supported for this geometry type");
     }
 
-    for (auto hair : hairs)
-      if (size_t(hair.vertex) >= N)
-        THROW_RUNTIME_ERROR("invalid hair");
+    if (type == RTC_GEOMETRY_TYPE_FLAT_HERMITE_CURVE ||
+        type == RTC_GEOMETRY_TYPE_ROUND_HERMITE_CURVE ||
+        type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_HERMITE_CURVE)
+    {
+      for (const auto& t : tangents) 
+        if (t.size() != N) 
+          THROW_RUNTIME_ERROR("incompatible tangent array size");
+    }
+    else
+    {
+      if (tangents.size())
+        THROW_RUNTIME_ERROR("tangent array not supported for this geometry type");
+    }
 
+    if (type == RTC_GEOMETRY_TYPE_FLAT_LINEAR_CURVE ||
+        //type == RTC_GEOMETRY_TYPE_ROUND_LINEAR_CURVE ||
+        //type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_LINEAR_CURVE ||
+        type == RTC_GEOMETRY_TYPE_FLAT_HERMITE_CURVE ||
+        type == RTC_GEOMETRY_TYPE_ROUND_HERMITE_CURVE ||
+        type == RTC_GEOMETRY_TYPE_NORMAL_ORIENTED_HERMITE_CURVE)
+    {
+      for (auto hair : hairs)
+        if (size_t(hair.vertex+1) >= N)
+          THROW_RUNTIME_ERROR("invalid hair");
+    }
+    else 
+    {
+      for (auto hair : hairs)
+        if (size_t(hair.vertex+3) >= N)
+          THROW_RUNTIME_ERROR("invalid hair");
+    }
+    
     if (flags.size() != 0 && flags.size() != hairs.size())
       THROW_RUNTIME_ERROR("size of flags array does not match size of curve array");
   }
