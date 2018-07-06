8507bf0945d6acba0602fea2653c107d00b732d7
Johannes Günther <johannes.guenther@intel.com>
2018-07-06 13:39:28 +0200

    This was not the right fix for unresolved externals
    This reverts commit ff64407196527d99e30816a5f14ece84cce4f307.

--- common/cmake/intel.cmake
+++ common/cmake/intel.cmake
@@ -88,12 +88,12 @@ IF (WIN32)
 
   # remove libmmd dependency
   IF (NOT EMBREE_STATIC_RUNTIME)
-    STRING(APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG " /nodefaultlib:libmmdd.lib libmmt.lib")
-    STRING(APPEND CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO " /nodefaultlib:libmmdd.lib libmmt.lib")
-    STRING(APPEND CMAKE_EXE_LINKER_FLAGS_RELEASE " /nodefaultlib:libmmd.lib libmmt.lib")
-    STRING(APPEND CMAKE_SHARED_LINKER_FLAGS_DEBUG " /nodefaultlib:libmmdd.lib libmmt.lib")
-    STRING(APPEND CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO " /nodefaultlib:libmmdd.lib libmmt.lib")
-    STRING(APPEND CMAKE_SHARED_LINKER_FLAGS_RELEASE " /nodefaultlib:libmmd.lib libmmt.lib")
+    STRING(APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG " /nodefaultlib:libmmdd.lib")
+    STRING(APPEND CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO " /nodefaultlib:libmmdd.lib")
+    STRING(APPEND CMAKE_EXE_LINKER_FLAGS_RELEASE " /nodefaultlib:libmmd.lib")
+    STRING(APPEND CMAKE_SHARED_LINKER_FLAGS_DEBUG " /nodefaultlib:libmmdd.lib")
+    STRING(APPEND CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO " /nodefaultlib:libmmdd.lib")
+    STRING(APPEND CMAKE_SHARED_LINKER_FLAGS_RELEASE " /nodefaultlib:libmmd.lib")
   ENDIF()
 
 ELSE()
