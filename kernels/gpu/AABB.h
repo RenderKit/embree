574ac598b1428f4d34db27090d3d9ee237eaaac0
cbenthin <carsten.benthin@intel.com>
2019-05-21 21:53:02 +0200

    updates

--- kernels/gpu/AABB.h
+++ kernels/gpu/AABB.h
@@ -24,9 +24,50 @@ namespace embree
 {
   namespace gpu
   {
-    struct AABB {
+    class AABB {
+    public:
       cl::sycl::float4 lower;
       cl::sycl::float4 upper;
+      AABB() = default;
+
+      inline void init()
+      {
+	const float pos_inf =  INFINITY;
+	const float neg_inf = -INFINITY;       
+	lower = (cl::sycl::float4)(pos_inf,pos_inf,pos_inf,0);
+	upper = (cl::sycl::float4)(neg_inf,neg_inf,neg_inf,0);	
+      }
+
+      inline void extend(class AABB &aabb)
+      {
+	lower = min(lower,aabb.lower);
+	upper = max(upper,aabb.upper);	
+      }
+
+      inline void extend(const cl::sycl::float4 &v)
+      {
+	lower = min(lower,v);
+	upper = max(upper,v);	
+      }
+
+      inline void enlarge(const cl::sycl::float4 &v)
+      {
+	lower -= v;
+	upper += v;	
+      }
+
+      inline cl::sycl::float4 size()
+      {
+	return upper - lower;
+      }
+
+      inline cl::sycl::float4 centroid2()
+      {
+	return upper + lower;
+      }
+
+      
+
     };
   };
 };
