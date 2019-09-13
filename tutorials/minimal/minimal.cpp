fe3100076aba26dd9276d43372fa1f2593f448d4
Sven Woop <sven.woop@intel.com>
2019-09-13 08:20:14 +0200

    Changed copyright from 2018 to 2019

--- tutorials/minimal/minimal.cpp
+++ tutorials/minimal/minimal.cpp
@@ -1,4 +1,4 @@
-/* ======================================================================== //
+// ======================================================================== //
 // Copyright 2009-2019 Intel Corporation                                    //
 //                                                                          //
 // Licensed under the Apache License, Version 2.0 (the "License");          //
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
 // See the License for the specific language governing permissions and      //
 // limitations under the License.                                           //
-// ======================================================================== */
+// ======================================================================== //
 
 #include <embree3/rtcore.h>
 #include <stdio.h>
--- tutorials/multi_instanced_geometry/geometry/ground.cpp
+++ tutorials/multi_instanced_geometry/geometry/ground.cpp
@@ -1,3 +1,19 @@
+// ======================================================================== //
+// Copyright 2009-2019 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
 namespace Ground {
   static const float vertices[] = {
     -92.0791f, -9.47014f, -1.19209e-07f, -90.5184f, -9.47014f, 18.7279f,
