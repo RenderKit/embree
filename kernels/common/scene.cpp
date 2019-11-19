46602e8cd46e704029e6d07d21cdf3d7c90f0c80
Sean C McDuffee <sean.mcduffee@intel.com>
2019-11-19 15:52:34 -0500

    patch internal tasking section for modified check

--- kernels/common/scene.cpp
+++ kernels/common/scene.cpp
@@ -790,10 +790,11 @@ namespace embree
       
       scheduler->join();
       return;
+    } else {
+      checkIfModifiedAndSet ();
     }
 
     /* fast path for unchanged scenes */
-    checkIfModifiedAndSet ();
     if (!isModified()) {
       scheduler->spawn_root([&]() { Lock<MutexSys> lock(schedulerMutex); this->scheduler = nullptr; }, 1, !join);
       return;
