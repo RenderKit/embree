e53bd68813a62910f552cecfc7885b138529fa96
Sven Woop <sven.woop@intel.com>
2019-11-29 11:45:11 +0100

    testing usm

--- tutorials/sycl_test/sycl_test.cpp
+++ tutorials/sycl_test/sycl_test.cpp
@@ -28,8 +28,7 @@ void exception_handler(cl::sycl::exception_list exceptions)
     try {
       std::rethrow_exception(e);
     } catch(cl::sycl::exception const& e) {
-      std::cout << "Caught asynchronous SYCL exception:\n" << e.what() << std::endl;
-      exit(1);
+      std::cout << "Caught asynchronous SYCL exception: " << e.what() << std::endl;
     }
   }
 };
@@ -42,12 +41,12 @@ struct Test
 
   virtual ~Test() {}
 
-  virtual bool run(cl::sycl::queue& myQueue) = 0;
+  virtual bool run(cl::sycl::device& myDevice, cl::sycl::context myContext, cl::sycl::queue& myQueue) = 0;
 
-  void invoke(cl::sycl::queue& myQueue)
+  void invoke(cl::sycl::device& myDevice, cl::sycl::context myContext, cl::sycl::queue& myQueue)
   {
     std::cout << name << ": " << std::flush;
-    bool passed = run(myQueue);
+    bool passed = run(myDevice,myContext,myQueue);
     if (passed) std::cout << " [PASSED]" << std::endl;
     else        std::cout << " [FAILED]" << std::endl;
   }
@@ -60,7 +59,7 @@ struct parallel_for_sycl_buffer_test : public Test
   parallel_for_sycl_buffer_test ()
     : Test("parallel_for_sycl_buffer_test") {}
   
-  bool run (cl::sycl::queue& myQueue)
+  bool run (cl::sycl::device& myDevice, cl::sycl::context myContext, cl::sycl::queue& myQueue)
   {
     std::vector<int> h_a(1000);
     std::vector<int> h_b(1000);
@@ -86,6 +85,8 @@ struct parallel_for_sycl_buffer_test : public Test
               c[i] = a[i] + b[i];
             });
         });
+
+      myQueue.wait_and_throw();
     }
     
     for (int i=0; i<1000; i++)
@@ -97,12 +98,99 @@ struct parallel_for_sycl_buffer_test : public Test
     return true;
   }
 };
+
+struct parallel_for_sycl_buffer_test_fail : public Test
+{
+  parallel_for_sycl_buffer_test_fail ()
+    : Test("parallel_for_sycl_buffer_test_fail") {}
+  
+  bool run (cl::sycl::device& myDevice, cl::sycl::context myContext, cl::sycl::queue& myQueue)
+  {
+    std::vector<int> h_a(1000);
+    std::vector<int> h_b(1000);
+    std::vector<int> h_c(1000);
     
+    for (int i=0; i<1000; i++) {
+      h_a[i] = i;
+      h_b[i] = i+5;
+      h_c[i] = 0;
+    }
+    
+    {
+      cl::sycl::buffer<int> d_a(h_a);
+      cl::sycl::buffer<int> d_b(h_b);
+      cl::sycl::buffer<int> d_c(h_c);
+      
+      myQueue.submit([&](cl::sycl::handler& cgh) {
+          auto a = d_a.get_access<cl::sycl::access::mode::read>();
+          auto b = d_b.get_access<cl::sycl::access::mode::read>();
+          auto c = d_c.get_access<cl::sycl::access::mode::write>();
+          cgh.parallel_for<class test>(cl::sycl::range<1>(1000), [=](cl::sycl::id<1> item) {
+              int i = item.get(0);
+              c[i] = a[i] + b[i];
+            });
+        });
+      
+      myQueue.wait_and_throw();
+    }
+    
+    for (int i=0; i<1000; i++)
+    {
+      if (h_a[i]+h_b[i] != h_c[i])
+        return false;
+    }
+    
+    return true;
+  }
+};
+
+struct parallel_for_sycl_aligned_alloc_test : public Test
+{
+  parallel_for_sycl_aligned_alloc_test ()
+    : Test("parallel_for_sycl_aligned_alloc_test") {}
+  
+  bool run (cl::sycl::device& myDevice, cl::sycl::context myContext, cl::sycl::queue& myQueue)
+  {
+    int* a = (int*) cl::sycl::aligned_alloc(64,1000*sizeof(int),myDevice,myContext,cl::sycl::usm::alloc::shared);
+    int* b = (int*) cl::sycl::aligned_alloc(64,1000*sizeof(int),myDevice,myContext,cl::sycl::usm::alloc::shared);
+    int* c = (int*) cl::sycl::aligned_alloc(64,1000*sizeof(int),myDevice,myContext,cl::sycl::usm::alloc::shared);
+
+    for (int i=0; i<1000; i++) {
+      a[i] = i;
+      b[i] = i+5;
+      c[i] = 0;
+    }
+    
+    {
+      myQueue.submit([&](cl::sycl::handler& cgh) {
+          cgh.parallel_for<class test>(cl::sycl::range<1>(1000), [=](cl::sycl::id<1> item) {
+              int i = item.get(0);
+              c[i] = a[i] + b[i];
+            });
+        });
+      myQueue.wait_and_throw();
+    }
+    
+    for (int i=0; i<1000; i++)
+    {
+      if (a[i]+b[i] != c[i])
+        return false;
+    }
+
+    cl::sycl::free(a, myContext);
+    cl::sycl::free(b, myContext);
+    cl::sycl::free(c, myContext);
+    
+    return true;
+  }
+};
+
 int main()
 {
   cl::sycl::device myDevice = cl::sycl::device(NEOGPUDeviceSelector());
   //cl::sycl::device myDevice = cl::sycl::device(CPUDeviceSelector());
   cl::sycl::queue myQueue = cl::sycl::queue(myDevice,exception_handler);
+  cl::sycl::context myContext = myQueue.get_context();
   
   std::cout << "GPU Device: " << myDevice.get_info<cl::sycl::info::device::name>() << std::endl;
   int gpu_maxWorkGroupSize = myDevice.get_info<cl::sycl::info::device::max_work_group_size>();
@@ -112,9 +200,12 @@ int main()
 
   std::vector<std::unique_ptr<Test>> tests;
   tests.push_back(std::unique_ptr<Test>(new parallel_for_sycl_buffer_test()));
+  tests.push_back(std::unique_ptr<Test>(new parallel_for_sycl_buffer_test_fail()));
+  tests.push_back(std::unique_ptr<Test>(new parallel_for_sycl_aligned_alloc_test()));
 
-  for (auto& test : tests)
-    test->invoke(myQueue);
+  for (auto& test : tests) {
+    test->invoke(myDevice,myContext,myQueue);
+  }
  
   return 0;    
 }
