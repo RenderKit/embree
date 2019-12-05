e9e3387ddaf829e594929862162fdfb933a1b586
Sven Woop <sven.woop@intel.com>
2019-12-05 10:52:50 +0100

    updates

--- kernels/bvh/bvh_intersector_stream_gpu_mb.cpp
+++ kernels/bvh/bvh_intersector_stream_gpu_mb.cpp
@@ -14,6 +14,8 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
+#define EMBREE_SYCL_SIMD_LIBRARY
+
 #include "bvh_intersector_gpu.h"
 
 #define DBG(x) 
@@ -26,11 +28,6 @@ namespace embree
 {  
   namespace isa
   {
-
-#if defined(EMBREE_DPCPP_SUPPORT)
-    
-#endif
-    
     /*! BVH ray stream GPU intersector */
     template<typename Primitive>
     class BVHNGPUIntersectorStreamMB
