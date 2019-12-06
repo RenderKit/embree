ce2f38948a8c1053c0a94fb7d61c2e1a96d52f45
Sven Woop <sven.woop@intel.com>
2019-12-06 08:26:58 +0100

    compile fix for MacOSX

--- common/sys/sysinfo.cpp
+++ common/sys/sysinfo.cpp
@@ -551,13 +551,15 @@ namespace embree
     static int nThreads = -1;
     if (nThreads != -1) return nThreads;
 
-    //nThreads = sysconf(_SC_NPROCESSORS_ONLN); // does not work in Linux LXC container
-    //assert(nThreads);
-
+#if defined(__MACOSX__)
+    nThreads = sysconf(_SC_NPROCESSORS_ONLN); // does not work in Linux LXC container
+    assert(nThreads);
+#else
     cpu_set_t set;
     if (pthread_getaffinity_np(pthread_self(), sizeof(set), &set) == 0)
       nThreads = CPU_COUNT(&set);
-
+#endif
+    
     assert(nThreads);
     return nThreads;
   }
