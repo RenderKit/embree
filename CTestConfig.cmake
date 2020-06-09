90d6a8823d7456e54d1531e5717bd8695b4fe079
Sven Woop <sven.woop@intel.com>
2020-06-09 11:24:23 +0200

    updates models hash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,4 +2,4 @@
 ## SPDX-License-Identifier: Apache-2.0
 
 set(CTEST_PROJECT_NAME "Embree")
-set(TEST_MODELS_HASH f7869349babc4000bd3c3fe98a2f09506c3d95e8)
+set(TEST_MODELS_HASH fb6b209fcb17ff6fce661f31557758308d3bae4d)
