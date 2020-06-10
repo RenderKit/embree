e80e96cc30fc44236e22605b7f3ba5789462758f
Sven Woop <sven.woop@intel.com>
2020-06-10 10:55:09 +0200

    using ICC2019 and Visual Studio 2017 for windows release build

--- scripts/test.py
+++ scripts/test.py
@@ -103,9 +103,13 @@ def runConfig(config):
       conf.append("-G \"Visual Studio 15 2017"+ext+"\"")
       conf.append("-T \"V141\"")
       ispc_ext = "-vs2015"
-    elif (compiler == "ICC17-VC141"):
+    elif (compiler == "ICC19-VC141"):
       conf.append("-G \"Visual Studio 15 2017"+ext+"\"")
-      conf.append("-T \"Intel C++ Compiler 17.0\"")
+      conf.append("-T \"Intel C++ Compiler 19.0\"")
+      ispc_ext = "-vs2015"
+    elif (compiler == "ICC18-VC141"):
+      conf.append("-G \"Visual Studio 15 2017"+ext+"\"")
+      conf.append("-T \"Intel C++ Compiler 18.0\"")
       ispc_ext = "-vs2015"
     elif (compiler == "V140"):
       conf.append("-G \"Visual Studio 14 2015"+ext+"\"")
