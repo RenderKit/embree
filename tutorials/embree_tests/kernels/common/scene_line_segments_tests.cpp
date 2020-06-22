2f3e165211ce130f292d784e067eb061d79a2b85
Sean C McDuffee <sean.mcduffee@intel.com>
2020-06-22 17:23:49 -0400

    remove test for now need to get proper static libs into test

--- tutorials/embree_tests/kernels/common/scene_line_segments_tests.cpp
+++ tutorials/embree_tests/kernels/common/scene_line_segments_tests.cpp
@@ -14,13 +14,13 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#include "../../../external/catch.hpp"
+// #include "../../../external/catch.hpp"
 // #include "../../../../kernels/common/scene_line_segments.h"
 
-using namespace embree;
+// using namespace embree;
 
-TEST_CASE ("Segment Left/Right exists tests - Issue 726", "[scene_line_segments]") 
-{
-  // embree::Device device ("");
-  // LineSegments lines (&device, Geometry::GTY_ROUND_LINEAR_CURVE);
-}
+// TEST_CASE ("Segment Left/Right exists tests - Issue 726", "[scene_line_segments]") 
+// {
+//   // embree::Device device ("");
+//   // LineSegments lines (&device, Geometry::GTY_ROUND_LINEAR_CURVE);
+// }
