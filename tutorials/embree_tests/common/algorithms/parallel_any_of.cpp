dabd22861389b11209965ca04a1e435b94867755
Florian Reibold <florian.reibold@intel.com>
2020-09-02 21:05:40 +0200

    add unit test for parallel_any_of algorithm.

---
+++ tutorials/embree_tests/common/algorithms/parallel_any_of.cpp
@@ -0,0 +1,36 @@
+// Copyright 2009-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "../../../external/catch.hpp"
+#include "../common/tasking/taskscheduler.h"
+#include "../common/algorithms/parallel_any_of.h"
+
+#include <vector>
+#include <numeric>
+#include <thread>
+
+using namespace embree;
+
+namespace __parallel_any_of_unit_tests_internal {
+
+TEST_CASE ("Test parallel any of", "[parallel_any_of]")
+{
+  const size_t num_threads = std::thread::hardware_concurrency();
+  TaskScheduler::create(num_threads, true, false);
+
+  std::vector<int> data(1024);
+  std::iota(data.begin(), data.end(), 0);
+
+  std::shuffle(data.begin(), data.end(), std::mt19937{7777});
+
+  auto unaryPredicateTrue  = [&](size_t i) -> bool { return data[i] == 512; };
+  auto unaryPredicateFalse = [&](size_t i) -> bool { return data[i] == 1048; };
+
+  bool resultUnaryPredicateTrue  = parallel_any_of(size_t(0), data.size(), unaryPredicateTrue);
+  bool resultUnaryPredicateFalse = parallel_any_of(size_t(0), data.size(), unaryPredicateFalse);
+
+  REQUIRE(resultUnaryPredicateTrue);
+  REQUIRE(!resultUnaryPredicateFalse);
+}
+
+}
---
+++ tutorials/embree_tests/common/common_tests.cpp
@@ -0,0 +1,4 @@
+// Copyright 2009-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "algorithms/algorithms_tests.cpp"
