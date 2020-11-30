835f2b57a96f9d69ce0e702e2f2d183ad09c4236
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2020-11-30 15:13:48 +0100

    ci: increase severity in klocwork check script

--- scripts/klocwork_check.sh
+++ scripts/klocwork_check.sh
@@ -9,7 +9,7 @@ export KW_PROJECT_NAME=embree
 export KW_CRITICAL_OUTPUT_PATH=./kw_critical.out
 
 echo "Checking for critical issues in $KW_BUILD_NUMBER ..."
-no_proxy=$KW_SERVER_IP curl -f --data "action=search&project=$KW_PROJECT_NAME&query=build:'$KW_BUILD_NUMBER'%20severity:Critical%20status:Analyze,Fix&user=$KW_USER&ltoken=$KW_LTOKEN" http://$KW_SERVER_IP:$KW_SERVER_PORT/review/api -o $KW_CRITICAL_OUTPUT_PATH
+no_proxy=$KW_SERVER_IP curl -f --data "action=search&project=$KW_PROJECT_NAME&query=build:'$KW_BUILD_NUMBER'%20severity:Error,Critical%20status:Analyze,Fix&user=$KW_USER&ltoken=$KW_LTOKEN" http://$KW_SERVER_IP:$KW_SERVER_PORT/review/api -o $KW_CRITICAL_OUTPUT_PATH
 getCriticalCount() {
     cat $KW_CRITICAL_OUTPUT_PATH | wc -l
 }
