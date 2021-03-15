79e7b231525c2944a91406a5607c720cd21391c2
Florian Reibold <florian.reibold@intel.com>
2021-03-15 20:43:57 +0100

    insert suites and subsuites

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -44,8 +44,13 @@ RUN_TUT_BENCHMARKS=true
 #
 ################################# PLEASE READ ###################################
 
-benny insert code_context "${PROJECT_NAME}" ${SOURCE_ROOT} --save-json code_context.json
-benny insert run_context ${TOKEN} ./code_context.json --save-json run_context.json
+initContext() {
+  if [ -z "$HAVE_CONTEXT" ]; then
+    HAVE_CONTEXT=1
+    benny insert code_context "${PROJECT_NAME}" ${SOURCE_ROOT} --save-json code_context.json
+    benny insert run_context ${TOKEN} ./code_context.json --save-json run_context.json
+  fi
+}
 
 ####################
 # build benchmarks #
@@ -53,6 +58,7 @@ benny insert run_context ${TOKEN} ./code_context.json --save-json run_context.js
 
 if ${RUN_BUILD_BENCHMARKS}; then
   SUITE_NAME="Build"
+  benny insert suite ${PROJECT_NAME} ${SUITE_NAME}
 
   ## subsuites of build benchmarks
   subsuites="update_dynamic_deformable \
@@ -75,8 +81,9 @@ if ${RUN_BUILD_BENCHMARKS}; then
     for i in ${subsuites}
     do
       SUBSUITE_NAME=$i
-      benny insert suite ${PROJECT_NAME} ${SUITE_NAME}
       benny insert subsuite ${PROJECT_NAME} ${SUITE_NAME} ${SUBSUITE_NAME}
+
+      initContext
 
       echo "${NUMACTL} ./buildbench --benchmark_out=results-${SUITE_NAME}-${SUBSUITE_NAME}.json ${BENCHMARK} --benchmark_type ${SUBSUITE_NAME} -i ${SCENE} ${array[@]} ${THREADS}"
       ${NUMACTL} ./buildbench --benchmark_out=results-${SUITE_NAME}-${SUBSUITE_NAME}.json ${BENCHMARK} --benchmark_type ${SUBSUITE_NAME} -i ${SCENE} ${array[@]} ${THREADS}
@@ -91,6 +98,7 @@ fi
 
 if ${RUN_TRACE_BENCHMARKS}; then
   SUITE_NAME="Trace"
+  benny insert suite ${PROJECT_NAME} ${SUITE_NAME}
 
   while IFS= read -r line
   do
@@ -105,17 +113,16 @@ if ${RUN_TRACE_BENCHMARKS}; then
     unset "array[1]"
 
     SUBSUITE_NAME="Viewer"
-    benny insert suite ${PROJECT_NAME} ${SUITE_NAME}
     benny insert subsuite ${PROJECT_NAME} ${SUITE_NAME} ${SUBSUITE_NAME}
+    initContext
 
     echo "${NUMACTL} ./viewer --benchmark_out=results-${SUITE_NAME}-${SUBSUITE_NAME}.json ${BENCHMARK} -i ${SCENE} ${array[@]} ${THREADS} --size 256 256"
     ${NUMACTL} ./viewer --benchmark_out=results-${SUITE_NAME}-${SUBSUITE_NAME}.json ${BENCHMARK} -i ${SCENE} ${array[@]} ${THREADS}
     benny insert googlebenchmark ./run_context.json ${SUITE_NAME} ${SUBSUITE_NAME} ./results-${SUITE_NAME}-${SUBSUITE_NAME}.json
 
-
     SUBSUITE_NAME="Pathtracer"
-    benny insert suite ${PROJECT_NAME} ${SUITE_NAME}
     benny insert subsuite ${PROJECT_NAME} ${SUITE_NAME} ${SUBSUITE_NAME}
+    initContext
 
     echo "${NUMACTL} ./pathtracer --benchmark_out=results-${SUITE_NAME}-${SUBSUITE_NAME}.json ${BENCHMARK} -i ${SCENE} ${array[@]} ${THREADS} --size 256 256 --spp 32"
     ${NUMACTL} ./pathtracer --benchmark_out=results-${SUITE_NAME}-${SUBSUITE_NAME}.json ${BENCHMARK} -i ${SCENE} ${array[@]} ${THREADS}
@@ -125,8 +132,8 @@ if ${RUN_TRACE_BENCHMARKS}; then
 
   if ${RUN_TUT_BENCHMARKS}; then
     SUBSUITE_NAME="Tutorials"
-    benny insert suite ${PROJECT_NAME} ${SUITE_NAME}
     benny insert subsuite ${PROJECT_NAME} ${SUITE_NAME} ${SUBSUITE_NAME}
+    initContext
 
     tutorials="triangle_geometry \
                grid_geometry \
