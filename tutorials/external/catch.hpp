03950fd93b5b7630e54c8771b2b9616b154b10a3
Sven Woop <sven.woop@intel.com>
2022-03-07 18:35:39 +0100

    bugfix to compile with recent GCC

--- tutorials/external/catch.hpp
+++ tutorials/external/catch.hpp
@@ -8750,7 +8750,7 @@ namespace Catch {
 
     // 32kb for the alternate stack seems to be sufficient. However, this value
     // is experimentally determined, so that's not guaranteed.
-    constexpr static std::size_t sigStackSize = 32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ;
+    constexpr static std::size_t sigStackSize = 32768; //32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ;
 
     static SignalDefs signalDefs[] = {
         { SIGINT,  "SIGINT - Terminal interrupt signal" },
