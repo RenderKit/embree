9b9f863f9495b20ee952ababa2d34047a4234b72
Sven Woop <sven.woop@intel.com>
2022-04-29 10:39:31 +0200

    updated model hash to devel branch of model repo

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,4 +2,4 @@
 ## SPDX-License-Identifier: Apache-2.0
 
 set(CTEST_PROJECT_NAME "Embree")
-set(TEST_MODELS_HASH 431bba098be39a66d586ef049ae756213e6a5ac8)
+set(TEST_MODELS_HASH b1098cbc2e5e17d8abde1b666112c332acdedfd5)
