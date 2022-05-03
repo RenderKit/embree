184332d75aca805a19ff45f9cc936bfaf046a180
Sven Woop <sven.woop@intel.com>
2022-05-03 07:58:11 +0200

    extending download_gfx script to support agama releases

--- scripts/download_gfx.sh
+++ scripts/download_gfx.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -z $3 ]; then
-  echo "ERROR: usage $0 ARTIFACTORY_TOKEN version target-directory (version: master/VERSION_NUMBER, comp_igc/VERSION_NUMBER, or neo/VERSION_NUMBER)"
+  echo "ERROR: usage $0 ARTIFACTORY_TOKEN version target-directory (version: agama/VERSION_NUMBER, master/VERSION_NUMBER, master_release/VERSION_NUMBER, comp_igc/VERSION_NUMBER, neo/VERSION_NUMBER)"
   exit 1
 fi
 
@@ -16,27 +16,20 @@ GFX_VERSION_NUMBER=$(echo $VERSION | sed 's/.*\///')
 [ -d "${DST_DIR}/${VERSION}" ] && { echo "GFX version ${VERSION} already downloaded in ${DST_DIR}"; exit 0; }
 
 URL=""
-DEBS="*"
-if [[ "${GFX_TYPE}" == "master" ]]; then
-  URL="https://gfx-assets-build.igk.intel.com/artifactory/gfx-driver-builds/ci/master/gfx-driver-ci-master-${GFX_VERSION_NUMBER}/artifacts/Linux/Ubuntu/20.04/ReleaseInternal/"
-  DEBS="level-zero|intel-level-zero-gpu|intel-opencl-icd|libigc1|libigc-tools|libigdfcl1|libigdgmm"
+DEBS="level-zero|intel-level-zero-gpu|intel-opencl-icd|libigc1|libigc-tools|libigdfcl1|libigdgmm"
+
+if [[ "${GFX_TYPE}" == "agama" ]]; then
+  URL="https://gfx-assets-build.intel.com/artifactory/agama-builds/ci/prerelease/agama-ci-prerelease-${GFX_VERSION_NUMBER}/artifacts/linux/ubuntu/20.04/"
+elif [[ "${GFX_TYPE}" == "master_release" ]]; then
+  URL="https://gfx-assets-build.igk.intel.com/artifactory/gfx-driver-builds/ci/master/gfx-driver-ci-master-${GFX_VERSION_NUMBER}/artifacts/Linux/Ubuntu/20.04/Release/" 
+elif [[ "${GFX_TYPE}" == "master" ]]; then
+  URL="https://gfx-assets-build.igk.intel.com/artifactory/gfx-driver-builds/ci/master/gfx-driver-ci-master-${GFX_VERSION_NUMBER}/artifacts/Linux/Ubuntu/20.04/ReleaseInternal/" 
 elif [[ "${GFX_TYPE}" == "comp_igc" ]]; then
   URL="https://gfx-assets-build.igk.intel.com/artifactory/gfx-driver-builds/ci/comp_igc/gfx-driver-ci-comp_igc-${GFX_VERSION_NUMBER}/artifacts/Linux/Ubuntu/20.04/ReleaseInternal/"
-  if [[ "${GFX_VERSION_NUMBER}" -gt "9423" ]]; then
-    echo "gfx verison ${GFX_VERSION_NUMBER} is greater then 9423"
-    DEBS="level-zero|intel-level-zero-gpu|intel-opencl-icd|libigc1|libigc-tools|libigdfcl1|libigdgmm"
-  else
-    echo "gfx verison ${GFX_VERSION_NUMBER} is not greater then 9423"
-    DEBS="intel-gmmlib|intel-igc-core|intel-igc-opencl|intel-level-zero-gpu|intel-ocloc|intel-opencl"
-  fi
 elif [[ "${GFX_TYPE}" == "neo" ]]; then
   URL="https://gfx-assets-build.intel.com/artifactory/neo-builds/ci/embargo/ci-neo-${GFX_VERSION_NUMBER}/artifacts/Linux/Ubuntu/20.04/Release/"
-  DEBS="intel-gmmlib|intel-igc-core|intel-igc-opencl|intel-level-zero-gpu|intel-ocloc|intel-opencl"
-elif [[ "${GFX_TYPE}" == "neo" ]]; then
-  URL="https://gfx-assets-build.intel.com/artifactory/neo-builds/ci/embargo/ci-neo-${GFX_VERSION_NUMBER}/artifacts/Linux/Ubuntu/20.04/Release/"
-  DEBS="intel-gmmlib|intel-igc-core|intel-igc-opencl|intel-level-zero-gpu|intel-ocloc|intel-opencl"
 else
-  echo "Error GFX version ${VERSION} not recognized because of type (only master, igc_comp and neo allowed)."
+  echo "Error GFX version ${VERSION} not recognized because of type (only master, comp_igc, neo, and agama allowed)."
 fi
 
 CUR_DIR=$(pwd)
