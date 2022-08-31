543924e46f9aa0003f9b741fc8c483c810d03b1f
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2022-08-31 16:19:05 +0200

    TBB: Stop task_scheduler_observer when TaskScheduler is destroyed

--- common/tasking/taskschedulertbb.cpp
+++ common/tasking/taskschedulertbb.cpp
@@ -12,17 +12,17 @@ namespace embree
 #else
   static tbb::task_scheduler_init g_tbb_threads(tbb::task_scheduler_init::deferred);
 #endif
-  
+
   class TBBAffinity: public tbb::task_scheduler_observer
   {
   public:
-    
+
     void on_scheduler_entry( bool ) {
-      setAffinity(TaskScheduler::threadIndex()); 
+      setAffinity(TaskScheduler::threadIndex());
     }
-    
+
   } tbb_affinity;
-  
+
   void TaskScheduler::create(size_t numThreads, bool set_affinity, bool start_threads)
   {
     assert(numThreads);
@@ -37,13 +37,13 @@ namespace embree
 #endif
       g_tbb_threads_initialized = false;
     }
-    
+
     /* only set affinity if requested by the user */
 #if TBB_INTERFACE_VERSION >= 9000 // affinity not properly supported by older TBB versions
     if (set_affinity)
       tbb_affinity.observe(true);
-#endif 
-    
+#endif
+
     /* now either keep default settings or configure number of threads */
     if (numThreads == std::numeric_limits<size_t>::max()) {
       numThreads = threadCount();
@@ -60,7 +60,7 @@ namespace embree
     }
 
     /* start worker threads */
-    if (start_threads) 
+    if (start_threads)
     {
       BarrierSys barrier(numThreads);
       tbb::parallel_for(size_t(0), size_t(numThreads), size_t(1), [&] ( size_t i ) {
@@ -68,9 +68,16 @@ namespace embree
         });
     }
   }
-  
+
   void TaskScheduler::destroy()
   {
+#if TBB_INTERFACE_VERSION >= 9000 // affinity not properly supported by older TBB versions
+    // Stop observe to prevent calling on_scheduler_entry
+    // when static objects are already destroyed.
+    if (tbb_affinity.is_observing())
+      tbb_affinity.observe(false);
+#endif
+
     if (g_tbb_threads_initialized) {
 #if TBB_INTERFACE_VERSION >= 11005
       delete g_tbb_thread_control;
