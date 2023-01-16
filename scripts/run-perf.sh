86ae9b0e15a1b24c6a9576037764eb97bee2c296
Tomasz Pyra <tomasz.pyra@intel.com>
2023-01-16 21:23:44 +0100

    new perf CI runners, added asia_dragon workload (#2440)

--- scripts/run-perf.sh
+++ scripts/run-perf.sh
@@ -8,17 +8,23 @@ unzip -u -q crown.zip
 if [ "$1" != "gpu" ]; then
   echo "VIEWER CPU"
   ./embree_viewer -c crown/crown.ecs --size 2048 2048 --benchmark 10 100 | grep avg
+  ./embree_viewer -c asian_dragon/asian_dragon.ecs --size 2048 2048 --benchmark 10 100 | grep avg
 
   echo "PATHTRACER CPU"
   ./embree_pathtracer -c crown/crown.ecs --size 2048 2048 --benchmark 5 40 | grep avg
+  ./embree_pathtracer -c asian_dragon/asian_dragon.ecs --size 2048 2048 --benchmark 5 40 | grep avg
 fi
 
 if [ "$1" != "gpu" ]; then
   echo "VIEWER CPU-ispc"
   ./embree_viewer_ispc -c crown/crown.ecs --size 2048 2048 --benchmark 10 100 | grep avg
+  ./embree_viewer_ispc -c asian_dragon/asian_dragon.ecs --size 2048 2048 --benchmark 10 100 | grep avg
+
 
   echo "PATHTRACER CPU-ispc"
   ./embree_pathtracer_ispc -c crown/crown.ecs --size 2048 2048 --benchmark 5 40 | grep avg
+  ./embree_pathtracer_ispc -c asian_dragon/asian_dragon.ecs --size 2048 2048 --benchmark 5 40 | grep avg
+
 fi
 
 
@@ -26,9 +32,11 @@ fi
 if [ "$1" != "cpu" ]; then
   echo "VIEWER GPU"
   ./embree_viewer_sycl -c crown/crown.ecs --size 2048 2048 --benchmark 50 500 | grep avg
+  ./embree_viewer_sycl -c asian_dragon/asian_dragon.ecs --size 2048 2048 --benchmark 50 500 | grep avg
 
   echo "PATHTRACER GPU"
   ./embree_pathtracer_sycl crown/crown.ecs --size 2048 2048 --benchmark 10 100 | grep avg
+  ./embree_pathtracer_sycl asian_dragon/asian_dragon.ecs --size 2048 2048 --benchmark 10 100 | grep avg
 fi
 
 
