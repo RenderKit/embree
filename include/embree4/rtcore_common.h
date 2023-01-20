63e7cf5dd03f4152181790d954dd210bb62e3a0a
Carsten Benthin <carsten.benthin@intel.com>
2023-01-20 10:43:36 +0100

    replaced get_access with get_host_access as former is deprecated

--- include/embree4/rtcore_common.h
+++ include/embree4/rtcore_common.h
@@ -457,7 +457,7 @@ inline decltype(F) rtcGetSYCLDeviceFunctionPointer(sycl::queue& queue)
 {
   sycl::buffer<cl_ulong> fptr_buf(1);
   {
-    auto fptr_acc = fptr_buf.get_access<sycl::access::mode::write>();
+    auto fptr_acc = fptr_buf.get_host_access(); // <sycl::access::mode::write>
     fptr_acc[0] = 0;
   }
 
@@ -469,7 +469,7 @@ inline decltype(F) rtcGetSYCLDeviceFunctionPointer(sycl::queue& queue)
     });
   queue.wait_and_throw();
 
-  auto fptr_acc = fptr_buf.get_access<sycl::access::mode::read>();
+  auto fptr_acc = fptr_buf.get_host_access(); // <sycl::access::mode::read>
   return (decltype(F)) fptr_acc[0];
 }
 
