0d29bfc3b86cb85c3a1918f3eae04c70ac681ce6
Tomasz Pyra <tomasz.pyra@intel.com>
2024-03-01 13:31:20 +0100

    fine tuning google benchmark

--- scripts/run-perf.sh
+++ scripts/run-perf.sh
@@ -18,8 +18,8 @@ ecs_files=($(find $models_dir -name "*.ecs"))
 
 # config
 resolution="2048 2048"
-cpubench="1 10"
-gpubench="10 20"
+cpubench="1 8"
+gpubench="5 20"
 
 if [ "$1" != "gpu" ]; then
   echo "VIEWER CPU"
