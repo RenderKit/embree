78408c3a98ccb3f2202da6930401be72389e02f4
Opitz, Daniel <daniel.opitz@intel.com>
2025-01-17 11:34:02 +0100

    removed internal rtas builder, made EMBREE_SYCL_L0_RTAS_BUILDER the default

--- kernels/level_zero/ze_wrapper.h
+++ kernels/level_zero/ze_wrapper.h
@@ -27,16 +27,10 @@ typedef struct _ze_rtas_builder_build_op_debug_exp_desc_t
 
 struct ZeWrapper
 {
-  enum RTAS_BUILD_MODE {
-    AUTO = 0,       // try L0 implementation first and fallback to internal implementation
-    INTERNAL = 1,   // use internal RTAS build implementation
-    LEVEL_ZERO = 2, // use Level Zero provided RTAS build implementation
-  };
-
   ~ZeWrapper();
 
   static ze_result_t init();
-  static ze_result_t initRTASBuilder(ze_driver_handle_t hDriver, RTAS_BUILD_MODE rtas_build_mode = RTAS_BUILD_MODE::AUTO);
+  static ze_result_t initRTASBuilder(ze_driver_handle_t hDriver);
   
   static ze_result_t zeMemFree(ze_context_handle_t, void*);
   static ze_result_t zeMemAllocHost(ze_context_handle_t, const ze_host_mem_alloc_desc_t*, size_t, size_t, void**);
@@ -66,6 +60,6 @@ struct ZeWrapper
   static ze_result_t zeRTASParallelOperationGetPropertiesExp( ze_rtas_parallel_operation_exp_handle_t hParallelOperation, ze_rtas_parallel_operation_exp_properties_t* pProperties );
   static ze_result_t zeRTASParallelOperationJoinExp( ze_rtas_parallel_operation_exp_handle_t hParallelOperation);
 
-  static RTAS_BUILD_MODE rtas_builder;
+  static bool rtas_builder_selected;
 };
 
