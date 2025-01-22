b75cd82151dea05a2ea3e8d2439567ffcadd7c67
Opitz, Daniel <daniel.opitz@intel.com>
2025-01-22 11:29:35 +0100

    Revert "EMBREE_SYCL_BVH_DATA_HOST_DEVICE turned on by default now"
    This reverts commit d3c165be9ef25db24bf5c42d43d59a297b80d1a6. newest public driver under linux does not fix this issue.

--- kernels/sycl/rthwif_embree_builder.cpp
+++ kernels/sycl/rthwif_embree_builder.cpp
@@ -665,10 +665,10 @@ namespace embree
     {
       throw_RTCError(RTC_ERROR_INVALID_ARGUMENT, "AccelBuffer constructor called with non-GPU device");
     }
-#if defined(EMBREE_SYCL_DISABLE_BVH_DATA_HOST_DEVICE)
-    unifiedMemory = true;
-#else
+#if defined(EMBREE_SYCL_BVH_DATA_HOST_DEVICE)
     unifiedMemory = gpu_device->has_unified_memory();
+#else
+    unifiedMemory = true;
 #endif
 
     if (unifiedMemory)
