e7b9f989611d8358b5a5976bafd8a5c3646734fb
Werner, Stefan <stefan.werner@intel.com>
2025-02-28 16:33:41 +0100

    little cleanup

--- kernels/common/scene_quad_mesh.cpp
+++ kernels/common/scene_quad_mesh.cpp
@@ -140,9 +140,9 @@ namespace embree
     for (unsigned int t=0; t<numTimeSteps; t++) {
       if (vertices[t].getStride() != vertices[0].getStride())
         throw_RTCError(RTC_ERROR_INVALID_OPERATION,"stride of vertex buffers have to be identical for each time step");
-      syncBufferWithDevice(vertices[t]);
+      vertices[t].buffer->commitIfNeeded();
     }
-    syncBufferWithDevice(quads);
+    quads.buffer->commitIfNeeded();
     Geometry::commit();
   }
 
