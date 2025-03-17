49e5042c0098c891a5cb74f896f946d097608fdb
Daniel Simon <DanSimon09@gmail.com>
2025-03-17 10:03:54 -0700

    Added getters for if the scene is modified, the progress monitor function, the progress user pointer, and the scene build quality. Also added getters for getting the number of attached geometries as well as the array of geometries attached to the scene. All of these are available in the C API.

--- kernels/common/rtcore.cpp
+++ kernels/common/rtcore.cpp
@@ -398,6 +398,32 @@ RTC_NAMESPACE_BEGIN;
     RTC_CATCH_END2(scene);
   }
 
+  RTC_API RTCProgressMonitorFunction rtcGetSceneProgressMonitorFunction(RTCScene hscene)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetSceneProgressMonitorFunction);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_ENTER_DEVICE(hscene);
+    Lock<MutexSys> lock(g_mutex);
+    return scene->getProgressMonitorFunction();
+    RTC_CATCH_END2(scene);
+    return nullptr;
+  }
+
+  RTC_API void* rtcGetSceneProgressMonitorFunctionUserPtr(RTCScene hscene)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetSceneProgressMonitorFunctionUserPtr);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_ENTER_DEVICE(hscene);
+    Lock<MutexSys> lock(g_mutex);
+    return scene->getProgressMonitorFunctionUserPtr();
+    RTC_CATCH_END2(scene);
+    return nullptr;
+  }
+
   RTC_API void rtcSetSceneBuildQuality (RTCScene hscene, RTCBuildQuality quality) 
   {
     Scene* scene = (Scene*) hscene;
@@ -410,6 +436,18 @@ RTC_NAMESPACE_BEGIN;
       throw std::runtime_error("invalid build quality");
     scene->setBuildQuality(quality);
     RTC_CATCH_END2(scene);
+  }
+
+  RTC_API RTCBuildQuality rtcGetSceneBuildQuality (RTCScene hscene)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetSceneBuildQuality);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_ENTER_DEVICE(hscene);
+    return scene->getBuildQuality();
+    RTC_CATCH_END2(scene);
+    return RTC_BUILD_QUALITY_MEDIUM;
   }
 
   RTC_API void rtcSetSceneFlags (RTCScene hscene, RTCSceneFlags flags) 
@@ -460,6 +498,18 @@ RTC_NAMESPACE_BEGIN;
 
     scene->commit(true);
     RTC_CATCH_END2(scene);
+  }
+
+  RTC_API bool rtcIsSceneModified(RTCScene hscene)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcIsSceneModified);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_ENTER_DEVICE(hscene);
+    return scene->isModified();
+    RTC_CATCH_END2(scene);
+    return false;
   }
 
   RTC_API void rtcGetSceneBounds(RTCScene hscene, RTCBounds* bounds_o)
@@ -2359,6 +2409,84 @@ RTC_API void rtcSetGeometryTransform(RTCGeometry hgeometry, unsigned int timeSte
     RTC_VERIFY_HANDLE(hgeometry);
     geometry->refDec();
     RTC_CATCH_END2(geometry);
+  }
+
+  RTC_API size_t rtcGetNumAttachedGeometryIDs (RTCScene hscene)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetNumAttachedGeometryIDs);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_ENTER_DEVICE(hscene);
+    unsigned int numGeomIDs = scene->size();
+    for (unsigned int i = 0; i < scene->size(); ++i) {
+      Geometry* ptr = scene->get(i);
+      if (!ptr) {
+        numGeomIDs -= 1;
+      }
+    }
+    return numGeomIDs;
+    RTC_CATCH_END2(scene);
+    return 0;
+  }
+
+  RTC_API void rtcGetAttachedGeometryIDs (RTCScene hscene, size_t* numGeomIDs, unsigned int* geomIDs)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetAttachedGeometryIDs);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_VERIFY_HANDLE(numGeomIDs);
+    RTC_ENTER_DEVICE(hscene);
+    *numGeomIDs = scene->size();
+    for (unsigned int i = 0; i < scene->size(); ++i) {
+      Geometry* ptr = scene->get(i);
+      if (!ptr) {
+        *numGeomIDs -= 1;
+      } else if (geomIDs) {
+        *geomIDs++ = i;
+      }
+    }
+    RTC_CATCH_END2(scene);
+  }
+
+  RTC_API size_t rtcGetNumAttachedGeometries (RTCScene hscene)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetNumAttachedGeometryIDs);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_ENTER_DEVICE(hscene);
+    unsigned int numGeomIDs = scene->size();
+    for (unsigned int i = 0; i < scene->size(); ++i) {
+      Geometry* ptr = scene->get(i);
+      if (!ptr) {
+        numGeomIDs -= 1;
+      }
+    }
+    return numGeomIDs;
+    RTC_CATCH_END2(scene);
+    return 0;
+  }
+
+  RTC_API void rtcGetAttachedGeometries (RTCScene hscene, size_t* numGeoms, RTCGeometry* geometries)
+  {
+    Scene* scene = (Scene*) hscene;
+    RTC_CATCH_BEGIN;
+    RTC_TRACE(rtcGetAttachedGeometries);
+    RTC_VERIFY_HANDLE(hscene);
+    RTC_VERIFY_HANDLE(numGeoms);
+    RTC_ENTER_DEVICE(hscene);
+    *numGeoms = scene->size();
+    for (unsigned int i = 0; i < scene->size(); ++i) {
+      Geometry* ptr = scene->get(i);
+      if (!ptr) {
+        *numGeoms -= 1;
+      } else if (geometries) {
+        *geometries++ = (RTCGeometry) ptr;
+      }
+    }
+    RTC_CATCH_END2(scene);
   }
 
   RTC_API RTCGeometry rtcGetGeometry (RTCScene hscene, unsigned int geomID)
