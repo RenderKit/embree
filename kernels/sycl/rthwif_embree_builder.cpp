cb11976e5e48f6431fa42df334df53bc8c496a4b
Werner, Stefan <stefan.werner@intel.com>
2026-08-18 11:29:04 +0200

    Removed unused functions and variables

--- kernels/sycl/rthwif_embree_builder.cpp
+++ kernels/sycl/rthwif_embree_builder.cpp
@@ -172,32 +172,11 @@ namespace embree
     return pvc;
   }
 
-  static bool hasDriverExtension(ze_driver_handle_t hDriver, const char* extensionName)
-  {
-    uint32_t count = 0;
-    ze_result_t result = ZeWrapper::zeDriverGetExtensionProperties(hDriver, &count, nullptr);
-    if (result != ZE_RESULT_SUCCESS)
-      return false;
-
-    std::vector<ze_driver_extension_properties_t> extensions(count);
-    result = ZeWrapper::zeDriverGetExtensionProperties(hDriver, &count, extensions.data());
-    if (result != ZE_RESULT_SUCCESS)
-      return false;
-
-    for (uint32_t i = 0; i < count; i++)
-    {
-      if (strncmp(extensionName, extensions[i].name, sizeof(extensions[i].name)) == 0)
-        return true;
-    }
-    return false;
-  }
-
   void* rthwifAllocAccelBuffer(Device* embree_device, size_t bytes, sycl::device device, sycl::context context, sycl::usm::alloc alloc_type)
   {
     ze_context_handle_t hContext = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(context);
     ze_device_handle_t  hDevice  = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(device);
     sycl::platform platform = device.get_platform();
-    ze_driver_handle_t hDriver = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(platform);
 
     ze_rtas_device_ext_properties_t rtasProp = { ZE_STRUCTURE_TYPE_RTAS_DEVICE_EXT_PROPERTIES };
     ze_device_properties_t devProp = { ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES, &rtasProp };
